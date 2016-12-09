648d97dc2673307774de5344bda30b8a731ccdc0
Yves Müller <yves.mueller@liqd.de>
2016-12-09 12:18:49 +0100

    Finish java script packaging

---
+++ index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    ratings: require('./adhocracy4/ratings/static/ratings/react_ratings.jsx'),
+    config: require('./adhocracy4/static/config.js'),
+    api: require('./adhocracy4/static/api.js')
+}
