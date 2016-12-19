6ca267a7e3ffa107d5d569149aa9aa3a4b9a9320
Yves Müller <yves.mueller@liqd.de>
2016-12-19 17:12:41 +0100

    Configure image factories for all tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,13 @@
+import pytest
 from pytest_factoryboy import register
 
+from tests.images import factories as img_factories
 from adhocracy4.test import factories
+
+
+@pytest.fixture
+def image_factory():
+    return  img_factories.ImageFactory()
 
 
 register(factories.UserFactory)
--- tests/images/conftest.py
+++
@@ -1,7 +0,0 @@
-import pytest
-
-from tests.images.factories import ImageFactory
-
-@pytest.fixture
-def image_factory():
-    return  ImageFactory()
