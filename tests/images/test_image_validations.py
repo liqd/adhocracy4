2713dbfc8292a0e9417cf18bb1f9ce53621d75ad
Yves Müller <yves.mueller@liqd.de>
2016-12-28 17:14:07 +0100

    Adapt validation tests to new validation api

--- tests/images/test_image_validations.py
+++ tests/images/test_image_validations.py
@@ -9,14 +9,14 @@ from adhocracy4.images.validators import validate_image
 def test_min_size_validation(image_factory):
     with pytest.raises(ValidationError):
         image = image_factory((100, 100), 'JPEG')
-        validate_image(image, 50, 101)
+        validate_image(image, (50, 101))
 
     with pytest.raises(ValidationError):
         image = image_factory((100, 100), 'JPEG')
-        validate_image(image, 101, 50)
+        validate_image(image, (101, 50))
 
     image = image_factory((100, 100), 'JPEG')
-    validate_image(image, 100, 100)
+    validate_image(image, (100, 100))
 
 
 @override_settings(ALLOWED_UPLOAD_IMAGES=('image/jpeg'))
@@ -24,7 +24,7 @@ def test_aspect_validation(image_factory):
     square_image = image_factory((100, 109), 'JPEG')
     image = image_factory((100, 120), 'JPEG')
 
-    validate_image(square_image, 100, 100, (1, 1))
+    validate_image(square_image, (100, 100), aspect_ratio=(1, 1))
 
     with pytest.raises(ValidationError):
-        validate_image(image, 100, 100, (1, 1))
+        validate_image(image, (100, 100), aspect_ratio=(1, 1))
