6618976b04422b586b4ec5ab98010b29b431b0ef
Yves Müller <yves.mueller@liqd.de>
2017-01-02 17:54:12 +0100

    Add delete signal setup for generics
     - improve documentation of generics
     - setup delete signals for ratings

--- adhocracy4/ratings/apps.py
+++ adhocracy4/ratings/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class RatingsConfig(AppConfig):
     name = 'adhocracy4.ratings'
     label = 'a4ratings'
+
+    def ready(self):
+        from . import signals  # noqa:F401
