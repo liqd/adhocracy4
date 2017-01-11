840572ca07423600f49d5fa68083ee6ed51abc48
vellip <vellerphilipp@googlemail.com>
2017-01-11 14:18:25 +0100

    add migrations

---
+++ adhocracy4/comments/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django.utils.timezone
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Comment',
+            fields=[
+                ('id', models.AutoField(primary_key=True, auto_created=True, verbose_name='ID', serialize=False)),
+                ('created', models.DateTimeField(editable=False, default=django.utils.timezone.now)),
+                ('modified', models.DateTimeField(editable=False, blank=True, null=True)),
+                ('object_pk', models.PositiveIntegerField()),
+                ('comment', models.TextField(max_length=1024)),
+                ('is_removed', models.BooleanField(default=False)),
+                ('is_censored', models.BooleanField(default=False)),
+                ('content_type', models.ForeignKey(to='contenttypes.ContentType')),
+                ('creator', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+                'verbose_name_plural': 'Comments',
+                'ordering': ('created',),
+                'verbose_name': 'Comment',
+            },
+        ),
+    ]
