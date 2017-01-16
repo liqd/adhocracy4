ebbdb88332a6d52ef1c1980116eb0be83fb7236f
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-01-16 14:08:46 +0100

    Use helper function for comments delete handler

--- adhocracy4/comments/signals.py
+++ adhocracy4/comments/signals.py
@@ -1,21 +1,6 @@
 from django.conf import settings
-from django.contrib.contenttypes.models import ContentType
-from django.db.models.signals import post_delete
+from adhocracy4 import generics
 
 from .models import Comment
 
-
-def delete_content_object_handler(sender, instance, **kwargs):
-    contenttype = ContentType.objects.get_for_model(instance)
-    pk = instance.pk
-    Comment.objects\
-           .filter(content_type=contenttype, object_pk=pk)\
-           .delete()
-
-
-# setup signal handlers for all commentables
-for commentable in settings.A4_COMMENTABLES:
-    post_delete.connect(
-        delete_content_object_handler,
-        '.'.join(commentable)
-    )
+generics.setup_delete_signals(settings.A4_COMMENTABLES, Comment)
