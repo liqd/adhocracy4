af5ab6c4e3b96b97c3f4058eeadf77cc5e3b06b2
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-01-25 10:35:25 +0100

    rm aria-hidden on essential elements

--- adhocracy4/comments/static/comments/CommentManageDropdown.jsx
+++ adhocracy4/comments/static/comments/CommentManageDropdown.jsx
@@ -12,15 +12,15 @@ const CommentManageDropdown = (props) => {
         <ul className="dropdown-menu">
           {props.renderModeratorOptions && [
             <li key="1">
-              <button onClick={props.toggleEdit} aria-hidden="true">{django.gettext('Edit')}</button>
+              <button onClick={props.toggleEdit}>{django.gettext('Edit')}</button>
             </li>,
             <li className="divider" key="2" />,
             <li key="3"><a href={`#comment_delete_${props.id}`} data-toggle="modal"
-              aria-hidden="true">{django.gettext('Delete')}</a></li>,
+              >{django.gettext('Delete')}</a></li>,
             <li className="divider" key="4" />
           ]}
           <li><a href={`#report_comment_${props.id}`} data-toggle="modal"
-            aria-hidden="true">{django.gettext('Report')}</a>
+            >{django.gettext('Report')}</a>
           </li>
         </ul>
       </li>
