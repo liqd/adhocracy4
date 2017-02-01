57b2f1bc5eae28279b7c7e58327d91398cb28fe6
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-02-01 14:36:37 +0100

    adapt to adhocracy4#27

--- apps/ideas/views.py
+++ apps/ideas/views.py
@@ -28,10 +28,6 @@ class IdeaListView(mixins.ProjectMixin, SortableListView):
             .annotate_negative_rating_count() \
             .annotate_comment_count()
 
-    # FIXME: remove once https://github.com/liqd/adhocracy4/pull/27 is merged
-    def get_ordering(self):
-        return self.ordering[0]
-
 
 class IdeaDetailView(PermissionRequiredMixin, generic.DetailView):
     model = idea_models.Idea
