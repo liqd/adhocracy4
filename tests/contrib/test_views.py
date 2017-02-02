ee6da3613154c36451b8d4b151fac727dd8f51b8
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-02-02 16:18:08 +0100

    remove duplicate request factory

--- tests/contrib/test_views.py
+++ tests/contrib/test_views.py
@@ -4,40 +4,40 @@ from tests.apps.questions.views import QuestionList
 
 
 @pytest.mark.django_db
-def test_sort_view_ordering_default(request_factory):
-    request = request_factory.get(path='/')
+def test_sort_view_ordering_default(rf):
+    request = rf.get(path='/')
     sort_view = QuestionList(request=request)
     sort_view.dispatch(request)
     assert sort_view.ordering == ['-created']
 
 
 @pytest.mark.django_db
-def test_sort_view_ordering_valid(request_factory):
-    request = request_factory.get(path='/?ordering=text')
+def test_sort_view_ordering_valid(rf):
+    request = rf.get(path='/?ordering=text')
     sort_view = QuestionList(request=request)
     sort_view.dispatch(request)
     assert sort_view.ordering == ['text']
 
 
 @pytest.mark.django_db
-def test_sort_view_ordering_invalid(request_factory):
-    request = request_factory.get(path='/?ordering=invalid')
+def test_sort_view_ordering_invalid(rf):
+    request = rf.get(path='/?ordering=invalid')
     sort_view = QuestionList(request=request)
     sort_view.dispatch(request)
     assert sort_view.ordering == ['-created']
 
 
 @pytest.mark.django_db
-def test_sort_view_get_ordering(request_factory):
-    request = request_factory.get(path='/?ordering=text')
+def test_sort_view_get_ordering(rf):
+    request = rf.get(path='/?ordering=text')
     sort_view = QuestionList(request=request)
     sort_view.dispatch(request)
     assert sort_view.get_ordering() == 'text'
 
 
 @pytest.mark.django_db
-def test_sort_view_ordering_name(request_factory):
-    request = request_factory.get(path='/')
+def test_sort_view_ordering_name(rf):
+    request = rf.get(path='/')
     sort_view = QuestionList(request=request)
     sort_view.dispatch(request)
     assert sort_view.get_ordering_name() == 'Most recent'
