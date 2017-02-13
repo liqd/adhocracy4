aae199b67dbeecab738ed9ddb501b052b12cdcd4
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-02-13 13:43:09 +0100

    add dropdown-item class
    this was added in bootstrap 4 in order to avoid descendant/child
    selectors and make styling easier.
    
    https://getbootstrap.com/components/#dropdowns-example
    https://v4-alpha.getbootstrap.com/components/dropdowns/

--- adhocracy4/comments/static/comments/CommentManageDropdown.jsx
+++ adhocracy4/comments/static/comments/CommentManageDropdown.jsx
@@ -12,14 +12,14 @@ const CommentManageDropdown = (props) => {
         <ul className="dropdown-menu">
           {props.renderModeratorOptions && [
             <li key="1">
-              <button type="button" onClick={props.toggleEdit}>{django.gettext('Edit')}</button>
+              <button type="button" className="dropdown-item" onClick={props.toggleEdit}>{django.gettext('Edit')}</button>
             </li>,
             <li className="divider" key="2" />,
-            <li key="3"><a href={`#comment_delete_${props.id}`} data-toggle="modal"
+            <li key="3"><a className="dropdown-item" href={`#comment_delete_${props.id}`} data-toggle="modal"
               >{django.gettext('Delete')}</a></li>,
             <li className="divider" key="4" />
           ]}
-          <li><a href={`#report_comment_${props.id}`} data-toggle="modal"
+          <li><a className="dropdown-item" href={`#report_comment_${props.id}`} data-toggle="modal"
             >{django.gettext('Report')}</a>
           </li>
         </ul>
