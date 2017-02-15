c5c06eed75f79f9b0d1f124a034379d1c26528c7
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-02-15 12:40:49 +0100

    Gardening

--- meinberlin/assets/js/app.js
+++ meinberlin/assets/js/app.js
@@ -1,14 +1,10 @@
 /* eslint no-unused-vars: "off" */
 
-var ReactComments = require('adhocracy4').comments
-var ReactRatings = require('adhocracy4').ratings
-
-var ReactParagraphs = require('../../../apps/documents/assets/ParagraphBox.jsx')
-
 // make jquery available for non-webpack js
 var $ = window.jQuery = window.$ = require('jquery')
 window.Tether = require('tether/dist/js/tether.js')
 
+// load bootstrap components
 var dropdown = require('bootstrap/js/src/dropdown.js')
 var modal = require('bootstrap/js/src/modal.js')
 var tab = require('bootstrap/js/src/tab.js')
@@ -18,6 +14,12 @@ var popover = require('bootstrap/js/src/popover.js')
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
+
+// expose react components
+var ReactComments = require('adhocracy4').comments
+var ReactRatings = require('adhocracy4').ratings
+
+var ReactParagraphs = require('../../../apps/documents/assets/ParagraphBox.jsx')
 
 module.exports = {
   'renderComment': ReactComments.renderComment,
