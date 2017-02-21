44a49a19aead88b2a11eeec1271508d68a51f5e0
vellip <vellerphilipp@googlemail.com>
2017-02-21 13:59:54 +0100

    fix import order

--- apps/projects/urls.py
+++ apps/projects/urls.py
@@ -1,5 +1,6 @@
-from adhocracy4.projects.urls import urlpatterns as a4_projects_urls
 from django.conf.urls import url
+
+from adhocracy4.projects.urls import urlpatterns as a4_projects_urls
 
 from . import views
 
