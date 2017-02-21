7c7fd06674e176bda54780af37af97c877320418
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-02-21 15:06:25 +0100

    add categories app

---
+++ adhocracy4/categories/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from adhocracy4.models import base
+from adhocracy4.modules import models as module_models
+
+
+class Category(base.UserGeneratedContentModel):
+    name = models.CharField(max_length=120)
+    module = models.ForeignKey(
+        module_models.Module,
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return "{} - {}".format(self.module, self.name)
+
+
+class Categorizable(models.Model):
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        abstract = True
