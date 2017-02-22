41efd7f1318019a002eca4100fe6361171b3ffe2
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-02-22 10:55:13 +0100

    remove creator

--- adhocracy4/categories/models.py
+++ adhocracy4/categories/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-from adhocracy4.models import base
 from adhocracy4.modules import models as module_models
 
 
-class Category(base.UserGeneratedContentModel):
+class Category(models.Model):
     name = models.CharField(max_length=120)
     module = models.ForeignKey(
         module_models.Module,
