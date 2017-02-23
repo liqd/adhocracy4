458f3529a40b70ff2c528a9e60b9108305f36dbd
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-02-23 09:23:07 +0100

    filter by module in django admin

--- adhocracy4/categories/admin.py
+++ adhocracy4/categories/admin.py
@@ -2,7 +2,9 @@ from django.contrib import admin
 
 from . import models
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('module',)
+
 
 admin.site.register(models.Category, CategoryAdmin)
