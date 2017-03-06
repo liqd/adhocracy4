97f5d3595a650d04595dc714a2effa58dbc6c816
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-03-06 16:41:02 +0100

    enforce max_num

--- apps/contrib/static/js/formset.js
+++ apps/contrib/static/js/formset.js
@@ -10,17 +10,21 @@
   var DynamicFormSet = function ($formset) {
     this.$formset = $formset
     this.$formTemplate = this.$formset.find('.js-form-template')
-    this.$formset.find('.js-add-form').on('click', this.addForm.bind(this))
     this.prefix = this.$formset.data('prefix')
     this.$totalInput = this.$formset.find('#id_' + this.prefix + '-TOTAL_FORMS')
     this.id = parseInt(this.$formset.data('initial-id'))
+    this.maxNum = parseInt(this.$formset.find('#id_' + this.prefix + '-MAX_NUM_FORMS').val())
+
+    this.$formset.find('.js-add-form').on('click', this.addForm.bind(this))
   }
 
   DynamicFormSet.prototype.addForm = function () {
-    this.id += 1
-    this.$totalInput.val(this.id + 1)
-    var newForm = getNewForm(this.$formTemplate, this.id)
-    this.$formTemplate.before(newForm)
+    if (this.id + 1 < this.maxNum) {
+      this.id += 1
+      this.$totalInput.val(this.id + 1)
+      var newForm = getNewForm(this.$formTemplate, this.id)
+      this.$formTemplate.before(newForm)
+    }
   }
 
   function getNewForm ($formTemplate, id) {
