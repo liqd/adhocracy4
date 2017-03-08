59df0c907d0f94f6f6151ac67b9022abb405d0fe
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-03-08 13:23:08 +0100

    use filtered-list-view for project list

--- apps/projects/views.py
+++ apps/projects/views.py
@@ -1,13 +1,33 @@
+import django_filters
 from django.utils.translation import ugettext as _
 
-from adhocracy4.contrib.views import SortableListView
+from adhocracy4.contrib.views import FilteredListView
 from adhocracy4.projects import models
 
+from apps.contrib.widgets import DropdownLinkWidget
 
-class ProjectListView(SortableListView):
+
+class OrderingWidget(DropdownLinkWidget):
+    label = _('Ordering')
+    right = True
+
+
+class ProjectFilterSet(django_filters.FilterSet):
+
+    ordering = django_filters.OrderingFilter(
+        choices=(
+            ('-created', _('Most recent')),
+        ),
+        empty_label=None,
+        widget=OrderingWidget,
+    )
+
+    class Meta:
+        model = models.Project
+        fields = []
+
+
+class ProjectListView(FilteredListView):
     model = models.Project
     paginate_by = 16
-    ordering = ['-created']
-    orderings_supported = [
-        ('-created', _('Most recent')),
-    ]
+    filter_set = ProjectFilterSet
