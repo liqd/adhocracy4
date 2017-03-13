881d091d895311fb04cc756d7f98b2d82cc4e2c0
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-03-13 15:08:27 +0100

    resolve migration conflict

--- adhocracy4/projects/migrations/0005_auto_20170313_1407.py
+++ adhocracy4/projects/migrations/0005_auto_20170313_1407.py
@@ -7,12 +7,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('a4projects', '0003_auto_20170130_0836'),
+        ('a4projects', '0004_project_is_archived'),
     ]
 
     operations = [
         migrations.AlterModelOptions(
             name='project',
             options={'ordering': ['-created']},
+        ),
+        migrations.AlterField(
+            model_name='project',
+            name='is_archived',
+            field=models.BooleanField(verbose_name='Project is archived', default=False, help_text='Set to archive the project'),
         ),
     ]
