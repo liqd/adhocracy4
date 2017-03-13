b831ae1e707d1b75c1ab20cca4be09c89ba49687
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-03-13 16:45:52 +0100

    remove sort view

--- tests/apps/questions/views.py
+++ tests/apps/questions/views.py
@@ -1,11 +1,17 @@
-from adhocracy4.contrib.views import SortableListView
+import django_filters
+
+from adhocracy4.contrib.views import FilteredListView
+
 from . import models
 
 
-class QuestionList(SortableListView):
+class QuestionFilterSet(django_filters.FilterSet):
+
+    class Meta:
+        model = models.Question
+        fields = ['text']
+
+
+class QuestionList(FilteredListView):
     model = models.Question
-    ordering = ['-created']
-    orderings_supported = [
-        ('-created', 'Most recent'),
-        ('text', 'Alphabetical'),
-    ]
+    filter_set = QuestionFilterSet
