9b8223f42f4cb2e4a2945b8899c508b9589595e8
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-03-15 09:48:57 +0100

    Cleanups

--- apps/embed/urls.py
+++ apps/embed/urls.py
@@ -3,6 +3,5 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',
-        views.EmbedView.as_view(), name='idea-embed'),
+    url(r'^$', views.EmbedView.as_view(), name='embed'),
 ]
