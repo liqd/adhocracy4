838920c7103027e972c47f452d88bd63e19d5b3e
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-03-21 11:16:12 +0100

    use categoribale for from core

--- apps/ideas/forms.py
+++ apps/ideas/forms.py
@@ -1,25 +1,9 @@
-from django import forms
-
-from adhocracy4.categories import models as category_models
+from adhocracy4.categories import forms as category_forms
 
 from . import models
 
 
-class IdeaForm(forms.ModelForm):
-
-    def __init__(self, *args, **kwargs):
-        module = kwargs.pop('module')
-        super().__init__(*args, **kwargs)
-        queryset = category_models.Category.objects.filter(module=module)
-        self.fields['category'] = forms.ModelChoiceField(
-            queryset=queryset,
-            empty_label=None,
-            required=False,
-        )
-
-    def show_categories(self):
-        module_has_categories = len(self.fields['category'].queryset) > 0
-        return module_has_categories
+class IdeaForm(category_forms.CategorizableForm):
 
     class Meta:
         model = models.Idea
