4803f4a2b9dcd80bf4db15f3cd3c51c4e55ae71c
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-03-22 16:18:48 +0100

    revert document view changes

--- apps/documents/views.py
+++ apps/documents/views.py
@@ -1,12 +1,12 @@
 from django.views import generic
 
-from adhocracy4.modules import views as module_views
+from adhocracy4.projects import mixins as project_mixins
 from adhocracy4.rules import mixins as rules_mixins
 
 from . import models
 
 
-class DocumentCreateView(module_views.ItemCreateView):
+class DocumentCreateView(project_mixins.ProjectMixin, generic.TemplateView):
     template_name = 'meinberlin_documents/document_form.html'
     permission_required = 'meinberlin_documents.view'
 
@@ -15,7 +15,7 @@ class DocumentCreateView(module_views.ItemCreateView):
         return models.Document.objects.filter(module=self.module).first()
 
 
-class DocumentDetailView(module_views.ItemDetailView):
+class DocumentDetailView(project_mixins.ProjectMixin, generic.DetailView):
     model = models.Document
     permission_required = 'meinberlin_documents.view'
 
