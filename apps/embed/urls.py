f0728355eeb757c39667b9073e953463cdbf590c
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-03-28 10:48:51 +0200

    rename/refactor view

--- apps/embed/urls.py
+++ apps/embed/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^projects/(?P<slug>[-\w_]+)/$', views.EmbedView.as_view(),
+    url(r'^projects/(?P<slug>[-\w_]+)/$', views.EmbedProjectView.as_view(),
         name='embed-project'),
 ]
