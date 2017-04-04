840de90d9d44907112a6f28068f45f6b7c820d96
Yves Müller <yves.mueller@liqd.de>
2017-04-04 15:12:18 +0200

    Enable rules permissions for ratings

---
+++ adhocracy4/ratings/rules.py
@@ -0,0 +1,34 @@
+import rules
+
+from adhocracy4.modules import predicates as module_predicates
+from adhocracy4.phases import predicates as phase_predicates
+
+
+@rules.predicate
+def content_object_allows_rating(user, rating):
+    content_object = rating.content_object
+    return phase_predicates.has_feature_active(
+        content_object.project, content_object.__class__, 'rate'
+    )
+
+
+rules.add_perm(
+    'a4ratings.change_rating',
+    module_predicates.is_project_admin |
+    (
+        module_predicates.is_context_member &
+        content_object_allows_rating &
+        module_predicates.is_owner
+    )
+)
+
+
+rules.add_perm(
+    'a4ratings.delete_rating',
+    module_predicates.is_project_admin |
+    (
+        content_object_allows_rating &
+        module_predicates.is_context_member &
+        module_predicates.is_owner
+    )
+)
