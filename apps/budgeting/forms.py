c99327363abd7d11cf0891c463f18faa643af93e
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-04-11 17:52:23 +0200

    Add moderator statement model to budgeting
    Add a moderator statement model and make it editable with a MultiForm on
    the moderation page.

--- apps/budgeting/forms.py
+++ apps/budgeting/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from multiform import MultiModelForm
 
 from adhocracy4.categories import forms as category_forms
 
@@ -12,12 +13,50 @@ class ProposalForm(category_forms.CategorizableForm):
         fields = ['name', 'description', 'category', 'budget']
 
 
-class ProposalModerateForm(forms.ModelForm):
+class ModeratorFeedbackForm(forms.ModelForm):
 
     def __init__(self, *args, **kwargs):
-        kwargs.pop('module')
-        super(ProposalModerateForm, self).__init__(*args, **kwargs)
+        kwargs.pop('user', None)
+        super(ModeratorFeedbackForm, self).__init__(*args, **kwargs)
 
     class Meta:
         model = models.Proposal
         fields = ['moderator_feedback']
+
+
+class ModeratorStatementForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(ModeratorStatementForm, self).__init__(*args, **kwargs)
+
+    def save(self, *args, **kwargs):
+        if self.instance.pk is None:
+            self.instance.creator = self.user
+        super(ModeratorStatementForm, self).save(*args, **kwargs)
+
+    class Meta:
+        model = models.ModeratorStatement
+        fields = ['statement']
+
+
+class ProposalModerateForm(MultiModelForm):
+    base_forms = [
+        ('feedback', ModeratorFeedbackForm),
+        ('statement', ModeratorStatementForm),
+    ]
+
+    def dispatch_init_instance(self, name, instance):
+        if name == 'feedback':
+            return instance
+
+        if name == 'statement':
+            try:
+                statement = instance.moderator_statement
+                return statement
+            except models.Proposal.moderator_statement\
+                    .RelatedObjectDoesNotExist:
+                return None
+
+        return super(ProposalModerateForm, self)\
+            .dispatch_init_instance(name, instance)
--- apps/budgeting/models.py
+++ apps/budgeting/models.py
@@ -1,6 +1,9 @@
+from ckeditor.fields import RichTextField
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+
+from adhocracy4.models.base import UserGeneratedContentModel
 
 from apps.ideas import models as idea_models
 from apps.moderatorfeedback.fields import ModeratorFeedbackField
@@ -22,3 +25,11 @@ class Proposal(idea_models.AbstractIdea):
 
     def get_absolute_url(self):
         return reverse('budgeting:proposal-detail', args=[str(self.slug)])
+
+
+class ModeratorStatement(UserGeneratedContentModel):
+    proposal = models.OneToOneField(Proposal,
+                                    primary_key=True,
+                                    related_name="moderator_statement")
+
+    statement = RichTextField(blank=True)
