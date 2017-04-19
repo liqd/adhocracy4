6629ea0a39b920c25c846b2557bf36359de539df
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-04-19 14:31:19 +0200

    add preset UI

--- apps/maps/widgets.py
+++ apps/maps/widgets.py
@@ -5,6 +5,9 @@ from django.contrib.staticfiles.storage import staticfiles_storage
 from django.forms.widgets import Widget
 from django.template import loader
 
+from apps.maps.models import MapPreset
+from apps.maps.models import MapPresetCategory
+
 
 class MapChoosePolygonWithPresetWidget(Widget):
 
@@ -21,14 +24,29 @@ class MapChoosePolygonWithPresetWidget(Widget):
             staticfiles_storage.url('leaflet.draw.css'),
         ]}
 
+    def get_presets(self, category):
+        presets = MapPreset.objects.filter(category=category).all()
+
+        return [{
+            'name': preset.name,
+            'polygon': json.dumps(preset.polygon)
+        } for preset in presets]
+
     def render(self, name, value, attrs):
+        presets_uncategorized = self.get_presets(None)
+        preset_categories = [
+            (category.name, self.get_presets(category))
+            for category in MapPresetCategory.objects.all()
+        ]
 
         context = {
             'baseurl': settings.A4_MAP_BASEURL,
             'attribution': settings.A4_MAP_ATTRIBUTION,
             'bbox': json.dumps(settings.A4_MAP_BOUNDING_BOX),
             'name': name,
-            'polygon': value
+            'polygon': value,
+            'presets_uncategorized': presets_uncategorized,
+            'preset_categories': preset_categories
         }
 
         return loader.render_to_string(
