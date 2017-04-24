077156a69250f09a177e692024fa045e47347c4f
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-04-24 14:41:35 +0200

    rename change project permission

--- adhocracy4/projects/rules.py
+++ adhocracy4/projects/rules.py
@@ -5,7 +5,7 @@ from adhocracy4.organisations.predicates import is_initiator
 
 from .predicates import is_live, is_member, is_public
 
-rules.add_perm('a4projects.edit_project',
+rules.add_perm('a4projects.change_project',
                is_superuser | is_initiator)
 
 
