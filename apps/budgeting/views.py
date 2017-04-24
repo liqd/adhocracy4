629b4a2c1451da70c19c02300de7e5bf653798cc
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-04-24 14:55:45 +0200

    Redirect to proposal detail after moderation

--- apps/budgeting/views.py
+++ apps/budgeting/views.py
@@ -90,7 +90,10 @@ class ProposalModerateView(rules_mixins.PermissionRequiredMixin,
     template_name = 'meinberlin_budgeting/proposal_moderate_form.html'
 
     def get_success_url(self):
-        return self.request.path
+        # Multiforms are not working nicely with the UpdateView redirection,
+        # so we have to use internal knowledge of the instantiated multiforms
+        # names for the correct redirection
+        return self.object['feedback'].get_absolute_url()
 
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
