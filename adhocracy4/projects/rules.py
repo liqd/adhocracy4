96d4be9d12c30504a7d768f3cd50bfdbfb5f8031
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-04-26 10:01:27 +0200

    add project rule

--- adhocracy4/projects/rules.py
+++ adhocracy4/projects/rules.py
@@ -5,6 +5,11 @@ from adhocracy4.organisations.predicates import is_initiator
 
 from .predicates import is_live, is_member, is_public
 
+
+rules.add_perm('a4projects.add_project',
+               is_superuser | is_initiator)
+
+
 rules.add_perm('a4projects.change_project',
                is_superuser | is_initiator)
 
