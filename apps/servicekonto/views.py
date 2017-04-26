279f080408ce01c1cf16ad9e424cdca4bd5fef8e
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-04-26 16:29:04 +0200

    backup

--- apps/servicekonto/views.py
+++ apps/servicekonto/views.py
@@ -1,3 +1,4 @@
+import osa
 from django.conf import settings
 from django.http import HttpResponseRedirect
 from django.views.decorators.csrf import csrf_exempt
@@ -22,8 +23,33 @@ def login(request):
 
 @csrf_exempt
 def callback(request):
-    pass
+    token = request.GET.get('Token', '')
+    if not token:
+        return render_authentication_error(
+            request, ServiceKontoProvider.id, error=AuthError.UNKNOWN)
+    complete_login()
 
 
-def complete_login(request, app, code):
-    pass
+def complete_login(request, token):
+    provider = providers.registry.by_id(ServiceKontoProvider.id, request)
+    try:
+        service_konto = osa.Client(service_konto_url)
+    except AttributeError:
+        raise ValueError('Failed connecting to ServiceKonto.')
+    response = service_konto.service.GetUserData(token)
+    response = requests.get(ACCESS_TOKEN_URL, {
+        'action': 'authorize',
+        'app': app.secret,
+        'code': code
+    })
+    response.raise_for_status()
+    response_json = response.json()
+
+    if 'error' in response_json:
+        raise DraugiemApiError(response_json['error'])
+
+    token = SocialToken(app=app, token=response_json['apikey'])
+
+    login = provider.sociallogin_from_response(request, response_json)
+    login.token = token
+    return login
