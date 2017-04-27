6ff7a572863348153e904239abd63f76ef87abe0
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-04-27 11:12:52 +0200

    backup

--- apps/servicekonto/views.py
+++ apps/servicekonto/views.py
@@ -27,29 +27,17 @@ def callback(request):
     if not token:
         return render_authentication_error(
             request, ServiceKontoProvider.id, error=AuthError.UNKNOWN)
-    complete_login()
+    complete_login(request, token)
 
 
 def complete_login(request, token):
     provider = providers.registry.by_id(ServiceKontoProvider.id, request)
     try:
-        service_konto = osa.Client(service_konto_url)
+        service_konto = osa.Client(settings.SERVICE_KONTO_API_URL)
     except AttributeError:
-        raise ValueError('Failed connecting to ServiceKonto.')
-    response = service_konto.service.GetUserData(token)
-    response = requests.get(ACCESS_TOKEN_URL, {
-        'action': 'authorize',
-        'app': app.secret,
-        'code': code
-    })
-    response.raise_for_status()
-    response_json = response.json()
-
-    if 'error' in response_json:
-        raise DraugiemApiError(response_json['error'])
-
-    token = SocialToken(app=app, token=response_json['apikey'])
-
-    login = provider.sociallogin_from_response(request, response_json)
+        return render_authentication_error(
+            request, ServiceKontoProvider.id, error=AuthError.UNKNOWN)
+    result = service_konto.service.GetUserData(token)
+    login = provider.sociallogin_from_response(request, None)
     login.token = token
     return login
