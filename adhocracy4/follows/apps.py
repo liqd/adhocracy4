87705d1ea8345b227c0fe34272aeae1163ac317d
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-05-02 16:40:49 +0200

    remove follows app

--- adhocracy4/follows/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class FollowsConfig(AppConfig):
-    name = 'euth.follows'
-    label = 'euth_follows'
-
-    def ready(self):
-        import euth.follows.signals  # noqa:F401
--- adhocracy4/follows/static/follows/react_follows.jsx
+++
@@ -1,8 +0,0 @@
-var FollowButton = require('./FollowButton')
-var React = require('react')
-var ReactDOM = require('react-dom')
-
-module.exports.renderFollow = function (el) {
-  let project = el.getAttribute('data-project')
-  ReactDOM.render(<FollowButton project={project} />, el)
-}
