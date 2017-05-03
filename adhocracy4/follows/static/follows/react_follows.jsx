e02ab9047a5963168e0235a278e9674c042e2144
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-05-03 17:00:32 +0200

    use inline script to initialize component
    At some point we want to use the same approach as in opin also in core,
    but not yet. See https://github.com/liqd/adhocracy4/issues/86

--- adhocracy4/follows/static/follows/react_follows.jsx
+++ adhocracy4/follows/static/follows/react_follows.jsx
@@ -2,7 +2,8 @@ var FollowButton = require('./FollowButton')
 var React = require('react')
 var ReactDOM = require('react-dom')
 
-module.exports.renderFollow = function (el) {
+module.exports.renderFollow = function (mountpoint) {
+  let el = document.getElementById(mountpoint)
   let project = el.getAttribute('data-project')
   ReactDOM.render(<FollowButton project={project} />, el)
 }
--- adhocracy4/follows/templatetags/react_follows.py
+++ adhocracy4/follows/templatetags/react_follows.py
@@ -6,7 +6,13 @@ register = template.Library()
 
 @register.simple_tag()
 def react_follows(project):
+    mountpoint = 'follows_for_project_{pk}'.format(pk=project.pk)
+
     return format_html(
-        '<span data-euth-widget="follows" data-project={project}></span>',
-        project=project.slug
+        (
+            '<span id="{mountpoint}" data-project={project}></span>'
+            "<script>window.adhocracy4.renderFollow('{mountpoint}')</script>"
+        ),
+        project=project.slug,
+        mountpoint=mountpoint
     )
