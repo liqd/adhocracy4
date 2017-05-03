ffdc8b2585adc4491974a6fb606536fdf375ee95
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-05-03 10:35:27 +0200

    use add isntead of create

--- apps/organisations/rules.py
+++ apps/organisations/rules.py
@@ -5,6 +5,3 @@ from .predicates import is_initiator
 
 rules.add_perm('meinberlin_organisations.modify_organisation',
                is_superuser | is_initiator)
-
-rules.add_perm('meinberlin_organisations.initiate_project',
-               is_superuser | is_initiator)
