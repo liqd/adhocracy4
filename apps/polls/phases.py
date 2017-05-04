7f6564e9b2d27331cb34bd57ae7a349363a5c233
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-05-04 16:25:18 +0200

    Remove comment phase from polls

--- apps/polls/phases.py
+++ apps/polls/phases.py
@@ -23,21 +23,4 @@ class VotingPhase(phases.PhaseContent):
         'comment': (models.Poll,),
     }
 
-
-class CommentPhase(phases.PhaseContent):
-    app = apps.Config.label
-    phase = 'voting'
-    weight = 30
-    view = views.PollDetailView
-
-    name = _('Comment phase')
-    description = _('Discuss the poll results.')
-    module_name = _('polls')
-
-    features = {
-        'comment': (models.Poll,),
-    }
-
-
 phases.content.register(VotingPhase())
-phases.content.register(CommentPhase())
