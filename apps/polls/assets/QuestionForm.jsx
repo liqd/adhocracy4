c164ecd7d035a81b3eadc5747684adc91c920b02
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-05-08 18:27:28 +0200

    rm unnecessary nesting

--- apps/polls/assets/QuestionForm.jsx
+++ apps/polls/assets/QuestionForm.jsx
@@ -48,71 +48,69 @@ let QuestionForm = React.createClass({
 
   render: function () {
     return (
-      <section>
-        <div className="commenting">
-          <div className="commenting__content">
-            <div className="form-group">
-              <label
-                htmlFor={'id_questions-' + this.props.key + '-name'}>
-                {django.gettext('Question:')}
-              </label>
-              <textarea
-                id={'id_questions-' + this.props.key + '-name'}
-                name={'questions-' + this.props.key + '-name'}
-                defaultValue={this.props.question.label}
-                onChange={this.handleLabelChange} />
-              <ErrorList errors={this.props.errors} />
-            </div>
-
-            <FlipMove easing="cubic-bezier(0.25, 0.5, 0.75, 1)">
-              {
-                this.props.question.choices.map((choice, index) => {
-                  var key = choice.id || choice.key
-                  var errors = this.props.errors && this.props.errors.choices
-                    ? this.props.errors.choices[index] : {}
-                  return (
-                    <ChoiceForm
-                      key={key}
-                      index={index}
-                      choice={choice}
-                      updateChoiceLabel={this.handleUpdateChoiceLabel}
-                      deleteChoice={this.handleDeleteChoice}
-                      errors={errors}
-                    />
-                  )
-                })
-              }
-            </FlipMove>
-            <button
-              className="button button--light"
-              onClick={this.handleAppendChoice}
-              type="button">
-              <i className="fa fa-plus" /> {django.gettext('add a new choice')}
-            </button>
+      <section className="commenting">
+        <div className="commenting__content">
+          <div className="form-group">
+            <label
+              htmlFor={'id_questions-' + this.props.key + '-name'}>
+              {django.gettext('Question:')}
+            </label>
+            <textarea
+              id={'id_questions-' + this.props.key + '-name'}
+              name={'questions-' + this.props.key + '-name'}
+              defaultValue={this.props.question.label}
+              onChange={this.handleLabelChange} />
+            <ErrorList errors={this.props.errors} />
           </div>
 
-          <div className="commenting__actions button-group">
-            <button
-              className="button button--light"
-              onClick={this.handleMoveUp}
-              disabled={!this.props.moveQuestionUp}
-              type="button">
-              <i className="fa fa-chevron-up" />
-            </button>
-            <button
-              className="button button--light"
-              onClick={this.handleMoveDown}
-              disabled={!this.props.moveQuestionDown}
-              type="button">
-              <i className="fa fa-chevron-down" />
-            </button>
-            <button
-              className="button button--light"
-              onClick={this.handleDelete}
-              type="button">
-              <i className="fa fa-trash" />
-            </button>
-          </div>
+          <FlipMove easing="cubic-bezier(0.25, 0.5, 0.75, 1)">
+            {
+              this.props.question.choices.map((choice, index) => {
+                var key = choice.id || choice.key
+                var errors = this.props.errors && this.props.errors.choices
+                  ? this.props.errors.choices[index] : {}
+                return (
+                  <ChoiceForm
+                    key={key}
+                    index={index}
+                    choice={choice}
+                    updateChoiceLabel={this.handleUpdateChoiceLabel}
+                    deleteChoice={this.handleDeleteChoice}
+                    errors={errors}
+                  />
+                )
+              })
+            }
+          </FlipMove>
+          <button
+            className="button button--light"
+            onClick={this.handleAppendChoice}
+            type="button">
+            <i className="fa fa-plus" /> {django.gettext('add a new choice')}
+          </button>
+        </div>
+
+        <div className="commenting__actions button-group">
+          <button
+            className="button button--light"
+            onClick={this.handleMoveUp}
+            disabled={!this.props.moveQuestionUp}
+            type="button">
+            <i className="fa fa-chevron-up" />
+          </button>
+          <button
+            className="button button--light"
+            onClick={this.handleMoveDown}
+            disabled={!this.props.moveQuestionDown}
+            type="button">
+            <i className="fa fa-chevron-down" />
+          </button>
+          <button
+            className="button button--light"
+            onClick={this.handleDelete}
+            type="button">
+            <i className="fa fa-trash" />
+          </button>
         </div>
       </section>
     )
