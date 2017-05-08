556b38140556ed48a40f378fa002502061d2761a
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-05-08 17:29:05 +0200

    fix copy-paste error

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ module.exports = {
   ratings: require('./adhocracy4/ratings/static/ratings/react_ratings.jsx'),
   comments: require('./adhocracy4/comments/static/comments/react_comments.jsx'),
   reports: require('./adhocracy4/reports/static/reports/react_reports.jsx'),
-  follows: require('./adhocracy4/reports/static/reports/react_follows.jsx'),
+  follows: require('./adhocracy4/follows/static/follows/react_follows.jsx'),
   config: require('./adhocracy4/static/config.js'),
   api: require('./adhocracy4/static/api.js')
 }
