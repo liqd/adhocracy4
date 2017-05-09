71f8edd0c61bd4bc02e26aef9c22a59cae9f8704
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-05-09 14:36:03 +0200

    Send bplan notification emails signal handling

---
+++ apps/bplan/emails.py
@@ -0,0 +1,32 @@
+from adhocracy4.emails import Email
+
+
+class OfficeWorkerNotification(Email):
+    template_name = 'meinberlin_bplan/emails/office_worker_notification'
+    fallback_language = 'de'
+
+    @property
+    def office_worker_email(self):
+        project = self.object.project
+        return project.externalproject.bplan.office_worker_email
+
+    def get_receivers(self):
+        return [self.office_worker_email]
+
+    def get_context(self):
+        context = super().get_context()
+        context['project'] = self.object.project
+        return context
+
+
+class SubmitterConfirmation(Email):
+    template_name = 'meinberlin_bplan/emails/submitter_confirmation'
+    fallback_language = 'de'
+
+    def get_receivers(self):
+        return [self.object.email]
+
+    def get_context(self):
+        context = super().get_context()
+        context['project'] = self.object.project
+        return context
