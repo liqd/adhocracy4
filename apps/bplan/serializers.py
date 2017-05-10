c5f047f60690319110df49aec3c5581b84031490
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-05-10 16:46:25 +0200

    image from image_url

--- apps/bplan/serializers.py
+++ apps/bplan/serializers.py
@@ -1,3 +1,8 @@
+import os
+
+from urllib import request
+from urllib.parse import urlparse
+
 from django.apps import apps
 from django.conf import settings
 from django.contrib.sites.models import Site
@@ -16,6 +21,8 @@ from .phases import StatementPhase
 BPLAN_EMBED = '<iframe height="500" style="width: 100%; min-height: 300px; ' \
               'max-height: 100vh" src="{}" frameborder="0"></iframe>'
 
+PROJECT_IMAGE_DIR = 'projects/backgrounds/'
+
 
 class BplanSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(required=False)
@@ -25,13 +32,14 @@ class BplanSerializer(serializers.ModelSerializer):
     # write_only as not part of the model
     start_date = serializers.DateTimeField(write_only=True)
     end_date = serializers.DateTimeField(write_only=True)
+    image_url = serializers.URLField(required=False, write_only=True)
 
     class Meta:
         model = Bplan
         fields = (
             'id', 'name', 'description', 'is_archived', 'url',
             'office_worker_email', 'typ', 'is_draft', 'start_date',
-            'end_date'
+            'end_date', 'image_url'
         )
 
     def create(self, validated_data):
@@ -39,8 +47,19 @@ class BplanSerializer(serializers.ModelSerializer):
         orga_model = apps.get_model(settings.A4_ORGANISATIONS_MODEL)
         orga = orga_model.objects.get(pk=orga_pk)
         validated_data['organisation'] = orga
+
         start_date = validated_data.pop('start_date')
         end_date = validated_data.pop('end_date')
+
+        image_url = validated_data.pop('image_url', None)
+        if image_url:
+            try:
+                validated_data['image'] = \
+                    self._download_image_from_url(image_url)
+            except:
+                raise serializers.ValidationError(
+                    'Failed to download image {}'.format(image_url))
+
         bplan = super().create(validated_data)
         self._create_module_and_phase(bplan, start_date, end_date)
         return bplan
@@ -77,3 +96,10 @@ class BplanSerializer(serializers.ModelSerializer):
         embed_url = reverse('embed-project', kwargs={'slug': bplan.slug, })
         url = 'https://{}{}'.format(site_url, embed_url)
         return url
+
+    def _download_image_from_url(self, url):
+        parsed_url = urlparse(url)
+        file_path = PROJECT_IMAGE_DIR + os.path.basename(parsed_url.path)
+        file_name = settings.MEDIA_ROOT + '/' + file_path
+        request.urlretrieve(url, file_name)
+        return file_path
