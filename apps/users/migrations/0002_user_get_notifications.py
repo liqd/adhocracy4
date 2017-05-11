3a880674d85d50cbb07edc3165738585f96e353d
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-05-11 13:11:30 +0200

    Add get_notifications setting to user model

---
+++ apps/users/migrations/0002_user_get_notifications.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('meinberlin_users', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='user',
+            name='get_notifications',
+            field=models.BooleanField(verbose_name='Send me email notifications', default=True, help_text='Designates whether you want to receive notifications. Unselect if you do not want to receive notifications.'),
+        ),
+    ]
