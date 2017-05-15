510f7aae206c9487b1b16f81a12c8f88eca6e52a
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-05-15 18:15:02 +0200

    simplify input action

--- apps/polls/assets/ChoiceForm.jsx
+++ apps/polls/assets/ChoiceForm.jsx
@@ -15,26 +15,28 @@ let ChoiceForm = React.createClass({
 
   render: function () {
     return (
-      <div className="form-group input-action input-action--center input-action--spaced">
+      <div className="form-group">
         <label
           className="sr-only"
           htmlFor={'id_choices-' + this.props.index + '-name'}>
           {django.gettext('Choice') + ` #${this.props.index}:`}
         </label>
-        <input
-          id={'id_choices-' + this.props.index + '-name'}
-          name={'choices-' + this.props.index + '-name'}
-          type="text"
-          className="input-action__input"
-          defaultValue={this.props.choice.label}
-          onChange={this.handleLabelChange} />
-        <button
-          className="input-action__action button button--light"
-          onClick={this.handleDelete}
-          aria-label={django.gettext('remove')}
-          type="button">
-          <i className="fa fa-times" />
-        </button>
+        <div className="button-group input-action">
+          <input
+            id={'id_choices-' + this.props.index + '-name'}
+            name={'choices-' + this.props.index + '-name'}
+            type="text"
+            className="input-action__input"
+            defaultValue={this.props.choice.label}
+            onChange={this.handleLabelChange} />
+          <button
+            className="input-action__action button button--light"
+            onClick={this.handleDelete}
+            aria-label={django.gettext('remove')}
+            type="button">
+            <i className="fa fa-times" />
+          </button>
+        </div>
         <ErrorList errors={this.props.errors} />
       </div>
     )
