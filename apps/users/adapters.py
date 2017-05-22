3f8d446007ca20c900d6766691993c0b57cd5e54
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-05-22 16:19:51 +0200

    Use the meinBerlin email template for allauth

--- apps/users/adapters.py
+++ apps/users/adapters.py
@@ -1,9 +1,26 @@
 import re
 
 from allauth.account.adapter import DefaultAccountAdapter
+from django.conf import settings
+
+from adhocracy4.emails import Email
 
 from apps.users import USERNAME_INVALID_MESSAGE
 from apps.users import USERNAME_REGEX
+
+
+class UserAccountEmail(Email):
+    def get_receivers(self):
+        return [self.object]
+
+    @property
+    def template_name(self):
+        return self.kwargs['template_name']
+
+    def get_context(self):
+        context = super().get_context()
+        context['contact_email'] = settings.CONTACT_EMAIL
+        return context
 
 
 class AccountAdapter(DefaultAccountAdapter):
@@ -12,3 +29,11 @@ class AccountAdapter(DefaultAccountAdapter):
         DefaultAccountAdapter.error_messages,
         invalid_username=USERNAME_INVALID_MESSAGE
     )
+
+    def send_mail(self, template_prefix, email, context):
+        user = context['user']
+        return UserAccountEmail.send(
+            user,
+            template_name=template_prefix,
+            **context
+        )
