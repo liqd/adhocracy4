6fc66c879346564b29334c394b1f24bafab08fce
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-05-24 11:08:18 +0200

    show participated projects in user profile

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,7 @@
+from django.db import models as django_models
 from django.views.generic.detail import DetailView
+
+from adhocracy4.projects.models import Project
 
 from . import models
 
@@ -6,3 +9,15 @@ from . import models
 class ProfileView(DetailView):
     model = models.User
     slug_field = 'username'
+
+    @property
+    def get_participated_projects(self):
+        user = self.object
+
+        qs = Project.objects.filter(
+            django_models.Q(follow__creator=user),
+            django_models.Q(follow__enabled=True) |
+            django_models.Q(participants=user)
+        ).distinct()
+
+        return qs
