3912e63570de5e11831204dd709412c10ab35840
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-06-21 15:25:50 +0200

    Fix argument unpacking
    args and kwargs may be serialized as array and dict
    but they have to be unpacked to call the dispatcher

--- adhocracy4/emails/tasks.py
+++ adhocracy4/emails/tasks.py
@@ -6,7 +6,7 @@ from background_task import background
 @background(schedule=1)
 def send_async(email_module_name, email_class_name,
                app_label, model_name, object_id,
-               *args, **kwargs):
+               args, kwargs):
     mod = importlib.import_module(email_module_name)
     cls = getattr(mod, email_class_name)
     model = apps.get_model(app_label, model_name)
