0f11f3d3ae6ace992649dbcddaf7e7933c7642e0
vellip <vellerphilipp@googlemail.com>
2017-07-14 13:57:36 +0200

    fix & add tests

--- tests/projects/test_views.py
+++ tests/projects/test_views.py
@@ -6,7 +6,6 @@ from django.core.urlresolvers import reverse
 def test_hide_private_projects(client, user, project_factory):
     public = project_factory()
     private = project_factory(is_public=False)
-    private.participants.add(user)
 
     client.login(username=user, password='password')
     url = reverse('project-list')
@@ -16,3 +15,35 @@ def test_hide_private_projects(client, user, project_factory):
     project_list = response.context['project_list']
     assert public in project_list
     assert private not in project_list
+
+
+@pytest.mark.django_db
+def test_show_private_projects_participant(client, user, project_factory):
+    public = project_factory()
+    private = project_factory(is_public=False)
+    private.participants.add(user)
+
+    client.login(username=user, password='password')
+    url = reverse('project-list')
+    response = client.get(url)
+    assert response.status_code == 200
+
+    project_list = response.context['project_list']
+    assert public in project_list
+    assert private in project_list
+
+
+@pytest.mark.django_db
+def test_show_private_projects_initiators(client, user, project_factory):
+    public = project_factory()
+    private = project_factory(is_public=False)
+    private.organisation.initiators.add(user)
+
+    client.login(username=user, password='password')
+    url = reverse('project-list')
+    response = client.get(url)
+    assert response.status_code == 200
+
+    project_list = response.context['project_list']
+    assert public in project_list
+    assert private in project_list
