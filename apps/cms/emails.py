f3c6fe6b3c75a03747d822bd58832667b6f1690a
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-07-17 16:20:33 +0200

    send forms as xlsx isntead of csv

--- apps/cms/emails.py
+++ apps/cms/emails.py
@@ -1,7 +1,8 @@
-import csv
 import io
+from email.mime.application import MIMEApplication
 from email.mime.text import MIMEText
 
+import xlsxwriter
 from django.utils import timezone
 from django.utils.translation import ugettext as _
 
@@ -15,24 +16,34 @@ class FormEmail(Email):
         return [x.strip() for x in self.object.to_address.split(',')]
 
 
-class CsvFormEmail(FormEmail):
+class XlsxFormEmail(FormEmail):
 
-    def _generate_csv(self):
-        stream = io.StringIO()
-        fw = csv.writer(stream, delimiter=';', quoting=csv.QUOTE_MINIMAL)
-        fw.writerow([_('Form Field'), _('Response')])
-        for field, value in self.object.field_values.items():
-            fw.writerow([field, value])
+    def _generate_xlsx(self):
+        stream = io.BytesIO()
+        workbook = xlsxwriter.Workbook(stream, {'in_memory': True})
+        worksheet = workbook.add_worksheet()
+
+        worksheet.write(0, 0, _('Form Field'))
+        worksheet.write(0, 1, _('Response'))
+
+        rows = self.object.field_values.items()
+        for rownum, row in enumerate(rows, start=1):
+            worksheet.write(rownum, 0, row[0])
+            worksheet.write(rownum, 1, row[1])
+
+        workbook.close()
         return stream.getvalue()
 
     def get_attachments(self):
         attachments = super().get_attachments()
-        csv_text = self._generate_csv()
-        mime_doc = MIMEText(_text=csv_text, _subtype='csv')
+        xlsx_data = self._generate_xlsx()
+        mime_doc = MIMEApplication(
+            _data=xlsx_data,
+            _subtype='vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         timestamp = timezone.now().strftime("%Y-%m-%d")
         form_title = self.object.title.replace(' ', '_')
         submission_pk = self.object.get_submission_class().objects.last().pk
-        filename = '{}_{}_{}.csv'.format(timestamp, form_title, submission_pk)
+        filename = '{}_{}_{}.xlsx'.format(timestamp, form_title, submission_pk)
         mime_doc.add_header(
             'Content-Disposition',
             'attachment; filename="{}"'.format(filename)
