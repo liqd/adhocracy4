9c485de0d0bf87d83665b53675d9140a552fd5f6
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-07-17 14:48:48 +0200

    Add test for new property

--- tests/modules/test_module_models.py
+++ tests/modules/test_module_models.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 import pytest
 
 from tests.apps.questions import models as q_models
@@ -7,3 +8,11 @@ from tests.apps.questions import models as q_models
 def test_has_feature(phase):
     module = phase.module
     assert module.has_feature('crud', q_models.Question)
+
+
+@pytest.mark.django_db
+def test_first_phase_start_date(phase, phase_factory):
+    module = phase.module
+    first_phase = phase_factory(
+        module=module, start_date=phase.start_date - timedelta(days=1))
+    assert module.first_phase_start_date == first_phase.start_date
