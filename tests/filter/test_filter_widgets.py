c3ef2d1a265e6b9d15be7f7b66ed05689a98c028
fuzzylogic2000 <katharina.albers@liqd.de>
2017-07-24 14:02:46 +0200

    add tests

---
+++ tests/filter/test_filter_widgets.py
@@ -0,0 +1,31 @@
+from adhocracy4.filters import widgets
+
+
+def test_render_dropdown_link_widget_without_choices():
+    widget = widgets.DropdownLinkWidget()
+    html = widget.render('test_filter', 'test_val1', attrs={'id': 'test_id'})
+
+    assert len(widget.choices) == 0
+    assert '' == html
+
+
+def test_render_dropdown_link_widget():
+
+    choices = (
+        ('test-val1', 'test-label1'),
+        ('test-val2', 'test-label2'),
+    )
+    widget = widgets.DropdownLinkWidget(choices=choices)
+    html = widget.render('test_filter', 'test_val1', attrs={'id': 'test_id'})
+
+    assert len(widget.choices) == 2
+    assert ('<li><a href="?test_filter=test-val1">test-label1</a></li>\n'
+            '<li><a href="?test_filter=test-val2">test-label2</a></li>'
+            in html)
+
+
+def test_render_text_input_widget():
+    widget = widgets.TextInputWidget()
+    html = widget.render('test_filter', 'test_val1', attrs={'id': 'test_id'})
+
+    assert ('<label for="test_id" class="filter-label">') in html
--- tests/project/settings.py
+++ tests/project/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'adhocracy4.maps.apps.MapsConfig',
     'adhocracy4.actions.apps.ActionsConfig',
     'adhocracy4.follows.apps.FollowsConfig',
+    'adhocracy4.filters.apps.FiltersConfig',
 
     # adhocrayc4 generic apps
     'adhocracy4.ratings.apps.RatingsConfig',
