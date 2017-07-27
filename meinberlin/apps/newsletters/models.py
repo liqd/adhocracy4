f264ac04467bef44afcf7e40eda7a9683820c163
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-07-27 16:36:41 +0200

    Replace relative media urls on newsletter save

--- meinberlin/apps/newsletters/models.py
+++ meinberlin/apps/newsletters/models.py
@@ -1,3 +1,5 @@
+import re
+
 from ckeditor_uploader.fields import RichTextUploadingField
 from django.conf import settings
 from django.db import models
@@ -46,3 +48,21 @@ class Newsletter(UserGeneratedContentModel):
     organisation = models.ForeignKey(settings.A4_ORGANISATIONS_MODEL,
                                      null=True, blank=True,
                                      on_delete=models.CASCADE)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        if not update_fields or 'body' in update_fields:
+            self.body = self.replace_relative_media_urls(self.body)
+        return super().save(force_insert, force_update, using, update_fields)
+
+    @staticmethod
+    def replace_relative_media_urls(text):
+        if not settings.MEDIA_URL.startswith('/'):
+            # Replace only if MEDIA_URL is relative
+            return text
+
+        # Find every occurrence of the MEDIA_URL that is either following a
+        # whitespace, an equal sign or a quotation mark.
+        pattern = re.compile(r'([\s="\'])(%s)' % re.escape(settings.MEDIA_URL))
+        text = re.sub(pattern, r'\1%s\2' % settings.BASE_URL, text)
+        return text
