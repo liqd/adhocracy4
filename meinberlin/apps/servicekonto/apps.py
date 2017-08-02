4ee589cd8fd7e60606524e26a3b69e202242b75c
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-02 15:50:22 +0200

    Fix servicekonto import to be lazy on ready

--- meinberlin/apps/servicekonto/apps.py
+++ meinberlin/apps/servicekonto/apps.py
@@ -1,12 +1,11 @@
 from allauth.socialaccount import providers
 from django.apps import AppConfig
 
-from .provider import ServiceKontoProvider
-
 
 class Config(AppConfig):
     name = 'meinberlin.apps.servicekonto'
     label = 'meinberlin_servicekonto'
 
     def ready(self):
+        from .provider import ServiceKontoProvider
         providers.registry.register(ServiceKontoProvider)
