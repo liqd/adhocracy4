e57642b61109746baa484c6aee93da05c5d1e867
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-07 15:56:37 +0200

    Add labels to custom newsletter form fields

--- meinberlin/apps/newsletters/forms.py
+++ meinberlin/apps/newsletters/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.apps import apps
 from django.conf import settings
+from django.utils.translation import ugettext_lazy as _
 
 from adhocracy4.projects.models import Project
 
@@ -20,17 +21,22 @@ class NewsletterForm(forms.ModelForm):
         choices = [(value, string)
                    for value, string in models.RECEIVER_CHOICES
                    if value != models.PLATFORM or (user and user.is_superuser)]
-        self.fields['receivers'] = \
-            forms.ChoiceField(choices=choices, widget=forms.RadioSelect())
+        self.fields['receivers'] = forms.ChoiceField(
+            label=_('Receivers'),
+            choices=choices,
+            widget=forms.RadioSelect(),
+        )
 
         project_qs = Project.objects
         if organisation:
             project_qs = Project.objects.filter(organisation=organisation.id)
 
         self.fields['project'] = forms.ModelChoiceField(
+            label=_('Project'),
             queryset=project_qs,
             required=False, empty_label=None)
 
         self.fields['organisation'] = forms.ModelChoiceField(
+            label=_('Organisation'),
             queryset=Organisation.objects,
             required=False, empty_label=None)
