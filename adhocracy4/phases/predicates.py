092b5d3890461aefaec9c38fdb4ebb8901f51b72
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-09 16:43:59 +0200

    Use module in phase predicates
    Features have to be checked in the context of the module.

--- adhocracy4/phases/predicates.py
+++ adhocracy4/phases/predicates.py
@@ -1,30 +1,30 @@
 import rules
 
 
-def has_feature_active(project, model, feature):
-    if not project.active_phase:
+def has_feature_active(module, model, feature):
+    if not module.active_phase:
         return False
     else:
-        return project.active_phase.has_feature(feature, model)
+        return module.active_phase.has_feature(feature, model)
 
 
 @rules.predicate
 def phase_allows_change(user, item):
-    return has_feature_active(item.project, item.__class__, 'crud')
+    return has_feature_active(item.module, item.__class__, 'crud')
 
 
 def phase_allows_add(item_class):
     @rules.predicate
     def _add_predicate(user, module):
-        return has_feature_active(module.project, item_class, 'crud')
+        return has_feature_active(module, item_class, 'crud')
     return _add_predicate
 
 
 @rules.predicate
 def phase_allows_comment(user, item):
-    return has_feature_active(item.project, item.__class__, 'comment')
+    return has_feature_active(item.module, item.__class__, 'comment')
 
 
 @rules.predicate
 def phase_allows_rate(user, item):
-    return has_feature_active(item.project, item.__class__, 'rate')
+    return has_feature_active(item.module, item.__class__, 'rate')
