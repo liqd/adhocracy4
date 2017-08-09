dce37e7dea514f45393c21ca6a7ff0cee43656ff
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-09 16:59:31 +0200

    Adapt Module model
    Properties are copied from Project

--- tests/modules/test_module_models.py
+++ tests/modules/test_module_models.py
@@ -1,13 +1,16 @@
 from datetime import timedelta
+from dateutil.parser import parse
 import pytest
-
-from tests.apps.questions import models as q_models
+from freezegun import freeze_time
 
 
 @pytest.mark.django_db
-def test_has_feature(phase):
+def test_active_phase(phase):
     module = phase.module
-    assert module.has_feature('crud', q_models.Question)
+    with freeze_time(phase.start_date):
+        assert module.active_phase == phase
+    with freeze_time(phase.start_date - timedelta(days=1)):
+        assert module.active_phase is None
 
 
 @pytest.mark.django_db
@@ -16,3 +19,33 @@ def test_first_phase_start_date(phase, phase_factory):
     first_phase = phase_factory(
         module=module, start_date=phase.start_date - timedelta(days=1))
     assert module.first_phase_start_date == first_phase.start_date
+
+
+@pytest.mark.django_db
+def test_phases(phase, phase_factory):
+    module = phase.module
+    phase2 = phase_factory(module=module)
+    assert list(module.phases) == [phase, phase2]
+
+
+@pytest.mark.django_db
+def test_last_active_phase(module, phase_factory):
+    phase1 = phase_factory(
+        module=module,
+        start_date=parse('2013-01-01 18:00:00 UTC'),
+        end_date=parse('2013-01-02 18:00:00 UTC'),
+    )
+    phase2 = phase_factory(
+        module=module,
+        start_date=parse('2013-02-01 18:00:00 UTC'),
+        end_date=parse('2013-02-02 18:00:00 UTC'),
+    )
+
+    with freeze_time(phase1.start_date - timedelta(minutes=1)):
+        assert module.last_active_phase is None
+    with freeze_time(phase1.start_date):
+        assert module.last_active_phase == phase1
+    with freeze_time(phase1.end_date):
+        assert module.last_active_phase == phase1
+    with freeze_time(phase2.end_date):
+        assert module.last_active_phase == phase2
