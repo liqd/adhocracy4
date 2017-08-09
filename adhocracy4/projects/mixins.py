26292d4f2ed8384a0fc0a2fb7f86aaa277ba1e1a
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-09 17:09:38 +0200

    Pass module in addition to project to ProjectMixin/project detail views
    The module that is rendered on the project detail page is passed to the
    active phases view in addition to the project.
    This is required as with multi modules it is not known which module of a
    project is currently active.
    The module may be None if no phase has been started yet.
    
    This should be non breaking as the counterpart to the ProjectMixin is
    always the PhaseDispatchMixin.

--- adhocracy4/projects/mixins.py
+++ adhocracy4/projects/mixins.py
@@ -1,21 +1,30 @@
+from django.utils.functional import cached_property
 from django.views import generic
 
 
 class PhaseDispatchMixin(generic.DetailView):
+
+    @cached_property
+    def project(self):
+        return self.get_object()
+
+    @cached_property
+    def module(self):
+        return self.project.last_active_module
+
     def dispatch(self, request, *args, **kwargs):
-        kwargs['project'] = self.get_object()
+        # Choose the appropriate view for the current active phase.
+        kwargs['project'] = self.project
+        kwargs['module'] = self.module
+
         return self._view_by_phase()(request, *args, **kwargs)
 
     def _view_by_phase(self):
         """
         Choose the appropriate view for the current active phase.
         """
-        project = self.get_object()
-
-        if project.active_phase:
-            return project.active_phase.view.as_view()
-        elif project.past_phases:
-            return project.past_phases[0].view.as_view()
+        if self.module and self.module.last_active_phase:
+            return self.module.last_active_phase.view.as_view()
         else:
             return super().dispatch
 
@@ -23,7 +32,9 @@ class PhaseDispatchMixin(generic.DetailView):
 class ProjectMixin(generic.base.ContextMixin):
     def dispatch(self, *args, **kwargs):
         self.project = kwargs['project']
-        self.phase = self.project.active_phase or self.project.past_phases[0]
-        self.module = self.phase.module if self.phase else None
+        self.module = kwargs['module']
+        self.phase = self.module.last_active_phase if self.module else None
+        # Workaround for filters
         self.request.module = self.module
+
         return super(ProjectMixin, self).dispatch(*args, **kwargs)
