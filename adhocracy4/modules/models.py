2872a87884cbe5e20d93c7c929e7e31705cc2710
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-09 16:59:31 +0200

    Adapt Module model
    Properties are copied from Project

--- adhocracy4/modules/models.py
+++ adhocracy4/modules/models.py
@@ -13,6 +13,9 @@ class Module(models.Model):
     project = models.ForeignKey(
         project_models.Project, on_delete=models.CASCADE)
 
+    class Meta:
+        ordering = ['weight']
+
     def __str__(self):
         return "{} ({})".format(self.project, self.weight)
 
@@ -23,6 +26,28 @@ class Module(models.Model):
         for setting in settingslist:
             if hasattr(self, setting):
                 return getattr(self, setting)
+
+    @property
+    def active_phase(self):
+        return self.phase_set \
+            .active_phases() \
+            .first()
+
+    @property
+    def phases(self):
+        return self.phase_set.all()
+
+    @property
+    def future_phases(self):
+        return self.phase_set.future_phases()
+
+    @property
+    def past_phases(self):
+        return self.phase_set.past_phases()
+
+    @property
+    def last_active_phase(self):
+        return self.active_phase or self.past_phases.first()
 
     @property
     def first_phase_start_date(self):
