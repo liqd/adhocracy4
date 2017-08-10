ca4e7bc9265e82c9c5f2a103510afaf7b01927b9
vellip <vellerphilipp@googlemail.com>
2017-08-10 09:51:48 +0200

    use native method instead of moment

--- adhocracy4/comments/static/comments/Comment.jsx
+++ adhocracy4/comments/static/comments/Comment.jsx
@@ -8,10 +8,13 @@ var CommentManageDropdown = require('./CommentManageDropdown')
 
 var React = require('react')
 var django = require('django')
-var moment = require('moment')
 
 var safeHtml = function (text) {
   return { __html: text }
+}
+
+var localeDate = function (dateStr) {
+  return new Date(dateStr).toLocaleString(document.documentElement.lang)
 }
 
 var Comment = React.createClass({
@@ -102,9 +105,9 @@ var Comment = React.createClass({
     let CommentList = require('./CommentList')
     let lastDate
     if (this.props.modified === null && !this.props.is_deleted) {
-      lastDate = moment(this.props.created).format('lll')
+      lastDate = localeDate(this.props.created)
     } else if (!this.props.is_deleted) {
-      lastDate = django.gettext('Latest edit on') + ' ' + moment(this.props.modified).format('lll')
+      lastDate = django.gettext('Latest edit on') + ' ' + localeDate(this.props.modified)
     }
 
     let moderatorLabel
