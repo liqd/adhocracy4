b87f320bd2057e935dd4a9fd9fbf0189d9106794
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-14 15:51:32 +0200

    Add basic create project form
    A project is created with basic information (name, desc, img).
    Afterwards it may be edited from the advanced dashboard2.0.
    This commit introduces the basic create form and a simple edit form.
    Futhermore it introduces a4dashboard namespaced urls.

--- meinberlin/apps/dashboard2/views.py
+++ meinberlin/apps/dashboard2/views.py
@@ -1,17 +1,26 @@
 from django.contrib.auth import get_user_model
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404
 from django.utils import functional
+from django.utils.translation import ugettext_lazy as _
 from django.views import generic
 
+from adhocracy4.modules import models as module_models
+from adhocracy4.phases import models as phase_models
 from adhocracy4.projects import models as project_models
 from adhocracy4.rules import mixins as rules_mixins
 from meinberlin.apps.organisations.models import Organisation
 
+from . import blueprints
+from . import forms
+from . import mixins
+
 User = get_user_model()
 
 
-class DashboardProjectListView(rules_mixins.PermissionRequiredMixin,
-                               generic.ListView):
+class ProjectListView(rules_mixins.PermissionRequiredMixin,
+                      generic.ListView):
     model = project_models.Project
     paginate_by = 12
     template_name = 'meinberlin_dashboard2/project_list.html'
@@ -30,3 +39,73 @@ class DashboardProjectListView(rules_mixins.PermissionRequiredMixin,
         return super().get_queryset().filter(
             organisation=self.organisation
         )
+
+
+class BlueprintListView(mixins.DashboardBaseMixin,
+                        generic.TemplateView):
+    blueprints = blueprints.blueprints
+    template_name = 'meinberlin_dashboard2/blueprint_list.html'
+    permission_required = 'a4projects.add_project'
+    menu_item = 'project'
+
+
+class ProjectCreateView(mixins.DashboardBaseMixin,
+                        mixins.BlueprintMixin,
+                        generic.CreateView,
+                        SuccessMessageMixin):
+    model = project_models.Project
+    form_class = forms.ProjectCreateForm
+    template_name = 'meinberlin_dashboard2/project_create_form.html'
+    permission_required = 'a4projects.add_project'
+    success_message = _('Project succesfully created.')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs['type'] = self.blueprint_key
+        kwargs['organisation'] = self.organisation
+        kwargs['creator'] = self.request.user
+        return kwargs
+
+    def get_success_url(self):
+        return reverse('a4dashboard:project-edit',
+                       kwargs={'project_slug': self.object.slug})
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+
+        # FIXME: maybe replace by dashboard signals
+        self._create_modules_and_phases(self.object)
+
+        return response
+
+    def _create_modules_and_phases(self, project):
+        module = module_models.Module(
+            name=project.slug + '_module',
+            weight=1,
+            project=project,
+        )
+        module.save()
+        self._create_phases(module, self.blueprint.content)
+
+    def _create_phases(self, module, blueprint_phases):
+        for phase_content in blueprint_phases:
+            phase = phase_models.Phase(
+                type=phase_content.identifier,
+                name=phase_content.name,
+                description=phase_content.description,
+                weight=phase_content.weight,
+                module=module,
+            )
+            phase.save()
+
+
+class ProjectUpdateView(mixins.DashboardBaseMixin,
+                        mixins.BlueprintMixin,
+                        generic.UpdateView,
+                        SuccessMessageMixin):
+    model = project_models.Project
+    slug_url_kwarg = 'project_slug'
+    form_class = forms.ProjectUpdateForm
+    template_name = 'meinberlin_dashboard2/project_update_form.html'
+    permission_required = 'a4projects.add_project'
+    success_message = _('Project succesfully created.')
