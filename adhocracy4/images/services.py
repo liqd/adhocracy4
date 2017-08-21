8b3e454e726270cea4ec8b7b00e2e08780322071
mkind <sascha.zinke@splone.com>
2017-08-21 17:26:04 +0200

    fixing issue https://github.com/liqd/a4-opin/issues/879

--- adhocracy4/images/services.py
+++ adhocracy4/images/services.py
@@ -3,6 +3,7 @@ from easy_thumbnails.files import get_thumbnailer
 
 def delete_images(imagefields):
     for imagefield in imagefields:
+        if not imagefield: continue
         thumbnailer = get_thumbnailer(imagefield)
         thumbnailer.delete_thumbnails()
         if imagefield.name:
