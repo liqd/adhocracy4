42566f3e9a5f972686bd929306b15070fa2b18b5
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-21 15:50:25 +0200

    Add old dashboard backwards compatibility

--- meinberlin/apps/offlineevents/urls.py
+++ meinberlin/apps/offlineevents/urls.py
@@ -1,8 +1,20 @@
 from django.conf.urls import url
 
 from . import views
+from . import views_old
 
 urlpatterns = [
     url(r'^(?P<slug>[-\w_]+)/$',
         views.OfflineEventDetailView.as_view(), name='offlineevent-detail'),
+    url(r'^projects/(?P<project_slug>[-\w_]+)/$',
+        views_old.OfflineEventListView.as_view(), name='offlineevent-list'),
+    url(r'^create/project/(?P<project_slug>[-\w_]+)/$',
+        views_old.OfflineEventCreateView.as_view(),
+        name='offlineevent-create'),
+    url(r'^(?P<slug>[-\w_]+)/update/$',
+        views_old.OfflineEventUpdateView.as_view(),
+        name='offlineevent-update'),
+    url(r'^(?P<slug>[-\w_]+)/delete/$',
+        views_old.OfflineEventDeleteView.as_view(),
+        name='offlineevent-delete'),
 ]
