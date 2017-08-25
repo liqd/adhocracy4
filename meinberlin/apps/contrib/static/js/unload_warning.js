420ef37223b24f686d8ae2fac82c213ba666e80c
vellip <vellerphilipp@googlemail.com>
2017-08-25 09:30:25 +0200

    add string

--- meinberlin/apps/contrib/static/js/unload_warning.js
+++ meinberlin/apps/contrib/static/js/unload_warning.js
@@ -1,11 +1,13 @@
-/* global $ CKEDITOR */
+/* global $ CKEDITOR django */
 
 $(function () {
   var submitted = false
   var changeHandler = function () {
-    $(window).on('beforeunload', function () {
+    $(window).on('beforeunload', function (e) {
       if (!submitted) {
-        return false
+        var string = django.gettext('If you leave this page changes you made will not be saved.')
+        e.returnValue = string
+        return string
       }
     })
   }
