1384779ca84ec66c4a20e56de4faa81da0115ae9
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-29 16:43:03 +0200

    Add export module component
    Currently the component always returns the first export found. In the
    future different exports for a single module may be possible, but this
    would require a intermediate page to select the desired export.

---
+++ meinberlin/apps/exports/dashboard.py
@@ -0,0 +1,37 @@
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from meinberlin.apps.dashboard2 import DashboardComponent
+from meinberlin.apps.dashboard2 import components
+
+from . import get_exports
+from . import views
+
+
+class ExportModuleComponent(DashboardComponent):
+    identifier = 'module_export'
+    weight = 50
+
+    def get_menu_label(self, module):
+        if get_exports(module):
+            return _('Export')
+        return ''
+
+    def get_progress(self, module):
+        return 0, 0
+
+    def get_base_url(self, module):
+        return reverse('a4dashboard:export-module', kwargs={
+            'module_slug': module.slug,
+            'export_id': 0,
+        })
+
+    def get_urls(self):
+        return [
+            (r'^modules/(?P<module_slug>[-\w_]+)/export/(?P<export_id>\d+)/$',
+             views.ExportModuleDispatcher.as_view(),
+             'export-module'),
+        ]
+
+
+components.register_module(ExportModuleComponent())
