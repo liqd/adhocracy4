4c84809e378497257d8eac8e8ed08715cf6a5e31
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-08-30 10:18:31 +0200

    Fix date label and enable hidden time label for screen readers

--- meinberlin/apps/datetimefield/widgets.py
+++ meinberlin/apps/datetimefield/widgets.py
@@ -3,11 +3,14 @@ import datetime
 from django.contrib.staticfiles.storage import staticfiles_storage
 from django.forms import widgets as form_widgets
 from django.template.loader import render_to_string
-from django.utils import formats
 from django.utils.timezone import localtime
 
 
 class DateTimeInput(form_widgets.SplitDateTimeWidget):
+    def __init__(self, time_label='', *args, **kwargs):
+        self.time_label = time_label
+        super().__init__(*args, **kwargs)
+
     class Media:
         js = (
             staticfiles_storage.url('datepicker.js'),
@@ -20,14 +23,14 @@ class DateTimeInput(form_widgets.SplitDateTimeWidget):
         date_attrs = self.build_attrs(attrs)
         date_attrs.update({
             'class': 'datepicker',
-            'placeholder': formats.localize_input(datetime.date.today()),
-            'id': name + '_date'
+            'placeholder': self.widgets[0].format_value(datetime.date.today()),
+            'id': attrs['id'] + '_date'
         })
         time_attrs = self.build_attrs(attrs)
         time_attrs.update({
             'class': 'timepicker',
-            'placeholder': '00:00',
-            'id': name + '_time'
+            'placeholder': self.widgets[1].format_value(datetime.time(0, 0)),
+            'id': attrs['id'] + '_time'
         })
 
         if isinstance(value, datetime.datetime):
@@ -37,7 +40,7 @@ class DateTimeInput(form_widgets.SplitDateTimeWidget):
         else:
             # value's just a list in case of an error
             date = value[0] if value else None
-            time = value[1] if value else '00:00'
+            time = value[1] if value else None
 
         return render_to_string(
             'meinberlin_datetimefield/datetime_input.html', {
@@ -50,5 +53,14 @@ class DateTimeInput(form_widgets.SplitDateTimeWidget):
                     name + '_1',
                     time,
                     time_attrs
-                )
+                ),
+                'time_label': {
+                    'label': self.time_label,
+                    'id_for_label': attrs['id'] + '_time'
+                },
             })
+
+    def id_for_label(self, id_):
+        if id_:
+            id_ += '_date'
+        return id_
