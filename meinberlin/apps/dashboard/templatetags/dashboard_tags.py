d0fd895d30ff755d84f31c8ddd5cce8b44517a9c
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-09-06 10:17:23 +0200

    Revert "Use blueprint registry"
    This reverts commit 31274340e0b6f0789dfeeaee7f027426214c0a5b.

--- meinberlin/apps/dashboard/templatetags/dashboard_tags.py
+++ meinberlin/apps/dashboard/templatetags/dashboard_tags.py
@@ -1,13 +1,13 @@
 from django import template
 
-from meinberlin.apps.dashboard2 import blueprints
+from meinberlin.apps.dashboard2.blueprints import blueprints
 
 register = template.Library()
 
 
 @register.filter
 def get_blueprint_title(key):
-    for k, blueprint in blueprints.blueprints:
+    for k, blueprint in blueprints:
         if k == key:
             return blueprint.title
     return key
