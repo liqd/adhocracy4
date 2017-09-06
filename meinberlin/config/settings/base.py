0529d98af1b2836c982cf181db79164dabcb1cbb
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-09-06 10:48:42 +0200

    make blueprints configurable

--- meinberlin/config/settings/base.py
+++ meinberlin/config/settings/base.py
@@ -371,6 +371,7 @@ A4_MAP_BOUNDING_BOX = ([[52.3517, 13.8229], [52.6839, 12.9543]])
 
 A4_DASHBOARD = {
     'PROJECT_DASHBOARD_CLASS': 'meinberlin.apps.dashboard.TypedProjectDashboard',
+    'BLUEPRINTS': 'meinberlin.apps.dashboard.blueprints.blueprints'
 }
 
 CONTACT_EMAIL = 'support-berlin@liqd.net'
