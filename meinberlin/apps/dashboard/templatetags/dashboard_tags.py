94edf459b1c01bf7cdfdd25b7b5543d60cc68199
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-09-18 13:38:08 +0200

    Remove potential circular import
    `get_blueprints()` dashboard2/blueprints.py is importing the module from
    the settings (in our case dashboard/blueprints.py) which is importing
    `ProjectBlueprint` from dashboard2/blueprints.py
    
    ```
    dashboard2/blueprints.py --[ blueprints = _get_blueprints() ]-->
    dashboard/blueprints.py --[ from meinberlin.apps.dashboard2.blueprints
    import ProjectBlueprint ]--> dasboard2/blueprints.py
    ```

--- meinberlin/apps/dashboard/templatetags/dashboard_tags.py
+++ meinberlin/apps/dashboard/templatetags/dashboard_tags.py
@@ -1,13 +1,13 @@
 from django import template
 
-from meinberlin.apps.dashboard2.blueprints import blueprints
+from meinberlin.apps.dashboard2.blueprints import get_blueprints
 
 register = template.Library()
 
 
 @register.filter
 def get_blueprint_title(key):
-    for k, blueprint in blueprints:
+    for k, blueprint in get_blueprints():
         if k == key:
             return blueprint.title
     return key
