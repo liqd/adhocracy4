138aa2a57655206831b24f83499019bec4c43c78
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-09-28 17:01:31 +0200

    refactor project list queryset

--- meinberlin/apps/projects/views.py
+++ meinberlin/apps/projects/views.py
@@ -114,12 +114,20 @@ class ProjectListView(filter_views.FilteredListView):
     filter_set = ProjectFilterSet
 
     def get_queryset(self):
+        # FIXME: has to be in sync with a4projects.view_project and should
+        #        be implemented on the ProjectQueryManager in core
+
+        q_false = Q(pk=None)
+        is_superuser = ~q_false if self.request.user.is_superuser else q_false
+
         return super().get_queryset().filter(
-            Q(is_draft=False, is_public=False,
-              participants__pk=self.request.user.pk) |
-            Q(is_draft=False, is_public=False,
-              organisation__initiators__pk=self.request.user.pk) |
-            Q(is_draft=False, is_public=True)
+            Q(is_draft=False) & (
+                Q(is_public=True) |
+                is_superuser |
+                Q(participants__pk=self.request.user.pk) |
+                Q(organisation__initiators__pk=self.request.user.pk) |
+                Q(moderators__pk=self.request.user.pk)
+            )
         ).distinct()
 
 
