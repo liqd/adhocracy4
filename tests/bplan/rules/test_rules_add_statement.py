8f33de610547306cb1a64d3f1ef8756ec969db17
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-10-04 12:08:23 +0200

    fix tests

--- tests/bplan/rules/test_rules_add_statement.py
+++ tests/bplan/rules/test_rules_add_statement.py
@@ -2,6 +2,8 @@ import pytest
 import rules
 
 from meinberlin.apps.bplan import phases
+from tests.helpers import freeze_phase
+from tests.helpers import freeze_post_phase
 from tests.helpers import freeze_pre_phase
 from tests.helpers import setup_phase
 from tests.helpers import setup_users
@@ -14,14 +16,42 @@ def test_perm_exists():
 
 
 @pytest.mark.django_db
-def test_rule(phase_factory, user):
+def test_pre_phase(phase_factory, user):
     phase, module, project, _ = setup_phase(phase_factory, None,
                                             phases.StatementPhase)
     anonymous, moderator, initiator = setup_users(project)
 
     assert project.is_public
     with freeze_pre_phase(phase):
+        assert not rules.has_perm(perm_name, anonymous, module)
+        assert not rules.has_perm(perm_name, user, module)
+        assert not rules.has_perm(perm_name, moderator, module)
+        assert not rules.has_perm(perm_name, initiator, module)
+
+
+@pytest.mark.django_db
+def test_active(phase_factory, user):
+    phase, module, project, _ = setup_phase(phase_factory, None,
+                                            phases.StatementPhase)
+    anonymous, moderator, initiator = setup_users(project)
+
+    assert project.is_public
+    with freeze_phase(phase):
         assert rules.has_perm(perm_name, anonymous, module)
         assert rules.has_perm(perm_name, user, module)
         assert rules.has_perm(perm_name, moderator, module)
         assert rules.has_perm(perm_name, initiator, module)
+
+
+@pytest.mark.django_db
+def test_post_phase(phase_factory, user):
+    phase, module, project, _ = setup_phase(phase_factory, None,
+                                            phases.StatementPhase)
+    anonymous, moderator, initiator = setup_users(project)
+
+    assert project.is_public
+    with freeze_post_phase(phase):
+        assert not rules.has_perm(perm_name, anonymous, module)
+        assert not rules.has_perm(perm_name, user, module)
+        assert not rules.has_perm(perm_name, moderator, module)
+        assert not rules.has_perm(perm_name, initiator, module)
