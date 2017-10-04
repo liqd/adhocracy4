liqd/adhocracy4

commit 425be59af8e18d7da7f65bba9b21fce45b2c221b

use native classes

adhocracy4/comments/static/comments/CommentEditForm.jsx
@@ -1,14 +1,16 @@
 var React = require('react')
 var django = require('django')
 
-var CommentEditForm = React.createClass({
-  getInitialState: function () {
-    return {comment: this.props.comment}
-  },
-  handleTextChange: function (e) {
+class CommentEditForm extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {comment: this.props.comment}
+  }
+  handleTextChange (e) {
     this.setState({comment: e.target.value})
-  },
-  handleSubmit: function (e) {
+  }
+  handleSubmit (e) {
     e.preventDefault()
     var comment = this.state.comment.trim()
     if (!comment) {
@@ -21,14 +23,14 @@ var CommentEditForm = React.createClass({
         contentTypeId: this.props.subjectType
       }
     })
-  },
-  render: function () {
+  }
+  render () {
     return (
-      <form className="general-form" onSubmit={this.handleSubmit}>
+      <form className="general-form" onSubmit={this.handleSubmit.bind(this)}>
         <div className="form-group">
           <textarea rows={this.props.rows} className="form-control"
             placeholder={django.gettext('Your comment here')}
-            onChange={this.handleTextChange} required="required" defaultValue={this.state.comment} />
+            onChange={this.handleTextChange.bind(this)} required="required" defaultValue={this.state.comment.bind(this)} />
         </div>
         <input type="submit" value={django.gettext('post')} className="submit-button" />
         <input type="submit" value={django.gettext('cancel')} className="cancel-button"
@@ -36,7 +38,7 @@ var CommentEditForm = React.createClass({
       </form>
     )
   }
-})
+}
 
 CommentEditForm.contextTypes = {
   isAuthenticated: React.PropTypes.bool

adhocracy4/comments/static/comments/Comment.jsx
@@ -14,37 +14,39 @@ var safeHtml = function (text) {
   return { __html: text }
 }
 
-var Comment = React.createClass({
-  getInitialState: function () {
-    return {
+class Comment extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
       edit: false,
       showChildComments: false
     }
-  },
+  }
 
-  toggleEdit: function (e) {
+  toggleEdit (e) {
     if (e) {
       e.preventDefault()
     }
     var newEdit = !this.state.edit
     this.setState({edit: newEdit})
-  },
+  }
 
-  showComments: function (e) {
+  showComments (e) {
     e.preventDefault()
     var newShowChildComment = !this.state.showChildComments
     this.setState({showChildComments: newShowChildComment})
-  },
+  }
 
-  allowForm: function () {
+  allowForm () {
     return !this.props.isReadOnly && this.props.content_type !== this.context.comments_contenttype
-  },
+  }
 
-  isOwner: function () {
+  isOwner () {
     return this.props.user_name === this.context.user_name
-  },
+  }
 
-  renderRatingBox: function () {
+  renderRatingBox () {
     if (!this.props.is_deleted) {
       return (
         <RatingBox
@@ -59,9 +61,9 @@ var Comment = React.createClass({
         />
       )
     }
-  },
+  }
 
-  renderComment: function () {
+  renderComment () {
     let comment
     if (this.state.edit) {
       comment = (
@@ -70,7 +72,7 @@ var Comment = React.createClass({
           subjectId={this.props.object_pk}
           comment={this.props.children}
           rows="5"
-          handleCancel={this.toggleEdit}
+          handleCancel={this.toggleEdit.bind(this)}
           onCommentSubmit={newComment => {
             this.props.handleCommentModify(newComment, this.props.index, this.props.parentIndex)
             this.state.edit = false
@@ -81,9 +83,9 @@ var Comment = React.createClass({
       comment = <div className="comment-text" dangerouslySetInnerHTML={safeHtml(this.props.children)} />
     }
     return comment
-  },
+  }
 
-  renderDeleteModal: function () {
+  renderDeleteModal () {
     if (this.isOwner() || this.context.isModerator) {
       return (
         <Modal
@@ -96,9 +98,9 @@ var Comment = React.createClass({
         />
       )
     }
-  },
+  }
 
-  render: function () {
+  render () {
     let CommentList = require('./CommentList')
     let lastDate
     if (this.props.modified === null && !this.props.is_deleted) {
@@ -136,13 +138,13 @@ var Comment = React.createClass({
             {this.context.isAuthenticated && !this.props.is_deleted &&
               <CommentManageDropdown
                 id={this.props.id}
-                toggleEdit={this.toggleEdit}
+                toggleEdit={this.toggleEdit.bind(this)}
                 renderModeratorOptions={(this.isOwner() || this.context.isModerator) && !this.props.isReadOnly}
               />
             }
           </nav>
         </div>
-        <CommentReplyBar allowForm={this.allowForm()} showComments={this.showComments}
+        <CommentReplyBar allowForm={this.allowForm()} showComments={this.showComments.bind(this)}
           childCommentsLength={this.props.child_comments ? this.props.child_comments.length : 0} />
         {this.state.showChildComments
           ? <div className="comment-child-list">
@@ -166,7 +168,7 @@ var Comment = React.createClass({
       </div>
     )
   }
-})
+}
 
 Comment.contextTypes = {
   comments_contenttype: React.PropTypes.number,

adhocracy4/comments/static/comments/CommentBox.jsx
@@ -6,8 +6,15 @@ var React = require('react')
 var update = require('immutability-helper')
 var django = require('django')
 
-let CommentBox = React.createClass({
-  updateStateComment: function (index, parentIndex, updatedComment) {
+class CommentBox extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      comments: this.props.comments
+    }
+  }
+  updateStateComment (index, parentIndex, updatedComment) {
     var comments = this.state.comments
     var diff = {}
     if (typeof parentIndex !== 'undefined') {
@@ -18,8 +25,8 @@ let CommentBox = React.createClass({
     }
     comments = update(comments, diff)
     this.setState({ comments: comments })
-  },
-  handleCommentSubmit: function (comment, parentIndex) {
+  }
+  handleCommentSubmit (comment, parentIndex) {
     api.comments.add(comment)
       .done(function (comment) {
         var comments = this.state.comments
@@ -33,8 +40,8 @@ let CommentBox = React.createClass({
           comments: update(comments, diff)
         })
       }.bind(this))
-  },
-  handleCommentModify: function (modifiedComment, index, parentIndex) {
+  }
+  handleCommentModify (modifiedComment, index, parentIndex) {
     var comments = this.state.comments
     var comment = comments[index]
     if (typeof parentIndex !== 'undefined') {
@@ -43,8 +50,8 @@ let CommentBox = React.createClass({
 
     api.comments.change(modifiedComment, comment.id)
       .done(this.updateStateComment.bind(this, index, parentIndex))
-  },
-  handleCommentDelete: function (index, parentIndex) {
+  }
+  handleCommentDelete (index, parentIndex) {
     var comments = this.state.comments
     var comment = comments[index]
     if (typeof parentIndex !== 'undefined') {
@@ -59,38 +66,33 @@ let CommentBox = React.createClass({
     }
     api.comments.delete(data, comment.id)
       .done(this.updateStateComment.bind(this, index, parentIndex))
-  },
-  getInitialState: function () {
-    return {
-      comments: this.props.comments
-    }
-  },
-  getChildContext: function () {
+  }
+  getChildContext () {
     return {
       isAuthenticated: this.props.isAuthenticated,
       isModerator: this.props.isModerator,
       comments_contenttype: this.props.comments_contenttype,
       user_name: this.props.user_name
     }
-  },
-  render: function () {
+  }
+  render () {
     return (
       <div>
         <div className="black-divider">{this.state.comments.length + ' ' + django.ngettext('comment', 'comments', this.state.comments.length)}</div>
         <div className="comment-box">
           <CommentForm subjectType={this.props.subjectType} subjectId={this.props.subjectId}
-            onCommentSubmit={this.handleCommentSubmit} placeholder={django.gettext('Your comment here')}
+            onCommentSubmit={this.handleCommentSubmit.bind(this)} placeholder={django.gettext('Your comment here')}
             rows="5" isReadOnly={this.props.isReadOnly} />
           <div className="comment-list">
-            <CommentList comments={this.state.comments} handleCommentDelete={this.handleCommentDelete}
-              handleCommentSubmit={this.handleCommentSubmit} handleCommentModify={this.handleCommentModify}
+            <CommentList comments={this.state.comments} handleCommentDelete={this.handleCommentDelete.bind(this)}
+              handleCommentSubmit={this.handleCommentSubmit.bind(this)} handleCommentModify={this.handleCommentModify.bind(this)}
               isReadOnly={this.props.isReadOnly} />
           </div>
         </div>
       </div>
     )
   }
-})
+}
 
 CommentBox.childContextTypes = {
   isAuthenticated: React.PropTypes.bool,

adhocracy4/comments/static/comments/CommentForm.jsx
@@ -3,14 +3,16 @@ var config = require('../../../static/config')
 var React = require('react')
 var django = require('django')
 
-let CommentForm = React.createClass({
-  getInitialState: function () {
-    return {comment: ''}
-  },
-  handleTextChange: function (e) {
+class CommentForm extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {comment: ''}
+  }
+  handleTextChange (e) {
     this.setState({comment: e.target.value})
-  },
-  handleSubmit: function (e) {
+  }
+  handleSubmit (e) {
     e.preventDefault()
     var comment = this.state.comment.trim()
     if (!comment) {
@@ -24,15 +26,15 @@ let CommentForm = React.createClass({
       }
     }, this.props.parentIndex)
     this.setState({comment: ''})
-  },
-  render: function () {
+  }
+  render () {
     if (this.context.isAuthenticated && !this.props.isReadOnly) {
       return (
-        <form className="general-form" onSubmit={this.handleSubmit}>
+        <form className="general-form" onSubmit={this.handleSubmit.bind(this)}>
           <div className="form-group">
             <textarea rows={this.props.rows} className="form-control"
               placeholder={django.gettext('Your comment here')}
-              onChange={this.handleTextChange} required="required" value={this.state.comment} />
+              onChange={this.handleTextChange.bind(this)} required="required" value={this.state.comment} />
           </div>
           <input type="submit" value={django.gettext('post')} className="submit-button" />
         </form>
@@ -51,7 +53,7 @@ let CommentForm = React.createClass({
       )
     }
   }
-})
+}
 
 CommentForm.contextTypes = {
   isAuthenticated: React.PropTypes.bool

adhocracy4/follows/static/follows/FollowButton.jsx
@@ -2,14 +2,16 @@ var api = require('../../../static/api')
 var django = require('django')
 var React = require('react')
 
-var FollowButton = React.createClass({
-  getInitialState: function () {
-    return {
+class FollowButton extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
       followed: undefined,
       follows: 0
     }
-  },
-  toggleFollow: function () {
+  }
+  toggleFollow () {
     api.follow.change({ enabled: !this.state.followed }, this.props.project)
        .done((follow) => {
          this.setState({
@@ -17,8 +19,8 @@ var FollowButton = React.createClass({
            follows: follow.follows
          })
        })
-  },
-  componentDidMount: function () {
+  }
+  componentDidMount () {
     api.follow.get(this.props.project)
        .done((follow) => {
          this.setState({
@@ -33,11 +35,11 @@ var FollowButton = React.createClass({
            })
          }
        })
-  },
-  render: function () {
+  }
+  render () {
     return (
       <span className="btngroup btngroup-gray">
-        <button className="btn btn-sm btn-dark btn-primary" type="button" onClick={this.toggleFollow}>
+        <button className="btn btn-sm btn-dark btn-primary" type="button" onClick={this.toggleFollow.bind(this)}>
           <i className={this.state.followed ? 'fa fa-star' : 'fa fa-star-o'} aria-hidden="true" />
           &nbsp;{this.state.followed ? django.gettext('Unfollow') : django.gettext('Follow')}
         </button>
@@ -45,6 +47,6 @@ var FollowButton = React.createClass({
       </span>
     )
   }
-})
+}
 
 module.exports = FollowButton

adhocracy4/ratings/static/ratings/react_ratings.jsx
@@ -5,8 +5,19 @@ var React = require('react')
 var ReactDOM = require('react-dom')
 var classnames = require('classnames')
 
-var RatingBox = React.createClass({
-  handleRatingCreate: function (number) {
+class RatingBox extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      positiveRatings: this.props.positiveRatings,
+      negativeRatings: this.props.negativeRatings,
+      userHasRatingd: this.props.userRating !== null,
+      userRating: this.props.userRating,
+      userRatingId: this.props.userRatingId
+    }
+  }
+  handleRatingCreate (number) {
     api.rating.add({
       urlReplaces: {
         objectPk: this.props.objectId,
@@ -22,8 +33,8 @@ var RatingBox = React.createClass({
         userRatingId: data.id
       })
     }.bind(this))
-  },
-  handleRatingModify: function (number, id) {
+  }
+  handleRatingModify (number, id) {
     api.rating.change({
       urlReplaces: {
         objectPk: this.props.objectId,
@@ -38,11 +49,11 @@ var RatingBox = React.createClass({
           userRating: data.meta_info.user_rating_on_same_object_value
         })
       }.bind(this))
-  },
-  updateUserRating: function (data) {
+  }
+  updateUserRating (data) {
     this.state.ratings[this.state.userRatingIndex] = data
-  },
-  ratingUp: function (e) {
+  }
+  ratingUp (e) {
     e.preventDefault()
     if (this.props.authenticatedAs === null) {
       window.location.href = config.loginUrl
@@ -62,8 +73,8 @@ var RatingBox = React.createClass({
     } else {
       this.handleRatingCreate(1)
     }
-  },
-  ratingDown: function (e) {
+  }
+  ratingDown (e) {
     e.preventDefault()
     if (this.props.authenticatedAs === null) {
       window.location.href = config.loginUrl
@@ -83,20 +94,8 @@ var RatingBox = React.createClass({
     } else {
       this.handleRatingCreate(-1)
     }
-  },
-  getInitialState: function () {
-    return {
-      positiveRatings: this.props.positiveRatings,
-      negativeRatings: this.props.negativeRatings,
-      userHasRatingd: this.props.userRating !== null,
-      userRating: this.props.userRating,
-      userRatingId: this.props.userRatingId
-    }
-  },
-  componentDidMount: function () {
-    this.getInitialState()
-  },
-  render: function () {
+  }
+  render () {
     let getRatingClasses = ratingType => {
       let valueForRatingType = ratingType === 'up' ? 1 : -1
       return classnames(`rating-button rating-${ratingType}`, {
@@ -109,21 +108,21 @@ var RatingBox = React.createClass({
         <button
           className={getRatingClasses('up')}
           disabled={this.props.isReadOnly}
-          onClick={this.ratingUp}>
+          onClick={this.ratingUp.bind(this)}>
           <i className="fa fa-chevron-up" aria-hidden="true" />
           {this.state.positiveRatings}
         </button>
         <button
           className={getRatingClasses('down')}
           disabled={this.props.isReadOnly}
-          onClick={this.ratingDown}>
+          onClick={this.ratingDown.bind(this)}>
           <i className="fa fa-chevron-down" aria-hidden="true" />
           {this.state.negativeRatings}
         </button>
       </div>
     )
   }
-})
+}
 
 module.exports.RatingBox = RatingBox
 

adhocracy4/reports/static/reports/ReportModal.jsx
@@ -5,19 +5,21 @@ let $ = require('jquery')
 let React = require('react')
 let django = require('django')
 
-const ReportModal = React.createClass({
-  getInitialState: function () {
-    return {
+class ReportModal extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
       report: '',
       showSuccessMessage: false,
       showErrorMessage: false,
       showReportForm: true
     }
-  },
-  handleTextChange: function (e) {
+  }
+  handleTextChange (e) {
     this.setState({report: e.target.value})
-  },
-  resetModal: function () {
+  }
+  resetModal () {
     if (!$('#' + this.props.name).is(':visible')) {
       this.setState({
         report: '',
@@ -27,14 +29,14 @@ const ReportModal = React.createClass({
         errors: null
       })
     } else {
-      setTimeout(this.resetModal, 500)
+      setTimeout(this.resetModal.bind(this), 500)
     }
-  },
-  closeModal: function () {
+  }
+  closeModal () {
     $('#' + this.props.name).modal('hide')
     this.resetModal()
-  },
-  submitReport: function () {
+  }
+  submitReport () {
     api.report.submit({
       description: this.state.report,
       content_type: this.props.contentType,
@@ -48,8 +50,8 @@ const ReportModal = React.createClass({
           showErrorMessage: false
         })
       }.bind(this))
-  },
-  render: function () {
+  }
+  render () {
     let partials = {}
     if (this.state.showSuccessMessage) {
       partials.title = (<span><i className="fa fa-check" /> Thank you! We are taking care of it.</span>)
@@ -60,7 +62,7 @@ const ReportModal = React.createClass({
       partials.body = (
         <div className="form-group">
           <textarea rows="5" className="form-control report-message" value={this.state.report}
-            placeholder={django.gettext('Your message here')} onChange={this.handleTextChange} />
+            placeholder={django.gettext('Your message here')} onChange={this.handleTextChange.bind(this)} />
           {this.state.errors && <span className="help-block">{this.state.errors.description}</span>}
         </div>
       )
@@ -70,14 +72,14 @@ const ReportModal = React.createClass({
       <Modal
         abort={this.props.abort}
         name={this.props.name}
-        closeHandler={this.closeModal}
-        submitHandler={this.submitReport}
+        closeHandler={this.closeModal.bind(this)}
+        submitHandler={this.submitReport.bind(this)}
         action={django.gettext('Send Report')}
         partials={partials}
         dismissOnSubmit={false}
       />
     )
   }
-})
+}
 
 module.exports = ReportModal

commit df829eb070626b1c2a260dbc2f5fd91b907a975f

Fix comment editing
regression that was introduced in 425be59a

adhocracy4/comments/static/comments/CommentEditForm.jsx
@@ -31,7 +31,7 @@ class CommentEditForm extends React.Component {
         <div className="form-group">
           <textarea rows={this.props.rows} className="form-control"
             placeholder={django.gettext('Your comment here')}
-            onChange={this.handleTextChange.bind(this)} required="required" defaultValue={this.state.comment.bind(this)} />
+            onChange={this.handleTextChange.bind(this)} required="required" defaultValue={this.state.comment} />
         </div>
         <input type="submit" value={django.gettext('post')} className="submit-button" />
         <input type="submit" value={django.gettext('cancel')} className="cancel-button"