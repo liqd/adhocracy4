f5b656d3ff2b3c0c186320fce5ddb2da7d6bc0f1
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-10-11 11:06:46 +0200

    Use app label for the exports registry

--- meinberlin/apps/exports/__init__.py
+++ meinberlin/apps/exports/__init__.py
@@ -13,22 +13,16 @@ class ExportsRegistry:
 
     def register(self, description, cls):
         app_config = django_apps.get_containing_app_config(cls.__module__)
-        app_name = app_config.name
-        app_exports = self._registry.get(app_name, [])
+        app_label = app_config.label
+        app_exports = self._registry.get(app_label, [])
         app_exports.append((description, cls))
-        self._registry[app_name] = sorted(app_exports, key=lambda e: e[1])
+        self._registry[app_label] = sorted(app_exports, key=lambda e: e[1])
 
     def __getitem__(self, module):
-        phase_content = module.phases.first().content()
-        app_config = django_apps.get_app_config(phase_content.app)
-        return self._registry[app_config.name]
+        return self._registry[module.phases.first().content().app]
 
     def __contains__(self, module):
-        try:
-            self[module]
-            return True
-        except AttributeError:
-            return False
+        return module.phases.first().content().app in self._registry
 
 
 exports = ExportsRegistry()
