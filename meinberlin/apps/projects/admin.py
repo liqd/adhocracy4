9ae0cfe12c439155d74eb37c006686de0d8d6fee
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-10-17 17:12:20 +0200

    rm actions

--- meinberlin/apps/projects/admin.py
+++ meinberlin/apps/projects/admin.py
@@ -4,16 +4,9 @@ from adhocracy4.projects import models
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    actions = ('publish', 'archive')
     list_display = ('__str__', 'organisation', 'is_draft', 'is_archived')
     list_filter = ('is_draft', 'is_archived', 'organisation')
     raw_id_fields = ('moderators', 'participants')
-
-    def publish(modeladmin, request, queryset):
-        queryset.update(is_draft=False)
-
-    def archive(modeladmin, request, queryset):
-        queryset.update(is_archived=False)
 
 
 # Overwrite adhocracy4.projects.admin
