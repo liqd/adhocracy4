dcb5d93b304c6a52688aca31b2c3367e354e9333
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-10-17 17:12:27 +0200

    add search

--- meinberlin/apps/projects/admin.py
+++ meinberlin/apps/projects/admin.py
@@ -6,6 +6,7 @@ from adhocracy4.projects import models
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'organisation', 'is_draft', 'is_archived')
     list_filter = ('is_draft', 'is_archived', 'organisation')
+    search_fields = ('name',)
     raw_id_fields = ('moderators', 'participants')
 
 
