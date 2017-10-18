3f1e21e1d2a3d1418c19e454f77071686d21f7b9
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-10-18 10:35:45 +0200

    add external project

---
+++ meinberlin/apps/extprojects/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+
+from . import models
+
+
+@admin.register(models.ExternalProject)
+class ExternalProjectAdmin(admin.ModelAdmin):
+    fields = (
+        'name', 'url', 'description', 'tile_image', 'tile_image_copyright',
+        'is_archived'
+    )
+    list_display = ('__str__', 'organisation', 'is_draft', 'is_archived')
+    list_filter = ('is_draft', 'is_archived', 'organisation')
+    search_fields = ('name',)
+    date_hierarchy = 'created'
+
+    def get_queryset(self, request):
+        return models.ExternalProject.objects.filter(bplan=None)
