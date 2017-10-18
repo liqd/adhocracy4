4da6c21bc07b546967b4e849e09b94b79fa44b17
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-10-18 15:14:41 +0200

    extend admins with columns and filter

--- meinberlin/apps/documents/admin.py
+++ meinberlin/apps/documents/admin.py
@@ -6,9 +6,14 @@ from . import models
 @admin.register(models.Paragraph)
 class ParagraphAdmin(admin.ModelAdmin):
     list_filter = ('chapter',)
+    list_display = ('__str__', 'name', 'chapter', 'created')
     readonly_fields = ('creator',)
+    date_hierarchy = 'created'
 
 
 @admin.register(models.Chapter)
 class ChapterAdmin(admin.ModelAdmin):
+    list_filter = ('module__project__organisation', 'module__project')
+    list_display = ('__str__', 'name', 'created')
     readonly_fields = ('creator', )
+    date_hierarchy = 'created'
