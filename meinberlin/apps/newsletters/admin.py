ccd0ae55214070a8b927b85ada5725b85f59bd8a
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-10-19 11:23:25 +0200

    use dynamic project filter

--- meinberlin/apps/newsletters/admin.py
+++ meinberlin/apps/newsletters/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
 
+from adhocracy4.projects.admin import ProjectAdminFilter
+
 from . import models
 
 
 @admin.register(models.Newsletter)
 class NewsletterAdmin(admin.ModelAdmin):
     list_display = ('subject', 'sent', 'project', 'organisation')
-    list_filter = ('organisation', )
+    list_filter = (
+        'project__organisation',
+        'project__is_archived',
+        ProjectAdminFilter
+    )
     date_hierarchy = 'sent'
