62ce49c804e86d3fe3e66bbac5f7e6c69bb071ae
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-10-30 16:43:05 +0100

    bplan tests

--- tests/bplan/test_views_integration.py
+++ tests/bplan/test_views_integration.py
@@ -6,18 +6,19 @@ from tests.helpers import freeze_phase
 
 
 @pytest.mark.django_db
-def test_statement_form_view(client, user, phase_factory, bplan,
+def test_statement_form_view(client, phase_factory, bplan_factory,
                              module_factory):
+    bplan = bplan_factory(is_draft=False)
     module = module_factory(project=bplan)
     phase = phase_factory(phase_content=phases.StatementPhase(), module=module)
-    url = '/embed' + bplan.get_absolute_url()
+    url = bplan.get_absolute_url()
     with freeze_phase(phase):
-        client.login(username=user.email, password='password')
         statement = {
             'name': 'User',
             'email': 'user@foo.bar',
             'street_number': "Some Street 1",
-            'postal_code_city': "12345 City"
+            'postal_code_city': "12345 City",
+            'statement': 'Something...'
         }
         response = client.post(url, statement)
-        assert redirect_target(response) == 'idea-detail'
+        assert redirect_target(response) == 'statement-sent'
