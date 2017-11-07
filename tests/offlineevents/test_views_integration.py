6cc770e65e433b25b6c4518a70c6348c992d6772
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-11-07 18:13:58 +0100

    Fixt tests (clear caches)
    python lru_caches may be invalid when using a single python instance in
    which the database is reset between calls.
    
    lru_cache uses `hash` to identify objects and django models are
    returning the object.pk as the hash. As the database is reset for every
    test function each projects pk is 1 and the cache is invalid. It always
    returns the first created projects status.

--- tests/offlineevents/test_views_integration.py
+++ tests/offlineevents/test_views_integration.py
@@ -4,8 +4,8 @@ from tests.helpers import assert_template_response
 
 
 @pytest.mark.django_db
-def test_detail_view(client, offlineevent):
-    url = offlineevent.get_absolute_url()
+def test_detail_view(client, offline_event):
+    url = offline_event.get_absolute_url()
     response = client.get(url)
     assert_template_response(
         response, 'meinberlin_offlineevents/offlineevent_detail.html')
