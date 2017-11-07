69832cf3ed4a0ec0f38d4e58c22ae325c98e39f0
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-11-07 14:17:02 +0100

    Add adminlog app
    The app listens to the dashboard signals and creates localized database
    log entries for them.
    The log entries may be viewed in the django admin.

---
+++ meinberlin/apps/adminlog/models.py
@@ -0,0 +1,58 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+from django.utils.encoding import force_text
+from django.utils.translation import ugettext_lazy as _
+
+from adhocracy4.modules.models import Module
+from adhocracy4.projects.models import Project
+
+PROJECT_COMPONENT_UPDATED = 1
+MODULE_COMPONENT_UPDATED = 2
+PROJECT_CREATED = 3
+PROJECT_PUBLISHED = 4
+PROJECT_UNPUBLISHED = 5
+
+ACTION_CHOICES = (
+    (PROJECT_COMPONENT_UPDATED, _('Project updated')),
+    (MODULE_COMPONENT_UPDATED, _('Module updated')),
+    (PROJECT_CREATED, _('Project created')),
+    (PROJECT_PUBLISHED, _('Project published')),
+    (PROJECT_UNPUBLISHED, _('Project unpublished'))
+)
+
+
+class LogEntry(models.Model):
+    timestamp = models.DateTimeField(default=timezone.now,
+                                     editable=False,
+                                     verbose_name=_('action time'))
+    actor = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              models.SET_NULL,
+                              null=True,
+                              verbose_name=_('User'),
+                              related_name='+')
+    project = models.ForeignKey(Project,
+                                on_delete=models.CASCADE,
+                                verbose_name=_('Project'),
+                                related_name='+')
+    module = models.ForeignKey(Module,
+                               on_delete=models.CASCADE,
+                               null=True,
+                               verbose_name=_('Module'),
+                               related_name='+')
+    component_identifier = models.CharField(blank=True,
+                                            max_length=255)
+    message = models.TextField(verbose_name=_('Message'))
+    action = models.SmallIntegerField(choices=ACTION_CHOICES,
+                                      verbose_name=_('Action'))
+
+    class Meta:
+        verbose_name = _('log entry')
+        verbose_name_plural = _('log entries')
+        ordering = ('-timestamp',)
+
+    def __repr__(self):
+        return force_text(self.timestamp)
+
+    def __str__(self):
+        return self.message
--- meinberlin/config/settings/base.py
+++ meinberlin/config/settings/base.py
@@ -86,6 +86,7 @@ INSTALLED_APPS = (
     'meinberlin.apps.offlineevents.apps.Config',
     'meinberlin.apps.newsletters.apps.Config',
     'meinberlin.apps.plans.apps.Config',
+    'meinberlin.apps.adminlog.apps.Config',
 
     'meinberlin.apps.account.apps.Config',
     'meinberlin.apps.dashboard.apps.Config',
