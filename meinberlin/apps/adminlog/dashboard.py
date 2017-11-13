e81047e9ca3f37ca7dc644b0990afc467adc4826
Alexej Starschenko <alexej.starschenko@liqd.de>
2017-11-13 09:31:16 +0100

    project admin dashboard component

---
+++ meinberlin/apps/adminlog/dashboard.py
@@ -0,0 +1,34 @@
+from django.urls import reverse
+from django.utils.translation import ugettext_lazy as _
+
+from meinberlin.apps.dashboard2 import DashboardComponent
+from meinberlin.apps.dashboard2 import components
+
+from . import views
+
+
+class AdminLogComponent(DashboardComponent):
+    identifier = 'adminlog'
+    weight = 60
+    label = _('Log')
+
+    def is_effective(self, project):
+        return True
+
+    def get_progress(self, project):
+        return 0, 0
+
+    def get_base_url(self, project):
+        return reverse('a4dashboard:adminlog', kwargs={
+            'project_slug': project.slug,
+        })
+
+    def get_urls(self):
+        return [
+            (r'^projects/(?P<project_slug>[-\w_]+)/log/$',
+             views.AdminLogProjectDashboardView.as_view(),
+             'adminlog'),
+        ]
+
+
+components.register_project(AdminLogComponent())
---
+++ meinberlin/apps/adminlog/views.py
@@ -0,0 +1,22 @@
+from django.views import generic
+
+from meinberlin.apps.contrib.views import ProjectContextMixin
+from meinberlin.apps.dashboard2 import mixins
+
+from . import models
+
+
+class AdminLogProjectDashboardView(ProjectContextMixin,
+                                   mixins.DashboardBaseMixin,
+                                   mixins.DashboardComponentMixin,
+                                   generic.ListView):
+    model = models.LogEntry
+    template_name = 'meinberlin_adminlog/project_adminlog.html'
+    permission_required = 'a4projects.change_project'
+    paginate_by = 15
+
+    def get_queryset(self):
+        return super().get_queryset().filter(project=self.project)
+
+    def get_permission_object(self):
+        return self.project
