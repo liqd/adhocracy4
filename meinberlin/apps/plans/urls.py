7448e0e3d60b13a7cc10282aa4c73c2c0a04b5b2
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-11-21 12:31:03 +0100

    Add plans export

--- meinberlin/apps/plans/urls.py
+++ meinberlin/apps/plans/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     url(r'^(?P<slug>[-\w_]+)/$',
         views.PlanDetailView.as_view(), name='plan-detail'),
+    url('^export/all/$',
+        views.PlanExportView.as_view(), name='plan-export'),
     url('^$',
         views.PlanListView.as_view(), name='plan-list'),
 ]
