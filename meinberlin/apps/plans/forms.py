690f8fae57f75a08a66b9471e5623c30b8487a2d
Tobias Bengfort <tobias.bengfort@liqd.de>
2017-11-29 17:23:18 +0100

    add datalist to category

--- meinberlin/apps/plans/forms.py
+++ meinberlin/apps/plans/forms.py
@@ -1,10 +1,20 @@
 from django import forms
 from django.conf import settings
+from django.db.models import Q
 from django.utils.translation import ugettext_lazy as _
 
 from adhocracy4.maps import widgets as maps_widgets
+from meinberlin.apps.contrib import widgets as contrib_widgets
 
 from . import models
+
+
+def get_category_options():
+    return models.Plan.objects\
+        .filter(~Q(category=''))\
+        .order_by('category')\
+        .values_list('category', flat=True)\
+        .distinct()
 
 
 class PlanForm(forms.ModelForm):
@@ -16,7 +26,10 @@ class PlanForm(forms.ModelForm):
                   'project']
         widgets = {
             'point': maps_widgets.MapChoosePointWidget(
-                polygon=settings.BERLIN_POLYGON)
+                polygon=settings.BERLIN_POLYGON),
+            'category': contrib_widgets.TextWithDatalistWidget(attrs={
+                'options': get_category_options()
+            })
         }
         error_messages = {
             'point': {
