133df14467c6fbdcf725410390e3eedf65479b4d
Johannes Dillmann <johannes.dillmann@liqd.de>
2017-12-21 10:17:11 +0100

    Add reference number to plans and use it from urls

--- meinberlin/apps/contrib/views.py
+++ meinberlin/apps/contrib/views.py
@@ -1,5 +1,6 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404
+from django.shortcuts import redirect
 from django.views import generic
 
 from adhocracy4.modules.models import Module
@@ -87,3 +88,19 @@ class ProjectContextMixin(generic.base.ContextMixin):
 
 class ComponentLibraryView(generic.base.TemplateView):
     template_name = 'meinberlin_contrib/component_library.html'
+
+
+class CanonicalURLDetailView(generic.DetailView):
+    """DetailView redirecting to the canonical absolute url of an object."""
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+
+        # Redirect to the absolute url if it differs from the current path
+        if hasattr(self.object, 'get_absolute_url'):
+            absolute_url = self.object.get_absolute_url()
+            if absolute_url != request.path:
+                return redirect(absolute_url)
+
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
