38b79013af480382e64e34a363df99be9f9de779
Johannes Dillmann <johannes.dillmann@liqd.de>
2018-02-13 17:44:28 +0100

    Fix import sorting

--- meinberlin/config/whitenoise.py
+++ meinberlin/config/whitenoise.py
@@ -3,6 +3,7 @@
 import os
 
 from django.core.wsgi import get_wsgi_application
+
 from whitenoise.django import DjangoWhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "meinberlin.config.settings")
