9302639e16a492d4fb43d9b773822351a5558ae4
Johannes Dillmann <johannes.dillmann@liqd.de>
2018-03-20 17:57:42 +0100

    Add documents to django admin as inline

--- adhocracy4/offlineevents/admin.py
+++ adhocracy4/offlineevents/admin.py
@@ -1,7 +1,24 @@
 from django.contrib import admin
 
+from adhocracy4.projects.admin import ProjectAdminFilter
+
 from . import models
 
 
-admin.site.register(models.OfflineEvent)
-admin.site.register(models.OfflineEventDocument)
+class OfflineEventDocumentInline(admin.StackedInline):
+    model = models.OfflineEventDocument
+
+
+@admin.register(models.OfflineEvent)
+class OfflineEventAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'project', 'date', 'created')
+    list_filter = (
+        'project__organisation',
+        'project__is_archived',
+        ProjectAdminFilter
+    )
+    date_hierarchy = 'created'
+    search_fields = ('name',)
+    inlines = [
+        OfflineEventDocumentInline
+    ]
