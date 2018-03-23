ef1dffadeb8e055bd41d388d5a8c0e39cb083ed1
mkind <sascha.zinke@splone.com>
2018-03-23 11:56:54 +0100

    only push remarks to server if something changed

--- meinberlin/apps/moderatorremark/static/js/popover.js
+++ meinberlin/apps/moderatorremark/static/js/popover.js
@@ -1,17 +1,26 @@
-var api = require('adhocracy4').api
 const $ = require('jquery')
+const a4api = require('adhocracy4').api
 
 $(function () {
   $('[data-toggle="popover"]').popover()
 
   $('#idea--popover__btn').on('hide.bs.popover', function () {
-    api.moderatorremark.change(
-      {remark: $('#idea--popover__input').val()},
-      $('#idea--popover__btn').data('slug')
-    )
+    var oldVal = $('#idea--popover__input')[0]['oldVal']
+    var newVal = $('#idea--popover__input').val()
+
+    if (oldVal !== newVal) {
+      a4api.moderatorremark.change(
+        {remark: $('#idea--popover__input').val()},
+        $('#idea--popover__btn').data('slug')
+      )
+    }
   })
 
   $('#idea--popover__btn').on('show.bs.popover', function () {
-    api.moderatorremark.get($('#idea--popover__btn').data('slug'))
+    a4api.moderatorremark.get($('#idea--popover__btn').data('slug'))
+      .done((v) => {
+        $('#idea--popover__input')[0]['oldVal'] = v.remark
+        $('#idea--popover__input').val(v.remark)
+      })
   })
 })
