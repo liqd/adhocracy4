de9d5dd3de7b368bb42a835638006aad5aebbdde
fuzzylogic2000 <katharina.albers@liqd.de>
2018-04-05 13:51:07 +0200

    add labels mixin

---
+++ adhocracy4/labels/mixins.py
@@ -0,0 +1,20 @@
+from adhocracy4.labels import models as labels_models
+
+
+class LabelsAddableFieldMixin:
+    labels_field_name = 'labels'
+
+    def __init__(self, *args, **kwargs):
+        if not hasattr(self, 'module'):
+            self.module = kwargs.pop('module')
+        super().__init__(*args, **kwargs)
+
+        field = self.fields[self.labels_field_name]
+        field.queryset = labels_models.Label.objects.filter(module=self.module)
+
+        field.required = False
+
+    def show_labels(self):
+        field = self.fields[self.labels_field_name]
+        module_has_labels = field.queryset.exists()
+        return module_has_labels
