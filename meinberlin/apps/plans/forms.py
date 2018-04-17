af47c7ce1e01ffc5993ba22d69e3db4791831424
fuzzylogic2000 <katharina.albers@liqd.de>
2018-04-17 14:28:41 +0200

    remove projct from plan form

--- meinberlin/apps/plans/forms.py
+++ meinberlin/apps/plans/forms.py
@@ -25,7 +25,7 @@ class PlanForm(forms.ModelForm):
         model = models.Plan
         fields = ['title', 'contact', 'point', 'point_label', 'district',
                   'cost', 'description', 'category', 'status',
-                  'participation', 'projects']
+                  'participation']
         widgets = {
             'point': maps_widgets.MapChoosePointWidget(
                 polygon=settings.BERLIN_POLYGON),
