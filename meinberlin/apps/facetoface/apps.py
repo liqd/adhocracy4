1eeb7cc001b27ac66ffd3c72dca93d7bfb5551c2
fuzzylogic2000 <katharina.albers@liqd.de>
2018-04-25 14:22:11 +0200

    add app

---
+++ meinberlin/apps/facetoface/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Config(AppConfig):
+    name = 'meinberlin.apps.facetoface'
+    label = 'meinberlin_facetoface'
--- meinberlin/config/settings/base.py
+++ meinberlin/config/settings/base.py
@@ -103,6 +103,7 @@ INSTALLED_APPS = (
     'meinberlin.apps.budgeting.apps.Config',
     'meinberlin.apps.documents.apps.Config',
     'meinberlin.apps.extprojects.apps.Config',
+    'meinberlin.apps.facetoface.apps.Config',
     'meinberlin.apps.ideas.apps.Config',
     'meinberlin.apps.kiezkasse.apps.Config',
     'meinberlin.apps.mapideas.apps.Config',
