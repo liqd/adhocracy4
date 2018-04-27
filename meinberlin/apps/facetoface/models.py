5c03357c186d1435b0750eb26c1cce8f2b618d29
fuzzylogic2000 <katharina.albers@liqd.de>
2018-04-27 16:04:32 +0200

    use collapsible uploading field for description

--- meinberlin/apps/facetoface/models.py
+++ meinberlin/apps/facetoface/models.py
@@ -1,18 +1,22 @@
 from autoslug import AutoSlugField
-from ckeditor.fields import RichTextField
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+from adhocracy4.ckeditor.fields import RichTextCollapsibleUploadingField
 from adhocracy4.modules import models as module_models
 
 
 class Activity(module_models.Item):
     slug = AutoSlugField(populate_from='name', unique=True)
     name = models.CharField(max_length=120, verbose_name=_('Title'))
-    highlight = models.CharField(max_length=120,
-                                 verbose_name=_('Highlighted Info')
-                                 )
-    description = RichTextField(verbose_name=_('Description'))
+    highlight = models.CharField(
+        max_length=120,
+        verbose_name=_('Highlighted Info')
+    )
+    description = RichTextCollapsibleUploadingField(
+        config_name='collapsible-image-editor',
+        verbose_name=_('Description')
+    )
 
     def get_absolute_url(self):
         return self.project.get_absolute_url()
