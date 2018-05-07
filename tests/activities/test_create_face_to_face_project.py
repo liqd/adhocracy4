632bf0d58221f985fdeedc268aafda8deb285f08
Irae Hueck Costa <i.hueck-costa@liqd.net>
2018-05-07 18:07:53 +0200

    renamed test name and pep8

--- tests/activities/test_create_face_to_face_project.py
+++ tests/activities/test_create_face_to_face_project.py
@@ -3,19 +3,26 @@ from django.urls import reverse
 
 from meinberlin.test.helpers import setup_users
 
-#@pytest.mark.django_db
-#def test_facetoface_in_blueprints(client, project):
 
 @pytest.mark.django_db
-def test_create_face_to_face_project(client, project):
+def test_facetoface_in_blueprints(client, project):
     anonymous, moderator, initiator = setup_users(project)
     client.login(username=initiator.email, password='password')
-    blueprints_url =  reverse('a4dashboard:blueprint-list', kwargs={'organisation_slug': project.organisation.slug})
-    facetoface_blueprint_url = reverse('a4dashboard:project-create', kwargs={'blueprint_slug': 'facetoface', 'organisation_slug': project.organisation.slug})
+    blueprints_url = reverse(
+        'a4dashboard:blueprint-list',
+        kwargs={
+            'organisation_slug': project.organisation.slug
+        })
+    facetoface_blueprint_url = reverse(
+        'a4dashboard:project-create',
+        kwargs={
+            'blueprint_slug': 'facetoface',
+            'organisation_slug': project.organisation.slug
+        })
     resp = client.get(blueprints_url, follow=True)
     assert facetoface_blueprint_url in resp.content.decode()
-    #assert 0, facetoface_blueprint_url
 
-    resp = client.get(facetoface_blueprint_url)
 
-    import pdb; pdb.set_trace()
+@pytest.mark.django_db
+def test_xxxx(client, project):
+    pass
