297c0e829a81cba5cb7bedc165698c0704149073
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-05-18 11:14:53 +0200

    add possibility to add matomo tracking code

--- meinberlin/config/settings/dev.py
+++ meinberlin/config/settings/dev.py
@@ -2,6 +2,7 @@ from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+TRACKING_ENABLED = False
 
 for template_engine in TEMPLATES:
     template_engine['OPTIONS']['debug'] = True
