c5a31be1bd452224c2b35c4f3e3132b2df1431e7
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-06-01 15:04:28 +0200

    reorder imports

--- meinberlin/apps/documents/exports.py
+++ meinberlin/apps/documents/exports.py
@@ -4,20 +4,19 @@ from django.utils.translation import ugettext_lazy as _
 from adhocracy4.comments.models import Comment
 from adhocracy4.exports import mixins as export_mixins
 from adhocracy4.exports import views as export_views
-from adhocracy4.projects.mixins import ProjectMixin
 from meinberlin.apps.exports import mixins as mb_export_mixins
 from meinberlin.apps.exports import register_export
 
 
 @register_export(_('Documents with comments'))
 class DocumentExportView(
-        export_views.BaseItemExportView,
         export_mixins.ExportModelFieldsMixin,
         mb_export_mixins.UserGeneratedContentExportMixin,
         export_mixins.ItemExportWithLinkMixin,
         export_mixins.ItemExportWithRatesMixin,
         mb_export_mixins.ItemExportWithRepliesToMixin,
-        ProjectMixin):
+        export_views.BaseItemExportView
+):
 
     model = Comment
 
