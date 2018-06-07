4eedcc5673efccb25b16f800730de41169b2eeb5
Irae Hueck Costa <i.hueck-costa@liqd.net>
2018-06-07 10:54:36 +0200

    Moved mb helpers to a4

--- adhocracy4/test/helpers.py
+++ adhocracy4/test/helpers.py
@@ -1,13 +1,20 @@
+from adhocracy4.test.helpers import redirect_target
+from contextlib import contextmanager
+from datetime import timedelta
+from django.conf import settings
+from django.contrib.auth.models import AnonymousUser
+from django.core.urlresolvers import resolve
+from django.db.models.signals import post_save
+from django.template import Context, Template
+from django.utils.encoding import smart_text
+from easy_thumbnails.files import get_thumbnailer
+from freezegun import freeze_time
+from unittest import mock
+from urllib.parse import urlparse
+import factory
 import importlib
 import json
 import os
-from unittest import mock
-from urllib.parse import urlparse
-
-from django.conf import settings
-from django.core.urlresolvers import resolve
-from django.template import Context, Template
-from easy_thumbnails.files import get_thumbnailer
 
 
 def create_thumbnail(imagefield):
@@ -76,3 +83,72 @@ def dispatch_view(view_class, request, *args, **kwargs):
     view.args = args
     view.kwargs = kwargs
     return view.dispatch(request, *args, **kwargs), view
+
+
+@factory.django.mute_signals(post_save)
+def setup_phase(phase_factory, item_factory, phase_content_class, **kwargs):
+    phase_content = phase_content_class()
+    phase = phase_factory(phase_content=phase_content, **kwargs)
+    module = phase.module
+    project = phase.module.project
+    item = item_factory(module=module) if item_factory else None
+    return phase, module, project, item
+
+
+@factory.django.mute_signals(post_save)
+def setup_users(project):
+    anonymous = AnonymousUser()
+    moderator = project.moderators.first()
+    initiator = project.organisation.initiators.first()
+    return anonymous, moderator, initiator
+
+
+@contextmanager
+def freeze_phase(phase):
+    with freeze_time(phase.start_date + timedelta(seconds=1)):
+        yield
+
+
+@contextmanager
+def freeze_pre_phase(phase):
+    with freeze_time(phase.start_date - timedelta(seconds=1)):
+        yield
+
+
+@contextmanager
+def freeze_post_phase(phase):
+    with freeze_time(phase.end_date + timedelta(seconds=1)):
+        yield
+
+
+def assert_template_response(response, template_name, status_code=200):
+    assert response.status_code == status_code
+    response_template = response.template_name[0]
+    assert response_template == template_name, \
+        '{} != {}'.format(response_template, template_name)
+
+
+def assert_dashboard_form_component_response(
+        response, component, status_code=200):
+    assert response.status_code == status_code
+    assert str(component.form_title) in smart_text(response.content)
+
+
+def assert_dashboard_form_component_edited(
+        response, component, obj, data, status_code=200):
+    obj.refresh_from_db()
+    assert redirect_target(response) == \
+        'dashboard-{}-edit'.format(component.identifier)
+    for key in data.keys():
+        attr = getattr(obj, key)
+        value = data.get(key)
+        assert attr == value, '{} != {}'.format(attr, value)
+
+
+def createThumbnail(imagefield):
+    thumbnailer = get_thumbnailer(imagefield)
+    thumbnail = thumbnailer.generate_thumbnail(
+        {'size': (800, 400), 'crop': 'smart'})
+    thumbnailer.save_thumbnail(thumbnail)
+    thumbnail_path = os.path.join(settings.MEDIA_ROOT, thumbnail.path)
+    return thumbnail_path
