24901e7a4f8e3bbb776e5ea65869b227dfd9e328
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-06-14 13:41:16 +0200

    add even more tests

--- tests/polls/test_models.py
+++ tests/polls/test_models.py
@@ -7,10 +7,10 @@ from adhocracy4.polls.models import Question, Choice
 
 @pytest.mark.django_db
 def test_question_choice_list(user,
-                          poll_factory,
-                          vote_factory,
-                          question_factory,
-                          choice_factory):
+                              poll_factory,
+                              vote_factory,
+                              question_factory,
+                              choice_factory):
 
     poll = poll_factory()
     question = question_factory(poll=poll)
@@ -29,10 +29,10 @@ def test_question_choice_list(user,
 
 @pytest.mark.django_db
 def test_question_queryset(user_factory,
-                          poll_factory,
-                          vote_factory,
-                          question_factory,
-                          choice_factory):
+                           poll_factory,
+                           vote_factory,
+                           question_factory,
+                           choice_factory):
 
     poll = poll_factory()
     question = question_factory(poll=poll)
@@ -42,8 +42,8 @@ def test_question_queryset(user_factory,
     user1 = user_factory()
     user2 = user_factory()
 
-    vote = vote_factory(creator=user1, choice=choice1)
-    vote = vote_factory(creator=user2, choice=choice2)
+    vote_factory(creator=user1, choice=choice1)
+    vote_factory(creator=user2, choice=choice2)
 
     questions = Question.objects.annotate_vote_count()
 
@@ -66,11 +66,10 @@ def test_choice_queryset(user_factory,
     user1 = user_factory()
     user2 = user_factory()
 
-    vote = vote_factory(creator=user1, choice=choice1)
-    vote = vote_factory(creator=user2, choice=choice2)
+    vote_factory(creator=user1, choice=choice1)
+    vote_factory(creator=user2, choice=choice2)
 
     choices = Choice.objects.annotate_vote_count()
 
     assert hasattr(choices.first(), 'vote_count')
     assert choices.first().vote_count == 1
-
---
+++ tests/polls/test_validators.py
@@ -0,0 +1,67 @@
+import pytest
+
+from rest_framework import status
+from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
+from adhocracy4.polls.models import Vote, Poll
+from adhocracy4.polls.phases import VotingPhase
+from adhocracy4.polls.validators import single_vote_per_user
+from adhocracy4.polls.validators import single_item_per_module
+
+
+@pytest.mark.django_db
+def test_choice_belongs_to_question(admin,
+                                    apiclient,
+                                    poll_factory,
+                                    question_factory,
+                                    choice_factory):
+
+    poll = poll_factory()
+    question1 = question_factory(poll=poll)
+    choice1 = choice_factory(question=question1)
+    question2 = question_factory(poll=poll)
+    choice2 = choice_factory(question=question2)
+
+    apiclient.force_authenticate(user=admin)
+
+    url = reverse(
+        'votes-list',
+        kwargs={
+            'question_pk': question1.pk
+        })
+
+    data = {
+        'choices': [choice2.pk]
+    }
+
+    with pytest.raises(ValidationError):
+        response = apiclient.post(url, data, format='json')
+
+
+@pytest.mark.django_db
+def test_single_vote_per_user(admin,
+                              apiclient,
+                              poll_factory,
+                              vote_factory,
+                              question_factory,
+                              choice_factory):
+
+    poll = poll_factory()
+    question1 = question_factory(poll=poll, multiple_choice=False)
+    choice1 = choice_factory(question=question1)
+
+    vote_factory(creator=admin, choice=choice1)
+
+    with pytest.raises(ValidationError):
+        single_vote_per_user(admin, choice1)
+
+
+@pytest.mark.django_db
+def test_single_item_per_module(poll_factory):
+
+    poll = poll_factory()
+    module = poll.module
+
+    with pytest.raises(ValidationError):
+        single_item_per_module(module, Poll)
+
