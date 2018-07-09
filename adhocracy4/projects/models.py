15a2932207a4679189c9a250e97e923f2de58364
Irae Hueck Costa <mail@irae.me>
2018-07-09 15:08:48 +0200

    Style for a4 project models changes

--- adhocracy4/projects/models.py
+++ adhocracy4/projects/models.py
@@ -28,14 +28,18 @@ class ProjectContactDetailMixin(models.Model):
     class Meta:
         abstract = True
 
-    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
-        message=_("Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed."))
+    phone_regex = RegexValidator(
+        regex=r'^\+?1?\d{9,15}$',
+        message=_("Phone number must be entered in the format: '+999999999'. "
+                  "Up to 15 digits allowed."))
 
     contact_address_text = models.TextField(blank=True)
     contact_email = models.EmailField(blank=True)
     contact_name = models.CharField(max_length=120, blank=True)
-    contact_phone = models.CharField(validators=[phone_regex], max_length=17, blank=True)
+    contact_phone = models.CharField(
+        validators=[phone_regex], max_length=17, blank=True)
     contact_url = models.URLField(blank=True)
+
 
 class Project(ProjectContactDetailMixin, base.TimeStampedModel):
     slug = AutoSlugField(populate_from='name', unique=True)
