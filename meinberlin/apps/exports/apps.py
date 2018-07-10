63ffd8362a90ede2e6898ec33e283bdac7cbf5e6
fuzzylogic2000 <katharina.albers@liqd.de>
2018-07-10 10:38:38 +0200

    remove export module registering

--- meinberlin/apps/exports/apps.py
+++ meinberlin/apps/exports/apps.py
@@ -4,7 +4,3 @@ from django.apps import AppConfig
 class Config(AppConfig):
     name = 'meinberlin.apps.exports'
     label = 'meinberlin_exports'
-
-    def ready(self):
-        from django.utils.module_loading import autodiscover_modules
-        autodiscover_modules('exports', register_to=self.module.exports)
