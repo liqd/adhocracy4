df662f17ba85e8f4bd27c3efee440fcd5b5032e5
fuzzylogic2000 <katharina.albers@liqd.de>
2018-09-10 15:01:53 +0200

    gardening for updated linters

--- meinberlin/assets/js/init-picker.js
+++ meinberlin/assets/js/init-picker.js
@@ -6,7 +6,7 @@ $(function () {
   let $inputs = $('.datepicker')
 
   $inputs.each(function (i, e) {
-    let initObject = {formElements: {}}
+    let initObject = { formElements: {} }
     initObject.formElements[e.id] = django.get_format('DATE_INPUT_FORMATS')[0]
     datePickerController.createDatePicker(initObject)
   })
