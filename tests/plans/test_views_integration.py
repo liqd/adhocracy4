eb6d899844fddefd253f0a4b5133d8c64ebd224a
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-09-25 13:18:37 +0200

    update test

--- tests/plans/test_views_integration.py
+++ tests/plans/test_views_integration.py
@@ -15,6 +15,16 @@ def test_list_view(client, plan_factory):
 
 
 @pytest.mark.django_db
+def test_list_view_no_district(client, plan_factory):
+    plan_factory()
+    plan_factory(district=None)
+    url = reverse('meinberlin_plans:plan-list')
+    response = client.get(url)
+    assert_template_response(
+        response, 'meinberlin_plans/plan_list.html')
+
+
+@pytest.mark.django_db
 def test_export_view(client, plan_factory, project_factory):
     project1 = project_factory()
     project2 = project_factory()
