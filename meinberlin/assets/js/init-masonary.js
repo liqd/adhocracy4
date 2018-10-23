6b9cf3cb9245ad570856ed13fd950fe0f54cf12a
Phillippa <p.morland@liqd.net>
2018-10-23 15:17:22 +0200

    linting errors correction

--- meinberlin/assets/js/init-masonary.js
+++ meinberlin/assets/js/init-masonary.js
@@ -1,5 +1,7 @@
+const $ = require('jquery')
+
 $('.grid').masonry({
 // set itemSelector so .grid-sizer is not used in layout
-itemSelector: '.grid-item',
+  itemSelector: '.grid-item'
 // use element for option
 })
