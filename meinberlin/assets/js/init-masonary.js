57d18e2868c54bfb4c5004ab49b075364c94515b
Phillippa <p.morland@liqd.net>
2018-10-24 13:59:50 +0200

    adding responsive sizing to tiles

--- meinberlin/assets/js/init-masonary.js
+++ meinberlin/assets/js/init-masonary.js
@@ -4,5 +4,7 @@ $('.grid').masonry({
   // set itemSelector so .grid-sizer is not used in layout
   itemSelector: '.grid-item',
   // use element for option
-  columnWidth: 160
+  // columnWidth: 160
+  percentPosition: true,
+  columnWidth: '.grid-sizer'
 })
