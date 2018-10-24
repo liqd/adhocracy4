51ea62d284acf0517047eeef20c1084669333325
fuzzylogic2000 <katharina.albers@liqd.de>
2018-10-24 17:59:32 +0200

    get all districts from bplan api endpoint

--- meinberlin/apps/bplan/management/commands/bplan_get_location.py
+++ meinberlin/apps/bplan/management/commands/bplan_get_location.py
@@ -3,28 +3,49 @@ import urllib.request
 
 from django.core.management.base import BaseCommand
 
+from adhocracy4.administrative_districts.models import AdministrativeDistrict
 from meinberlin.apps.bplan.models import Bplan
 
 
 class Command(BaseCommand):
-    help = ('Uses the Bplan identifier to get the location '
-            'information from API of bplan-prod.liqd.net')
+    help = ('Uses the Bplan identifier to get the coordinates '
+            'and the district from API of bplan-prod.liqd.net')
 
     def handle(self, *args, **options):
+
+        url_dis = 'https://bplan-prod.liqd.net/api/bezirke/'
+        req = urllib.request.Request(url_dis)
+        res = urllib.request.urlopen(req)
+        res_body = res.read()
+        res_json = json.loads(res_body.decode("utf-8"))
+
+        dis_dict = {}
+        dis_json = res_json.get('features')
+        if dis_json:
+            for district in dis_json:
+                dis_dict[district['properties']['pk']] = \
+                    district['properties']['name']
+        print(dis_dict)
+
+        dis_model = AdministrativeDistrict.objects.all()
+        print(dis_model)
 
         for bplan in Bplan.objects.all():
             print(bplan.identifier)
             if bplan.identifier:
-                url = 'https://bplan-prod.liqd.net/api/bplan/points/' + \
+                url_poi = 'https://bplan-prod.liqd.net/api/bplan/points/' + \
                     '?bplan={}'.format(bplan.identifier.replace(' ', '%20'))
                 try:
-                    req = urllib.request.Request(url)
+                    req = urllib.request.Request(url_poi)
                     res = urllib.request.urlopen(req)
                     res_body = res.read()
                     res_json = json.loads(res_body.decode("utf-8"))
 
                     features = res_json.get('features')
+                    print(features)
                     if features:
+                        district_pk = features[0]['properties']['bezirk']
+                        print(district_pk)
                         bplan.point = features[0]
                         bplan.save()
 
