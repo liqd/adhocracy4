9a4be34fad426edf75e9cea788f0a54c845e4c93
Robert Mader <r.mader@liqd.net>
2018-12-04 15:44:00 +0100

    tests: make helpers.py use timezone.now

--- tests/helpers.py
+++ tests/helpers.py
@@ -1,12 +1,12 @@
-from datetime import datetime, timedelta
-
 from contextlib import contextmanager
+from datetime import timedelta
+from django.utils import timezone
 from freezegun import freeze_time
 
 
 @contextmanager
 def active_phase(module, phase_type):
-    now = datetime.now()
+    now = timezone.now()
     phase = module.phase_set.create(
         start_date=now,
         end_date=now + timedelta(days=1),
