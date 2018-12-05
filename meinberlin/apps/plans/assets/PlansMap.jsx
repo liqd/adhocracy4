0dfa81ad556a0a60e792b57f622c6a5e6b8b433b
Magdalena Noffke <magdalena.noffke@liqd.de>
2018-12-05 17:21:15 +0100

    display markers

--- meinberlin/apps/plans/assets/PlansMap.jsx
+++ meinberlin/apps/plans/assets/PlansMap.jsx
@@ -1,3 +1,4 @@
+/* global $ */
 const React = require('react')
 const L = require('leaflet')
 require('mapbox-gl-leaflet')
@@ -11,6 +12,32 @@ require('mapbox-gl-leaflet')
   shadowAnchor: [20, 54],
   zIndexOffset: 1000
 }) */
+const pointToLatLng = function (point) {
+  if (point.geometry !== '') {
+    return {
+      lat: point.geometry.coordinates[1],
+      lng: point.geometry.coordinates[0]
+    }
+  }
+}
+
+const statusIconNames = [
+  'idee',
+  'planung',
+  'implementiert',
+  'beendet',
+  'pause'
+]
+
+const statusIconPins = statusIconNames.map((cls, i) => L.icon({
+  iconUrl: `/static/plan_icons/pins/${cls}_pin.svg`,
+  shadowUrl: '/static/images/map_shadow_01.svg',
+  iconSize: [30, 36],
+  iconAnchor: [15, 36],
+  shadowSize: [40, 54],
+  shadowAnchor: [20, 54],
+  zIndexOffset: 1000
+}))
 
 class PlansMap extends React.Component {
   constructor (props) {
@@ -45,6 +72,10 @@ class PlansMap extends React.Component {
     )
   } */
 
+  escapeHtml (unsafe) {
+    return $('<div>').text(unsafe).html()
+  }
+
   bindMap (element) {
     this.mapElement = element
   }
@@ -61,8 +92,43 @@ class PlansMap extends React.Component {
     return map
   }
 
+  addDistrictLayers (map) {
+    let districtStyle = {
+      'color': '#253276',
+      'weight': 1,
+      'opacity': 1,
+      'fillOpacity': 0
+    }
+    L.geoJSON(this.props.districts, { style: districtStyle }).addTo(map)
+  }
+
+  getPopUpContent (item) {
+    let popupContent = '<div class="maps-popups-popup-text-content">' +
+                             '<div class="maps-popups-popup-name">' +
+                                '<a href="' + item.url + '">' + this.escapeHtml(item.title) + '</a>' +
+                            '</div>' +
+                          '</div>'
+    return popupContent
+  }
+
+  addMarkers (cluster) {
+    this.props.items.map((item, i) => {
+      if (item.point !== '') {
+        let marker = L.marker(pointToLatLng(item.point), { icon: statusIconPins[item.status] })
+        cluster.addLayer(marker)
+        marker.bindPopup(this.getPopUpContent(item))
+        return marker
+      }
+    })
+  }
+
   componentDidMount () {
     this.map = this.createMap()
+    this.addDistrictLayers(this.map)
+    this.cluster = L.markerClusterGroup({
+      showCoverageOnHover: false
+    }).addTo(this.map)
+    this.markers = this.addMarkers(this.cluster)
   }
 
   render () {
