95d2007acfd9d28d8c057225da14b795afa52f85
Phillippa <p.morland@liqd.net>
2018-12-17 17:12:54 +0100

    correcting switch behaviour

--- meinberlin/apps/plans/assets/MapListSwitch.jsx
+++ meinberlin/apps/plans/assets/MapListSwitch.jsx
@@ -8,7 +8,7 @@ class MapListSwitch extends React.Component {
         <div className="u-spacer-left u-spacer-right">
           <div className="switch-group" role="group" aria-label={django.gettext('Filter bar')}>
             <div className="switch-label">{django.gettext('Show map')}</div>
-            <div className="switch u-mobile-display-none">
+            <div className="switch u-lg-mobile-display-none">
               <input
                 id="switch-primary"
                 onChange={this.props.toggleSwitch}
