963938871db57188000337dece084b0b3e18917b
Phillippa <p.morland@liqd.net>
2018-12-20 10:24:17 +0100

    image adjusting adding link to whole tile and styling

--- meinberlin/apps/plans/assets/PlansList.jsx
+++ meinberlin/apps/plans/assets/PlansList.jsx
@@ -2,8 +2,9 @@
 const React = require('react')
 
 var styles = {
-  backgroundImage: `url(https://placeimg.com/250/250/any/grayscale)`,
-  backgroundPosition: 'center'
+  backgroundImage: `url(https://placeimg.com/500/500/any/grayscale)`,
+  backgroundPosition: 'center',
+  backgroundSize: 'contain'
 }
 
 class PlansList extends React.Component {
@@ -12,41 +13,42 @@ class PlansList extends React.Component {
   }
   renderListItem (item, i) {
     let itemClass = 'maplist-item'
-    let statusClass = (item.participation_active === true) ? 'list-item__status--active' : 'list-item__status--inactive'
+    let statusClass = (item.participation_active === true) ? 'maplist-item__status--active' : 'maplist-item__status--inactive'
     return (
 
       <li className={itemClass} key={i} tabIndex="0">
-        <div className={item.type === 'project' ? 'maplist-proj' : 'd-none'}>
-          <div className="maplist__image" style={styles} alt="">
-            <span className="maplist__copyright copyright">© copyright</span>
+        <a href={item.url}>
+          <div className={item.type === 'project' ? 'maplist-item__proj' : 'd-none'}>
+            <div className="maplist-item__img" style={styles} alt="">
+              <span className="maplist-item__img-copyright copyright">© copyright</span>
+            </div>
+
+            <div className="maplist-item__info">
+              <div className="maplist-item__labels u-spacer-bottom">
+                <span className="label label--secondary">{item.theme}</span>
+              </div>
+              <span className="maplist-item__roofline">{item.district}</span>
+              <h3 className="maplist-item__title">{item.title}</h3>
+              <span>Short description?</span>
+            </div>
           </div>
 
-          <div className="maplist__info">
+          <div className={item.type === 'plan' ? 'maplist-item__plan' : 'd-none'}>
             <div className="maplist-item__labels u-spacer-bottom">
               <span className="label label--secondary">{item.theme}</span>
             </div>
             <span className="maplist-item__roofline">{item.district}</span>
-            <h3 className="maplist-item__title"><a href={item.url}>{item.title}</a></h3>
-            <span>Short description?</span>
+            <h3 className="maplist-item__title">{item.title}</h3>
+            <div>
+              <span className="maplist-item__proj-count"><i className="fas fa-th" />{django.gettext('Participation projects: ')}</span>
+              <span className="maplist-item__status--inactive">?</span>
+            </div>
+            <div>
+              <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('Participation: ')}</span>
+              <span className={statusClass}>{item.participation_string }</span>
+            </div>
           </div>
-        </div>
-
-        <div className={item.type === 'plan' ? 'maplist-plan' : 'd-none'}>
-          <div className="maplist-item__labels u-spacer-bottom">
-            <span className="label label--secondary">{item.theme}</span>
-          </div>
-          <span className="maplist-item__roofline">{item.district}</span>
-          <h3 className="maplist-item__title"><a href={item.url}>{item.title}</a></h3>
-          <div>
-            <span><i className="fas fa-th" />{django.gettext('Participation projects: ')}</span>
-            <span>?</span>
-          </div>
-          <div>
-            <span><i className="fas fa-clock" />{django.gettext('Participation: ')}</span>
-            <span className={statusClass}>{item.participation_string }</span>
-          </div>
-        </div>
-
+        </a>
       </li>
     )
   }
