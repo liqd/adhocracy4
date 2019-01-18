244a209dcaabbd383774a711157d5a0be6a6c2ec
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-01-18 18:54:55 +0100

    remove whitespace

--- meinberlin/apps/plans/assets/PopUp.jsx
+++ meinberlin/apps/plans/assets/PopUp.jsx
@@ -9,13 +9,13 @@ class PopUp extends React.Component {
 
   render () {
     let statusClass = (this.props.item.participation_active === true) ? 'maplist-item__status-active' : 'maplist-item__status-inactive'
-    if (this.props.item.type === ' project') {
+    if (this.props.item.type === 'project') {
       return (
         <div className="maps-popups-popup-text-content">
           <span className="label label--secondary maplist-item__label">{this.props.item.topic}</span>
           <span className="maplist-item__roofline">{this.props.item.district}</span>
           <div className="maps-popups-popup-name">
-            <a href={this.props.item.url}>{this.escapeHtml(this.props.item.title)}</a>'
+            <a href={this.props.item.url}>{this.props.item.title}</a>'
           </div>
           <div className="status-item status__future">
             <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('Participation: from ') + this.props.item.future_phase + django.gettext(' possible')}</span>
