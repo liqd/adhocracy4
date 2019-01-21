a865a4f6fd6014c8f2e050e400ddce325694a412
Phillippa <p.morland@liqd.net>
2019-01-21 11:12:33 +0100

    adding all required information to popups

--- meinberlin/apps/plans/assets/PopUp.jsx
+++ meinberlin/apps/plans/assets/PopUp.jsx
@@ -17,19 +17,41 @@ class PopUp extends React.Component {
           <div className="maps-popups-popup-name">
             <a href={this.props.item.url}>{this.props.item.title}</a>
           </div>
+          {this.props.item.future_phase &&
           <div className="status-item status__future">
             <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('Participation: from ') + this.props.item.future_phase + django.gettext(' possible')}</span>
           </div>
+          }
+          {this.props.item.active_phase &&
+          <div className="status-item status__active">
+            <div className="status-bar__active"><span className="status-bar__active-fill" /></div>
+            <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('remaining')} {this.props.item.active_phase[1]}</span>
+          </div>
+          }
+          {this.props.item.past_phase &&
+            <div className="status-item status-bar__past">
+              {django.gettext('Participation ended. Read result.')}
+            </div>
+          }
         </div>
       )
     } else {
       return (
-        <div className="maplist-item__stats">
-          <span className="maplist-item__proj-count">
-            <i className="fas fa-th" />{django.gettext('Participation projects: ') }</span>
-          <span>{django.gettext('some')}</span>
-          <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('Participation: ')}</span>
-          <span className={statusClass}>{this.props.item.participation_string}</span>
+        <div className="maps-popups-popup-text-content">
+          <div className="maplist-item__labels u-spacer-bottom-half">
+            <span className="label label--secondary">{this.props.item.topic}</span>
+          </div>
+          <span className="maplist-item__roofline">{this.props.item.district}</span>
+          <div className="maps-popups-popup-name">
+            <a href={this.props.item.url}>{this.props.item.title}</a>
+          </div>
+          <div className="maplist-item__stats">
+            <span className="maplist-item__proj-count">
+              <i className="fas fa-th" />{django.gettext('Participation projects: ') }</span>
+            <span>{this.props.item.published_projects_count}</span>
+            <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('Participation: ')}</span>
+            <span className={statusClass}>{this.props.item.participation_string}</span>
+          </div>
         </div>
       )
     }
