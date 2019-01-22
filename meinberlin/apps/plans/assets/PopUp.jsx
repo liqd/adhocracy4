99cddb3251cb7bde756d0a94745566ef11bfd2ad
Phillippa <p.morland@liqd.net>
2019-01-22 09:45:28 +0100

    Status bar styling and updates to class names, font and padding sizes

--- meinberlin/apps/plans/assets/PopUp.jsx
+++ meinberlin/apps/plans/assets/PopUp.jsx
@@ -7,30 +7,36 @@ class PopUp extends React.Component {
     return $('<div>').text(unsafe).html()
   }
 
+  // getWidth (item) {
+  //   return { width: this.props.item.active_phase[0] + '%' }
+  // }
+
   render () {
     let statusClass = (this.props.item.participation_active === true) ? 'maplist-item__status-active' : 'maplist-item__status-inactive'
     if (this.props.item.type === 'project') {
       return (
         <div className="maps-popups-popup-text-content">
+          {this.props.item.topic &&
           <span className="label label--secondary maplist-item__label u-spacer-bottom-half">{this.props.item.topic}</span>
+          }
           <span className="maplist-popup-item__roofline">{this.props.item.district}</span>
           <div className="maps-popups-popup-name u-spacer-bottom-half">
             <a href={this.props.item.url}>{this.props.item.title}</a>
           </div>
           {this.props.item.future_phase &&
-          <div className="status-item-popup status__future">
-            <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('Participation: from ') + this.props.item.future_phase + django.gettext(' possible')}</span>
+          <div className="status__future">
+            <span className="maplist-item-popup__status"><i className="fas fa-clock" />{django.gettext('Participation: from ') + this.props.item.future_phase + django.gettext(' possible')}</span>
           </div>
           }
           {this.props.item.active_phase &&
-          <div className="status-item-popup status__active">
+          <div className="status__active">
             <div className="status-bar__active"><span className="status-bar__active-fill" /></div>
-            <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('remaining')} {this.props.item.active_phase[1]}</span>
+            <span className="maplist-item-popup__status"><i className="fas fa-clock" />{django.gettext('remaining')} {this.props.item.active_phase[1]}</span>
           </div>
           }
           {this.props.item.past_phase &&
-            <div className="status-item-popup status-bar__past">
-              {django.gettext('Participation ended. Read result.')}
+            <div className="status-bar__past">
+              <span className="maplist-item-popup__status">{django.gettext('Participation ended. Read result.')}</span>
             </div>
           }
         </div>
@@ -43,15 +49,16 @@ class PopUp extends React.Component {
             <span className="label label--secondary">{this.props.item.topic}</span>
           </div>
           }
-          <span className="maplist-item__roofline">{this.props.item.district}</span>
+          <span className="maplist-popup-item__roofline">{this.props.item.district}</span>
           <div className="maps-popups-popup-name u-spacer-bottom-half">
             <a href={this.props.item.url}>{this.props.item.title}</a>
           </div>
           <div className="maplist-popup-item__stats">
-            <span className="maplist-item__proj-count">
+            <span className="maplist-item-popup__proj-count">
               <i className="fas fa-th" />{django.gettext('Participation projects: ') }</span>
             <span>{this.props.item.published_projects_count}</span>
-            <span className="maplist-item__status"><i className="fas fa-clock" />{django.gettext('Participation: ')}</span>
+            <br />
+            <span className="maplist-item-popup__status"><i className="fas fa-clock" />{django.gettext('Participation: ')}</span>
             <span className={statusClass}>{this.props.item.participation_string}</span>
           </div>
         </div>
