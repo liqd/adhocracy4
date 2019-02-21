0e79ca862f6c43b22d8dc9cd93e13e2eb9b4e133
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-02-21 15:28:23 +0100

    update verbose_name and helptext of external project url field

--- meinberlin/apps/extprojects/models.py
+++ meinberlin/apps/extprojects/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 from adhocracy4.projects import models as project_models
 
 
 class ExternalProject(project_models.Project):
-    url = models.URLField(blank=True)
+    url = models.URLField(blank=True,
+                          verbose_name='URL',
+                          help_text=_('Please enter '
+                                      'a full url which '
+                                      'starts with https:// '
+                                      'or http://'))
 
     @property
     def phase(self):
