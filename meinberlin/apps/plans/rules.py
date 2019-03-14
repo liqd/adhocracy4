89f8df83e51f4e0227f765d7ead1e37b451bbfed
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-03-14 12:46:21 +0100

    update rules

--- meinberlin/apps/plans/rules.py
+++ meinberlin/apps/plans/rules.py
@@ -2,6 +2,7 @@ import rules
 from rules.predicates import is_superuser
 
 from adhocracy4.organisations.predicates import is_initiator
+from adhocracy4.organisations.predicates import is_org_group_member
 
 rules.add_perm(
     'meinberlin_plans.list_plan',
@@ -15,12 +16,12 @@ rules.add_perm(
 
 rules.add_perm(
     'meinberlin_plans.add_plan',
-    is_superuser | is_initiator
+    is_superuser | is_initiator | is_org_group_member
 )
 
 rules.add_perm(
     'meinberlin_plans.change_plan',
-    is_superuser | is_initiator
+    is_superuser | is_initiator | is_org_group_member
 )
 
 rules.add_perm(
