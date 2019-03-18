0d2c1c348f74844cb589ae5b11a996c36b94c662
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-03-18 19:04:49 +0100

    test that group is saved on project creation in dashboard

--- tests/dashboard/test_views.py
+++ tests/dashboard/test_views.py
@@ -53,7 +53,14 @@ def test_blueprint_list(client, organisation, user, another_user):
 
 
 @pytest.mark.django_db
-def test_project_create(client, organisation, user, another_user):
+def test_project_create(client, organisation, user_factory, group_factory):
+    group1 = group_factory()
+    group2 = group_factory()
+    user = user_factory()
+    initiator = user_factory()
+    group_member = user_factory.create(groups=(group1, group2))
+    organisation.groups.add(group2)
+
     project_create_url = reverse('a4dashboard:project-create', kwargs={
         'organisation_slug': organisation.slug,
         'blueprint_slug': 'questions'
@@ -71,8 +78,8 @@ def test_project_create(client, organisation, user, another_user):
     response = client.post(project_create_url, data)
     assert response.status_code == 403
 
-    organisation.initiators.add(another_user)
-    client.login(username=another_user, password='password')
+    organisation.initiators.add(initiator)
+    client.login(username=initiator, password='password')
     response = client.post(project_create_url, data)
     assert redirect_target(response) == 'project-edit'
 
@@ -80,6 +87,13 @@ def test_project_create(client, organisation, user, another_user):
     project = Project.objects.all().first()
     assert 'project name' == project.name
     assert 'project description' == project.description
+
+    client.login(username=group_member, password='password')
+    response = client.post(project_create_url, data)
+    assert redirect_target(response) == 'project-edit'
+
+    assert 2 == Project.objects.all().count()
+    assert 1 == Project.objects.filter(group_id=group2.id).count()
 
 
 @pytest.mark.django_db
