dfad45be5c87742e255b71fdc4552be1e49d1a8b
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-03-18 19:02:10 +0100

    add groups to organisation factory

--- adhocracy4/test/factories/__init__.py
+++ adhocracy4/test/factories/__init__.py
@@ -66,6 +66,18 @@ class OrganisationFactory(factory.django.DjangoModelFactory):
     name = factory.Faker('company')
 
 
+    @factory.post_generation
+    def groups(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of groups were passed in, use them
+            for group in extracted:
+                self.groups.add(group)
+
+
 ORGANISATION_FACTORY = getattr(settings,
                                'A4_ORGANISATION_FACTORY',
                                OrganisationFactory)
