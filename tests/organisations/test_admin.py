eb2f734dc24363bda3ad109342e4e67f5853213b
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-04-02 17:31:56 +0200

    add test to test if projects after removal of group from organisation

--- tests/organisations/test_admin.py
+++ tests/organisations/test_admin.py
@@ -1,6 +1,7 @@
 import pytest
 from django.urls import reverse
 
+from adhocracy4.projects.models import Project
 from meinberlin.apps.organisations.models import Organisation
 
 
@@ -68,3 +69,33 @@ def test_organisation_admin_form(client, user_factory, group_factory):
     organisation = Organisation.objects.get(name='My Organisation')
     assert organisation.groups.all().count() == 2
     assert user.groups.all().count() == 1
+
+
+@pytest.mark.django_db
+def test_group_removal(client,
+                       organisation,
+                       project_factory,
+                       user_factory,
+                       group_factory):
+
+    group1 = group_factory()
+    group2 = group_factory()
+    organisation.groups.add(group1)
+    project = project_factory(group=group1, organisation=organisation)
+
+    assert Project.objects.all().count() == 1
+    assert Project.objects.get(slug=project.slug).group == group1
+
+    admin = user_factory(is_superuser=True, is_staff=True)
+    client.force_login(admin)
+
+    url = reverse(
+        'admin:meinberlin_organisations_organisation_change',
+        args=(organisation.id,))
+
+    data = {'name': organisation.name,
+            'groups': group2.id}
+    response = client.post(url, data)
+
+    assert response.status_code == 302
+    assert Project.objects.get(slug=project.slug).group is None
