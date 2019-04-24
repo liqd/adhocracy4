ab088d3e9d295fccdc0a05be994ca5b7685016c8
Robert Mader <r.mader@liqd.net>
2019-04-24 17:33:49 +0200

    Webpack: Remove redundant config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,12 +1,6 @@
-const webpack = require('webpack')
 const merge = require('webpack-merge')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
-  devtool: 'eval',
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ]
+  devtool: 'eval'
 })
