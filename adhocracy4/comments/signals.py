a04f64657fbef6153e1d593f0ba99e7c5655360a
fuzzylogic2000 <katharina.albers@liqd.de>
2019-04-26 12:28:47 +0200

    fill last discussed with newer of last discussed and created

--- adhocracy4/comments/signals.py
+++ adhocracy4/comments/signals.py
@@ -11,8 +11,9 @@ generics.setup_delete_signals(settings.A4_COMMENTABLES, Comment)
 @receiver(post_save, sender=Comment)
 def update_last_discussed(sender, instance, **kwargs):
     if hasattr(instance.content_object, 'last_discussed'):
-        if instance.modified:
-            instance.content_object.last_discussed = instance.modified
-        else:
-            instance.content_object.last_discussed = instance.created
+        last_discussed = instance.created
+        if (instance.last_discussed
+                and instance.last_discussed > last_discussed):
+            last_discussed = instance.last_discussed
+        instance.content_object.last_discussed = last_discussed
         instance.content_object.save(ignore_modified=True)
