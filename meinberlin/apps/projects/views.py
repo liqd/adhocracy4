08872376742985af416575cc7a70c61600ec44aa
Magdalena Noffke <magdalena.noffke@liqd.de>
2019-07-03 12:01:47 +0200

    apps/projects: fix containers

--- meinberlin/apps/projects/views.py
+++ meinberlin/apps/projects/views.py
@@ -362,8 +362,10 @@ class ProjectDetailView(PermissionRequiredMixin,
     def _view_by_phase(self):
         if self.module.last_active_phase:
             return self.module.last_active_phase.view.as_view()
-        else:
+        elif self.module.future_phases:
             return self.module.future_phases.first().view.as_view()
+        else:
+            return super().dispatch
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
