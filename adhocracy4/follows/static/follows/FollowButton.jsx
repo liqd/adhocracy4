29818e270c3c6bc8a4cccf1d584f70415d91fffc
Phillippa <p.morland@liqd.net>
2019-09-10 15:44:24 +0200

    follow: add alert for follow and unfollow with different wording

--- adhocracy4/follows/static/follows/FollowButton.jsx
+++ adhocracy4/follows/static/follows/FollowButton.jsx
@@ -2,6 +2,7 @@ var api = require('../../../static/api')
 var django = require('django')
 var PropTypes = require('prop-types')
 var React = require('react')
+var Alert = require('../../../static/Alert')
 
 class FollowButton extends React.Component {
   constructor (props) {
@@ -9,16 +10,33 @@ class FollowButton extends React.Component {
 
     this.state = {
       followed: undefined,
-      follows: 0
+      follows: 0,
+      alert: null
     }
   }
 
+  removeAlert () {
+    this.setState({
+      alert: null
+    })
+  }
+
   toggleFollow () {
+    var messages
+    if (this.state.followed) {
+      messages = django.gettext('You will no longer be notified about this project')
+    } else {
+      messages = django.gettext('Thank you for your interest! From now on you will be kept up to date via e-mail.')
+    }
     api.follow.change({ enabled: !this.state.followed }, this.props.project)
       .done((follow) => {
         this.setState({
           followed: follow.enabled,
-          follows: follow.follows
+          follows: follow.follows,
+          alert: {
+            type: 'success',
+            message: messages
+          }
         })
       })
   }
@@ -28,13 +46,18 @@ class FollowButton extends React.Component {
       .done((follow) => {
         this.setState({
           followed: follow.enabled,
-          follows: follow.follows
+          follows: follow.follows,
+          alert: follow.alert
         })
       })
       .fail((response) => {
         if (response.status === 404) {
           this.setState({
-            followed: false
+            followed: false,
+            alert: {
+              type: 'danger',
+              message: django.gettext('You are not following')
+            }
           })
         }
       })
@@ -43,9 +66,10 @@ class FollowButton extends React.Component {
   render () {
     return (
       <span>
-        <button className={this.state.followed ? 'btn btn-sm btn-secondary' : 'btn btn-sm btn-transparent'} type="button" onClick={this.toggleFollow.bind(this)}>
+        <button className={this.state.followed ? 'btn btn--sm btn--transparent' : 'btn btn--sm btn--secondary'} type="button" onClick={this.toggleFollow.bind(this)}>
           <i className={this.state.followed ? 'fa fa-check' : 'fa fa-plus'} aria-hidden="true" />&nbsp;<span className="follow__btn--content">{this.state.followed ? django.gettext('Following') : django.gettext('Follow project')}</span>
         </button>
+        <Alert onClick={this.removeAlert.bind(this)} {...this.state.alert} />
       </span>
     )
   }
