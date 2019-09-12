606e7171987141292639b427b7a10141527fc377
Phillippa <p.morland@liqd.net>
2019-09-12 13:26:12 +0200

    static: translatable strings to variables

--- adhocracy4/static/Alert.jsx
+++ adhocracy4/static/Alert.jsx
@@ -2,13 +2,14 @@ var React = require('react')
 var django = require('django')
 
 const Alert = ({type, message, onClick}) => {
+  const closeTag = django.gettext('Close')
   if (type) {
     return (
       <div className={`alert alert--${type}`} role="alert" onClick={onClick}>
         <div className="l-wrapper">
           {message}
-          <button className="alert__close" title={django.gettext('Close')}>
-            <i className="fa fa-times" aria-label={django.gettext('Close')} />
+          <button className="alert__close" title={closeTag}>
+            <i className="fa fa-times" aria-label={closeTag} />
           </button>
         </div>
       </div>
