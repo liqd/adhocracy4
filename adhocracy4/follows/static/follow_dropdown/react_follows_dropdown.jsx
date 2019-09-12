c845687031da2d67ac4ee2ee952fce3bc77a598d
Phillippa <p.morland@liqd.net>
2019-09-12 15:07:21 +0200

    old follow: removing dropdown follow component

--- adhocracy4/follows/static/follow_dropdown/react_follows_dropdown.jsx
+++
@@ -1,8 +0,0 @@
-var FollowDropdown = require('./FollowDropdown')
-var React = require('react')
-var ReactDOM = require('react-dom')
-
-module.exports.renderFollow = function (el) {
-  let project = el.getAttribute('data-project')
-  ReactDOM.render(<FollowDropdown project={project} />, el)
-}
