cc5fd2adcc0a25f09f8808d3941e53855af89930
Robert Mader <r.mader@liqd.net>
2019-09-12 17:08:18 +0200

    notifications: Add 'phase started' email
    Using a new action from A4, added on each phase start.

--- meinberlin/apps/notifications/signals.py
+++ meinberlin/apps/notifications/signals.py
@@ -25,8 +25,11 @@ def send_notifications(instance, created, **kwargs):
         if action.project:
             emails.NotifyModeratorsEmail.send(action)
 
-    elif action.type == 'phase' and verb == Verbs.SCHEDULE:
-        emails.NotifyFollowersOnPhaseIsOverSoonEmail.send(action)
+    elif action.type == 'phase':
+        if verb == Verbs.Start:
+            emails.NotifyFollowersOnPhaseStartedEmail.send(action)
+        elif verb == Verbs.SCHEDULE:
+            emails.NotifyFollowersOnPhaseIsOverSoonEmail.send(action)
 
 
 @receiver(dashboard_signals.project_created)
