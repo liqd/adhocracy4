6220b5b28b35b87b4a569d1cae46d232c3f561d6
fuzzylogic2000 <katharina.albers@liqd.de>
2019-11-01 13:48:39 +0100

    apps/dashboard: add signals for publish and unpublish module

--- adhocracy4/dashboard/signals.py
+++ adhocracy4/dashboard/signals.py
@@ -6,6 +6,8 @@ module_created = dispatch.Signal(providing_args=['module', 'user'])
 project_pre_publish = dispatch.Signal(providing_args=['project', 'user'])
 project_published = dispatch.Signal(providing_args=['project', 'user'])
 project_unpublished = dispatch.Signal(providing_args=['project', 'user'])
+module_published = dispatch.Signal(providing_args=['module', 'user'])
+module_unpublished = dispatch.Signal(providing_args=['module', 'user'])
 project_component_updated = dispatch.Signal(
     providing_args=['project', 'component', 'user'])
 module_component_updated = dispatch.Signal(
