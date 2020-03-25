2f928721c3714b6b68118d3311ca83b58c30eea2
Rineee <k.schmid@liqd.net>
2020-03-25 15:45:15 +0100

    apps/projects/emails: change e-mail wording and add contact_email to context

--- meinberlin/apps/projects/emails.py
+++ meinberlin/apps/projects/emails.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from meinberlin.apps.contrib.emails import Email
 
 
@@ -7,9 +9,19 @@ class InviteParticipantEmail(Email):
     def get_receivers(self):
         return [self.object.email]
 
+    def get_context(self):
+        context = super().get_context()
+        context['contact_email'] = settings.CONTACT_EMAIL
+        return context
+
 
 class InviteModeratorEmail(Email):
     template_name = 'meinberlin_projects/emails/invite_moderator'
 
     def get_receivers(self):
         return [self.object.email]
+
+    def get_context(self):
+        context = super().get_context()
+        context['contact_email'] = settings.CONTACT_EMAIL
+        return context
