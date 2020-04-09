25c07db153d0879e1b4444e725935faccd264ff3
Robert Mader <robert.mader@posteo.de>
2020-04-09 14:43:51 +0200

    apps/account/forms: Check username for case insensitive uniqueness

--- meinberlin/apps/account/forms.py
+++ meinberlin/apps/account/forms.py
@@ -8,3 +8,15 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['username', 'get_notifications', 'get_newsletters']
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        try:
+            user = User.objects.get(username__iexact=username)
+            if user != self.instance:
+                raise forms.ValidationError(
+                    User._meta.get_field('username').error_messages['unique'])
+        except User.DoesNotExist:
+            pass
+
+        return username
