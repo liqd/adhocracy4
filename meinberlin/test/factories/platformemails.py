c6a53941c3e4c24586ddad1350b93ad8fd75892b
Rineee <k.schmid@liqd.net>
2020-05-22 11:55:37 +0200

    test/factories: add platform email factory

---
+++ meinberlin/test/factories/platformemails.py
@@ -0,0 +1,16 @@
+import factory
+
+from adhocracy4.test import factories as a4_factories
+from meinberlin.apps.platformemails import models as platformemail_models
+
+
+class PlatformEmailFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = platformemail_models.PlatformEmail
+
+    sender = factory.Faker('email')
+    sender_name = factory.Faker('name')
+    subject = factory.Faker('sentence', nb_words=4)
+    body = factory.Faker('text')
+    creator = factory.SubFactory(a4_factories.USER_FACTORY)
