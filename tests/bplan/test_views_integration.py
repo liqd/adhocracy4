e0398359551391e07cf06be89dc794eb4b9c9eb3
fuzzylogic2000 <katharina.albers@liqd.de>
2020-07-13 15:26:52 +0200

    tests/bplan: test statement form dispatch after phase over

--- tests/bplan/test_views_integration.py
+++ tests/bplan/test_views_integration.py
@@ -4,6 +4,7 @@ from adhocracy4.test.helpers import redirect_target
 from meinberlin.apps.bplan import phases
 from meinberlin.test.helpers import assert_template_response
 from meinberlin.test.helpers import freeze_phase
+from meinberlin.test.helpers import freeze_post_phase
 
 
 @pytest.mark.django_db
@@ -27,3 +28,16 @@ def test_statement_form_view(client, phase_factory, bplan_factory,
         }
         response = client.post(url, statement)
         assert redirect_target(response) == 'statement-sent'
+
+
+@pytest.mark.django_db
+def test_statement_form_view_post_phase(client, phase_factory,
+                                        bplan_factory, module_factory):
+    bplan = bplan_factory(is_draft=False)
+    module = module_factory(project=bplan)
+    phase = phase_factory(phase_content=phases.StatementPhase(), module=module)
+    url = bplan.get_absolute_url()
+    with freeze_post_phase(phase):
+        response = client.get(url)
+        assert response.status_code == 302
+        assert redirect_target(response) == 'finished'
