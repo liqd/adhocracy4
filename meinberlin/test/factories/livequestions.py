3f3885b0570fd4bf1b269106696f338c674ff55d
Rineee <k.schmid@liqd.net>
2020-09-09 17:43:34 +0200

    test/factories: add livequestion factory

---
+++ meinberlin/test/factories/livequestions.py
@@ -0,0 +1,15 @@
+import factory
+
+from adhocracy4.test import factories as a4_factories
+from meinberlin.apps.livequestions import models
+from meinberlin.test.factories import CategoryFactory
+
+
+class LiveQuestionFactory(factory.django.DjangoModelFactory):
+
+    class Meta:
+        model = models.LiveQuestion
+
+    text = factory.Faker('text', max_nb_chars=50)
+    category = factory.SubFactory(CategoryFactory)
+    module = factory.SubFactory(a4_factories.ModuleFactory)
