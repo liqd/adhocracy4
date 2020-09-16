611621b560f87f3e50ddf1678f53ed0f71aa8330
Rineee <k.schmid@liqd.net>
2020-09-16 17:52:58 +0200

    tests/livequestions: test for templatetags

---
+++ tests/livequestions/test_livequestion_templatetags.py
@@ -0,0 +1,46 @@
+import pytest
+from django.urls import reverse
+
+from adhocracy4.test.helpers import render_template
+
+
+@pytest.mark.django_db
+def test_react_questions(rf, user, live_question):
+    request = rf.get('/')
+    request.user = user
+    module = live_question.module
+    template = '{% load react_questions %}{% react_questions module %}'
+    context = {'request': request, "module": module}
+
+    questions_api_url = reverse('questions-list',
+                                kwargs={'module_pk': module.pk})
+    present_url = reverse('meinberlin_livequestions:question-present',
+                          kwargs={'module_slug': module.slug})
+    ask_question_url = reverse('meinberlin_livequestions:question-create',
+                               kwargs={'slug': module.slug})
+    rendered = render_template(template, context)
+
+    assert rendered.startswith('<div data-ie-widget="questions"')
+    assert module.description.replace('\n', '\\n') in rendered
+    assert questions_api_url in rendered
+    assert present_url in rendered
+    assert live_question.category.name in rendered
+    assert ask_question_url in rendered
+
+
+@pytest.mark.django_db
+def test_react_questions_present(rf, user, live_question):
+    request = rf.get('/')
+    request.user = user
+    module = live_question.module
+    template = '{% load react_questions %}{% react_questions_present module %}'
+    context = {'request': request, "module": module}
+
+    questions_api_url = reverse('questions-list',
+                                kwargs={'module_pk': module.pk})
+    rendered = render_template(template, context)
+
+    assert rendered.startswith('<div data-ie-widget="present"')
+    assert questions_api_url in rendered
+    assert live_question.category.name in rendered
+    assert module.project.name in rendered
