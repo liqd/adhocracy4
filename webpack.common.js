5f4c92278f65f7efa9b99eb1b965259d4df3dbad
Robert Mader <r.mader@liqd.net>
2020-10-14 15:02:00 +0200

    webpack: Also optimize dsgvo-video-embed
    We use our own fork and it doesn't look trivial to always build
    optimized code. Just use the source and optimize it in webpack.

--- webpack.common.js
+++ webpack.common.js
@@ -98,7 +98,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules\/(?!(adhocracy4)\/).*/, // exclude most dependencies
+        exclude: /node_modules\/(?!(adhocracy4|dsgvo-video-embed)\/).*/, // exclude most dependencies
         loader: 'babel-loader',
         options: {
           presets: ['@babel/preset-env', '@babel/preset-react'].map(require.resolve),
