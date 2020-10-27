37f2d213924989086f5bd73e2fad172d883e22fb
Rineee <k.schmid@liqd.net>
2020-10-27 09:21:22 +0100

    projects/rules: allow viewing of semipublic projects
    And add missing default rules so they can always be used.

--- adhocracy4/projects/rules.py
+++ adhocracy4/projects/rules.py
@@ -6,9 +6,11 @@ from adhocracy4.organisations.predicates import is_org_group_member
 from adhocracy4.organisations.predicates import is_org_member
 
 from .predicates import is_live
+from .predicates import is_moderator
 from .predicates import is_prj_group_member
 from .predicates import is_project_member
 from .predicates import is_public
+from .predicates import is_semipublic
 
 rules.add_perm('a4projects.add_project',
                is_superuser | is_initiator | is_org_group_member)
@@ -20,4 +22,14 @@ rules.add_perm('a4projects.change_project',
 
 rules.add_perm('a4projects.view_project',
                is_superuser | is_initiator | is_prj_group_member |
-               ((is_public | is_project_member | is_org_member) & is_live))
+               is_moderator | ((is_public | is_semipublic | is_org_member |
+                                is_project_member) & is_live))
+
+
+rules.add_perm('a4projects.participate_in_project',
+               is_superuser | is_initiator | is_prj_group_member |
+               is_moderator | ((is_public | is_org_member |
+                                is_project_member) & is_live))
+
+rules.add_perm('a4projects.delete_project',
+               is_superuser | is_initiator)
