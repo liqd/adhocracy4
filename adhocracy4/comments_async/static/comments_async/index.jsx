a168bb0ace563a728b256328294dd9abcf555c2c
Robert Mader <robert.mader@posteo.de>
2021-02-23 16:15:11 +0100

    comments_async: Do not export 'renderComment' as default
    It broke comment initialization in a+:
    `ReactWidget.initialise('a4', 'comment_async', ReactCommentsAsync.renderComment)`

--- adhocracy4/comments_async/static/comments_async/index.jsx
+++ adhocracy4/comments_async/static/comments_async/index.jsx
@@ -3,10 +3,7 @@ import ReactDOM from 'react-dom'
 
 import CommentBox from './comment_box'
 
-function renderComment (el) {
+export function renderComment (el) {
   const props = JSON.parse(el.getAttribute('data-attributes'))
   ReactDOM.render(<CommentBox {...props} ref={(commentbox) => { window.commentbox = commentbox }} />, el)
 }
-
-export { renderComment }
-export default renderComment
