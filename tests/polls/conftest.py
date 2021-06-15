525f15bda8b6aa6db8c20b7ff123e142dd3ae542
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-06-15 16:35:12 +0200

    tests/polls: add tests for the new model fields

--- tests/polls/conftest.py
+++ tests/polls/conftest.py
@@ -4,5 +4,7 @@ from tests.polls import factories as poll_factories
 
 register(poll_factories.PollFactory)
 register(poll_factories.QuestionFactory)
+register(poll_factories.AnswerFactory)
 register(poll_factories.ChoiceFactory)
 register(poll_factories.VoteFactory)
+register(poll_factories.OtherVoteFactory)
