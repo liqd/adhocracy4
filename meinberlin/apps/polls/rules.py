b3836bc893afdc2880ccfdcac57f54d96ae6e167
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-06-28 14:17:38 +0200

    apps/polls: use view_poll rule from a4 - it has been changed to be the same

--- meinberlin/apps/polls/rules.py
+++ meinberlin/apps/polls/rules.py
@@ -7,9 +7,3 @@ rules.set_perm(
     module_predicates.is_context_initiator |
     module_predicates.is_context_moderator
 )
-
-rules.set_perm(
-    'a4polls.view_poll',
-    (module_predicates.is_project_admin |
-     module_predicates.is_allowed_view_item)
-)
