0c573b59f48b32dc669bad5af9032d4d3f8ffbd6
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-07-01 14:46:26 +0200

    tests/polls: move poll tests from mB

---
+++ tests/polls/dashboard_components/test_views_module_polls.py
@@ -0,0 +1,20 @@
+import pytest
+
+from adhocracy4.dashboard import components
+from adhocracy4.polls.phases import VotingPhase
+from adhocracy4.test.helpers import setup_phase
+
+component = components.modules.get('polls')
+
+
+@pytest.mark.django_db
+def test_edit_view(client, phase_factory, user_factory):
+    phase, module, project, item = setup_phase(
+        phase_factory, None, VotingPhase)
+    initiator = user_factory()
+    project.organisation.initiators.add(initiator)
+    url = component.get_base_url(module)
+    client.login(username=initiator.username, password='password')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.template_name[0] == 'a4polls/poll_dashboard.html'
