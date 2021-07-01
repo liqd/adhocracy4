3631eb29fffd83ffa35ea88b2cea8146ba1bc98d
rine <k.schmid@liqd.net>
2021-07-01 14:02:17 +0200

    tests/polls/test_vote_api: test create and update of open answers and other choice answers

--- tests/polls/test_vote_api.py
+++ tests/polls/test_vote_api.py
@@ -2,6 +2,8 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 
+from adhocracy4.polls.models import Answer
+from adhocracy4.polls.models import OtherVote
 from adhocracy4.polls.models import Vote
 from adhocracy4.polls.phases import VotingPhase
 from adhocracy4.projects.enums import Access
@@ -217,3 +219,161 @@ def test_participant_can_vote_in_private_project(user,
         assert response.status_code == status.HTTP_201_CREATED
 
     assert Vote.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_other_choice_vote_created(user,
+                                   question,
+                                   apiclient,
+                                   choice_factory):
+
+    choice_factory(question=question)
+    choice_other = choice_factory(question=question, is_other_choice=True)
+
+    assert Vote.objects.count() == 0
+    assert OtherVote.objects.count() == 0
+
+    apiclient.force_authenticate(user=user)
+
+    url = reverse(
+        'votes-list',
+        kwargs={
+            'question_pk': question.pk
+        })
+
+    data = {
+        'choices': [choice_other.pk],
+        'other_choice_answer': 'other choice answer',
+        'open_answer': ''
+    }
+
+    with active_phase(question.poll.module, VotingPhase):
+        response = apiclient.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+    assert Vote.objects.count() == 1
+    assert OtherVote.objects.count() == 1
+    assert OtherVote.objects.first().vote == Vote.objects.first()
+    assert OtherVote.objects.first().answer == 'other choice answer'
+
+
+@pytest.mark.django_db
+def test_other_choice_vote_updated(user,
+                                   question,
+                                   apiclient,
+                                   choice_factory):
+
+    choice_factory(question=question)
+    choice_other = choice_factory(question=question, is_other_choice=True)
+
+    assert Vote.objects.count() == 0
+    assert OtherVote.objects.count() == 0
+
+    apiclient.force_authenticate(user=user)
+
+    url = reverse(
+        'votes-list',
+        kwargs={
+            'question_pk': question.pk
+        })
+
+    data = {
+        'choices': [choice_other.pk],
+        'other_choice_answer': 'other choice answer',
+        'open_answer': ''
+    }
+
+    with active_phase(question.poll.module, VotingPhase):
+        response = apiclient.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+    assert Vote.objects.count() == 1
+    assert OtherVote.objects.count() == 1
+    assert OtherVote.objects.first().vote == Vote.objects.first()
+    assert OtherVote.objects.first().answer == 'other choice answer'
+
+    data = {
+        'choices': [choice_other.pk],
+        'other_choice_answer': 'other choice answer updated',
+        'open_answer': ''
+    }
+
+    with active_phase(question.poll.module, VotingPhase):
+        response = apiclient.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+    assert Vote.objects.count() == 1
+    assert OtherVote.objects.count() == 1
+    assert OtherVote.objects.first().vote == Vote.objects.first()
+    assert OtherVote.objects.first().answer == 'other choice answer updated'
+
+
+@pytest.mark.django_db
+def test_answer_created(user,
+                        open_question,
+                        apiclient):
+
+    assert Answer.objects.count() == 0
+
+    apiclient.force_authenticate(user=user)
+
+    url = reverse(
+        'votes-list',
+        kwargs={
+            'question_pk': open_question.pk
+        })
+
+    data = {
+        'choices': [],
+        'other_choice_answer': '',
+        'open_answer': 'answer to open question'
+    }
+
+    with active_phase(open_question.poll.module, VotingPhase):
+        response = apiclient.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+    assert Answer.objects.count() == 1
+    assert Answer.objects.first().answer == 'answer to open question'
+
+
+@pytest.mark.django_db
+def test_answer_updated(user,
+                        open_question,
+                        apiclient):
+
+    assert Answer.objects.count() == 0
+
+    apiclient.force_authenticate(user=user)
+
+    url = reverse(
+        'votes-list',
+        kwargs={
+            'question_pk': open_question.pk
+        })
+
+    data = {
+        'choices': [],
+        'other_choice_answer': '',
+        'open_answer': 'answer to open question'
+    }
+
+    with active_phase(open_question.poll.module, VotingPhase):
+        response = apiclient.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+    assert Answer.objects.count() == 1
+    assert Answer.objects.first().answer == 'answer to open question'
+
+    data = {
+        'choices': [],
+        'other_choice_answer': '',
+        'open_answer': 'answer to open question updated'
+    }
+
+    with active_phase(open_question.poll.module, VotingPhase):
+        response = apiclient.post(url, data, format='json')
+        assert response.status_code == status.HTTP_201_CREATED
+
+    assert Answer.objects.count() == 1
+    assert Answer.objects.first().answer == 'answer to open question updated'
