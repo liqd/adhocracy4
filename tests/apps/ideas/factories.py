b9391cc63c950c1cb3547009a3e7b8080fc605d6
rine <k.schmid@liqd.net>
2021-07-09 17:44:34 +0200

    tests/apps/ideas: add labels, moderator_feedback, moderator_statement and reference_number to idea test model

--- tests/apps/ideas/factories.py
+++ tests/apps/ideas/factories.py
@@ -1,6 +1,7 @@
 import factory
 
 from adhocracy4.test import factories
+from tests.apps.moderatorfeedback.factories import ModeratorStatementFactory
 
 from . import models
 
@@ -9,6 +10,9 @@ class IdeaFactory(factories.ItemFactory):
     class Meta:
         model = models.Idea
 
+    moderator_statement = factory.SubFactory(ModeratorStatementFactory)
+    moderator_feedback = ('CONSIDERATION', 'Under consideration')
+
     point_label = factory.Faker('address')
     point = {
         'type': 'Feature',
@@ -16,3 +20,14 @@ class IdeaFactory(factories.ItemFactory):
         'geometry': {'type': 'Point',
                      'coordinates': [13.447437286376953, 52.51518602243137]}
     }
+
+    @factory.post_generation
+    def labels(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+        if extracted:
+            # A list of labels were passed in, use them
+            for label in extracted:
+                self.labels.add(label)
