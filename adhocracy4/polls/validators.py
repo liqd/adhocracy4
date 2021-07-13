ad55d83ed624cc0470bac4a2f2532a2787dcaaaa
rine <k.schmid@liqd.net>
2021-07-13 11:28:36 +0200

    polls/validators: use ValidationError from rest_framework and remove unused validator

--- adhocracy4/polls/validators.py
+++ adhocracy4/polls/validators.py
@@ -1,6 +1,6 @@
-from django.core.exceptions import NON_FIELD_ERRORS
-from django.core.exceptions import ValidationError
+from django.core import exceptions as django_exceptions
 from django.utils.translation import ugettext as _
+from rest_framework import exceptions as rest_exceptions
 
 
 def single_item_per_module(module, model, pk=None):
@@ -10,40 +10,18 @@ def single_item_per_module(module, model, pk=None):
         siblings = siblings.exclude(pk=pk)
 
     if len(siblings) > 0:
-        raise ValidationError({
-            NON_FIELD_ERRORS: [
+        raise django_exceptions.ValidationError({
+            django_exceptions.NON_FIELD_ERRORS: [
                 _('Item of type %(item)s for that module already exists') % {
                     'item': model.__name__
                 }]
         })
 
 
-def single_vote_per_user(user, choice, pk=None):
-    from .models import Vote  # avoid circular import
-
-    if choice.question.multiple_choice:
-        # Allow multiple votes per user for multiple choice questions.
-        return
-
-    qs = Vote.objects\
-        .filter(choice__question=choice.question)\
-        .filter(creator=user)
-
-    if pk:
-        qs = qs.exclude(pk=pk)
-
-    if qs.exists():
-        raise ValidationError({
-            NON_FIELD_ERRORS: [
-                _('Only one vote per question is allowed per user.'),
-            ]
-        })
-
-
 def choice_belongs_to_question(choice, question_pk):
     if question_pk != choice.question.pk:
-        raise ValidationError({
-            NON_FIELD_ERRORS: [
+        raise rest_exceptions.ValidationError({
+            'choice': [
                 _('Choice has to belong to the question set in the url.'),
             ]
         })
