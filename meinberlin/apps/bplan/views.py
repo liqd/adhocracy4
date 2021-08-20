60a819a68663339b8b76b4d72b2d3b383a5d96be
sabinammm <isobel@posteo.de>
2021-08-20 11:50:11 +0200

    apps/bplans: add free text filter to dashboard list

--- meinberlin/apps/bplan/views.py
+++ meinberlin/apps/bplan/views.py
@@ -1,17 +1,37 @@
 from django.utils.translation import ugettext_lazy as _
-from django.views import generic
 from django.views.generic import TemplateView
 
 from adhocracy4.dashboard.blueprints import ProjectBlueprint
 from adhocracy4.dashboard.components.forms.views import \
     ProjectComponentFormView
 from adhocracy4.dashboard.mixins import DashboardBaseMixin
+from adhocracy4.filters import views as filter_views
+from adhocracy4.filters import widgets as filter_widgets
+from adhocracy4.filters.filters import DefaultsFilterSet
+from adhocracy4.filters.filters import FreeTextFilter
 from meinberlin.apps.bplan import phases as bplan_phases
 from meinberlin.apps.dashboard.mixins import DashboardProjectListGroupMixin
 from meinberlin.apps.extprojects.views import ExternalProjectCreateView
 
 from . import forms
 from . import models
+
+
+class FreeTextFilterWidget(filter_widgets.FreeTextFilterWidget):
+    label = _('Search')
+
+
+class BPlanFilterSet(DefaultsFilterSet):
+    defaults = {}
+
+    search = FreeTextFilter(
+        widget=FreeTextFilterWidget,
+        fields=['name']
+    )
+
+    class Meta:
+        model = models.Bplan
+        fields = ['search']
 
 
 class BplanStatementSentView(TemplateView):
@@ -58,12 +78,13 @@ class BplanProjectUpdateView(ProjectComponentFormView):
 
 class BplanProjectListView(DashboardProjectListGroupMixin,
                            DashboardBaseMixin,
-                           generic.ListView):
+                           filter_views.FilteredListView):
     model = models.Bplan
     paginate_by = 12
     template_name = 'meinberlin_bplan/bplan_list_dashboard.html'
     permission_required = 'a4projects.add_project'
     menu_item = 'project'
+    filter_set = BPlanFilterSet
 
     def get_queryset(self):
         return super().get_queryset().filter(
