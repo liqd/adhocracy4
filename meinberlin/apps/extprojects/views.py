b02e2728908a5c6cbc02318a835ef71a6a3f77bc
sabinammm <isobel@posteo.de>
2021-08-20 13:30:36 +0200

    apps/extprojects: add free text filter to dashboard list

--- meinberlin/apps/extprojects/views.py
+++ meinberlin/apps/extprojects/views.py
@@ -1,17 +1,37 @@
 from django.utils.translation import ugettext_lazy as _
-from django.views import generic
 
 from adhocracy4.dashboard.blueprints import ProjectBlueprint
 from adhocracy4.dashboard.components.forms.views import \
     ProjectComponentFormView
 from adhocracy4.dashboard.mixins import DashboardBaseMixin
 from adhocracy4.dashboard.views import ProjectCreateView
+from adhocracy4.filters import views as filter_views
+from adhocracy4.filters import widgets as filter_widgets
+from adhocracy4.filters.filters import DefaultsFilterSet
+from adhocracy4.filters.filters import FreeTextFilter
 from meinberlin.apps.dashboard.mixins import DashboardProjectListGroupMixin
 from meinberlin.apps.extprojects import phases as extprojects_phases
 
 from . import apps
 from . import forms
 from . import models
+
+
+class FreeTextFilterWidget(filter_widgets.FreeTextFilterWidget):
+    label = _('Search')
+
+
+class ExternalProjectFilterSet(DefaultsFilterSet):
+    defaults = {}
+
+    search = FreeTextFilter(
+        widget=FreeTextFilterWidget,
+        fields=['name']
+    )
+
+    class Meta:
+        model = models.ExternalProject
+        fields = ['search']
 
 
 class ExternalProjectCreateView(ProjectCreateView):
@@ -51,12 +71,13 @@ class ExternalProjectUpdateView(ProjectComponentFormView):
 
 class ExternalProjectListView(DashboardProjectListGroupMixin,
                               DashboardBaseMixin,
-                              generic.ListView):
+                              filter_views.FilteredListView):
     model = models.ExternalProject
     paginate_by = 12
     template_name = 'meinberlin_extprojects/extproject_list_dashboard.html'
     permission_required = 'a4projects.add_project'
     menu_item = 'project'
+    filter_set = ExternalProjectFilterSet
 
     def get_queryset(self):
         project_type = '{}.{}'.format(
