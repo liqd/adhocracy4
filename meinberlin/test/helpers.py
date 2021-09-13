478017ec75b5775333a758808d5bd424c0cd0832
sabinammm <isobel@posteo.de>
2021-09-13 15:58:42 +0200

    tests: delete unused project helpers

--- meinberlin/test/helpers.py
+++ meinberlin/test/helpers.py
@@ -1,10 +1,6 @@
-import os
-
 import factory
-from django.conf import settings
 from django.db.models.signals import post_save
 from django.utils.encoding import smart_text
-from easy_thumbnails.files import get_thumbnailer
 
 from adhocracy4.test.helpers import redirect_target
 
@@ -28,13 +24,6 @@ def setup_group_member(organisation, project, group_factory,
     return group_member, organisation, project
 
 
-def assert_template_response(response, template_name, status_code=200):
-    assert response.status_code == status_code
-    response_template = response.template_name[0]
-    assert response_template == template_name, \
-        '{} != {}'.format(response_template, template_name)
-
-
 def assert_dashboard_form_component_response(
         response, component, status_code=200):
     assert response.status_code == status_code
@@ -50,12 +39,3 @@ def assert_dashboard_form_component_edited(
         attr = getattr(obj, key)
         value = data.get(key)
         assert attr == value, '{} != {}'.format(attr, value)
-
-
-def createThumbnail(imagefield):
-    thumbnailer = get_thumbnailer(imagefield)
-    thumbnail = thumbnailer.generate_thumbnail(
-        {'size': (800, 400), 'crop': 'smart'})
-    thumbnailer.save_thumbnail(thumbnail)
-    thumbnail_path = os.path.join(settings.MEDIA_ROOT, thumbnail.path)
-    return thumbnail_path
