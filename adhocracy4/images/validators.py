10f7be079ee6667709db59d55a41f29d56021a86
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-09-15 15:09:57 +0200

    apps/images: make sure validation also works for APIs

--- adhocracy4/images/validators.py
+++ adhocracy4/images/validators.py
@@ -15,6 +15,7 @@ def validate_image(
     min_width, min_height = min_resolution
 
     imagetype = magic.from_buffer(image.read(), mime=True)
+
     if imagetype.lower() not in fileformats:
         msg = _(
             'Unsupported file format. Supported formats are {}.'.format(
@@ -26,17 +27,27 @@ def validate_image(
         max_size_mb = math.floor(max_size / 10**6)
         msg = _('Image should be at most {max_size} MB')
         errors.append(ValidationError(msg.format(max_size=max_size_mb)))
-    if image.width < min_width:
+
+    if hasattr(image, 'width'):
+        image_width = image.width
+    else:
+        image_width = image.image.width
+    if image_width < min_width:
         msg = _('Image must be at least {min_width} pixels wide')
         errors.append(ValidationError(msg.format(min_width=min_width)))
-    if image.height < min_height:
+
+    if hasattr(image, 'height'):
+        image_height = image.height
+    else:
+        image_height = image.image.height
+    if image_height < min_height:
         msg = _('Image must be at least {min_height} pixels high')
         errors.append(ValidationError(msg.format(min_height=min_height)))
 
     if aspect_ratio:
         aspect_heigth, aspect_width = aspect_ratio
         target_ratio = aspect_heigth / aspect_width
-        actual_ratio = image.height / image.width
+        actual_ratio = image_height / image_width
 
         if abs(target_ratio - actual_ratio) > aspect_marign:
             msg = _(
