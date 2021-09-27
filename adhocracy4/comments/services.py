65b80742899ac0091be5ab7df314997b0da4f6b2
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-09-27 14:54:55 +0200

    apps/comments: remove unused services

--- adhocracy4/comments/services.py
+++
@@ -1,8 +0,0 @@
-from .models import Comment
-
-
-def delete_comments(contenttype, pk):
-    comments = Comment.objects.all().filter(
-        content_type=contenttype, object_pk=pk)
-    for comment in comments:
-        comment.delete()
