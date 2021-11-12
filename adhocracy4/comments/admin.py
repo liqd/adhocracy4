3a30aa9cd701416516f41a66e731151f70b51aea
sabinammm <isobel@posteo.de>
2021-11-12 11:54:30 +0100

    adhocracy4/comments: correct position of is_blocked field

--- adhocracy4/comments/admin.py
+++ adhocracy4/comments/admin.py
@@ -7,13 +7,13 @@ from .models import Comment
 class CommentAdmin(admin.ModelAdmin):
     fields = (
         'content_type', 'content_object', 'comment', 'is_removed',
-        'is_censored', 'is_moderator_marked', 'creator', 'comment_categories',
-        'is_blocked'
+        'is_censored', 'is_blocked', 'is_moderator_marked',
+        'creator', 'comment_categories'
     )
     readonly_fields = ('creator', 'content_type', 'content_object')
     list_display = (
-        '__str__', 'creator', 'is_removed', 'is_censored', 'created',
-        'is_blocked'
+        '__str__', 'creator', 'is_removed', 'is_censored',
+        'is_blocked', 'created'
     )
     search_fields = ('comment',)
     date_hierarchy = 'created'
