73a9d01832939580d3574ca037df1327eacba235
Kha <k.tran@liqd.net>
2021-11-17 11:11:48 +0000

    github/workflows/eslint: moving ecmascript linting to frontend livequestions/category-select: fix lint error in test file

--- meinberlin/apps/livequestions/assets/__tests__/CatergorySelect.jest.jsx
+++ meinberlin/apps/livequestions/assets/__tests__/CatergorySelect.jest.jsx
@@ -6,7 +6,7 @@ test('SelectCategory renders correctly', () => {
   const { asFragment } = render(
     <SelectCategory
       name="Category"
-      category_dict={{ 123: 'category1' }, { 124: 'category2' }}
+      category_dict={{ 123: 'category1', 124: 'category2' }}
     />)
   expect(asFragment()).toMatchSnapshot()
 })
