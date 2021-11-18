4dd4a6c3d907bba826b299b157d29440cd3de40c
Kha <k.tran@liqd.net>
2021-11-18 15:10:58 +0000

    budgeting/proposal-list/pagination: adding test to pagination

---
+++ meinberlin/apps/budgeting/assets/Pagination.jest.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { render, fireEvent, screen } from '@testing-library/react'
+import { Pagination } from './Pagination'
+
+test('clicking on page 2 returns value of 2', () => {
+  const onPageChangedFn = jest.fn()
+  render(
+    <Pagination
+      currPageIndex={1}
+      pageCount={3}
+      onPaginate={selPage => onPageChangedFn(selPage)}
+    />
+  )
+  const pageButton2 = screen.getByText('2')
+  fireEvent.click(pageButton2)
+  expect(onPageChangedFn).toHaveBeenCalledWith(2)
+})
