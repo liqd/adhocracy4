86932e8fed53732dada58c7989dcb415b8d465fc
rine <k.schmid@liqd.net>
2021-12-02 15:17:31 +0100

    budgeting/templatetags/react_proposal: fix is_voting_phase when no phase is active, fixes  #3988

--- meinberlin/apps/budgeting/templatetags/react_proposals.py
+++ meinberlin/apps/budgeting/templatetags/react_proposals.py
@@ -4,14 +4,21 @@ from django import template
 from django.urls import reverse
 from django.utils.html import format_html
 
+from adhocracy4.phases.predicates import has_feature_active
+from meinberlin.apps.budgeting.models import Proposal
+
 register = template.Library()
 
 
 @register.simple_tag(takes_context=True)
-def react_proposals(context, obj):
-    proposals_api_url = reverse('proposals-list', kwargs={'module_pk': obj.pk})
+def react_proposals(context, module):
+    proposals_api_url = reverse('proposals-list',
+                                kwargs={'module_pk': module.pk})
     attributes = {'proposals_api_url': proposals_api_url,
-                  'is_voting_phase': 'voting' in obj.active_phase.type}
+                  'is_voting_phase': has_feature_active(module,
+                                                        Proposal,
+                                                        'vote')
+                  }
 
     return format_html(
         '<div data-mb-widget="proposals" '
