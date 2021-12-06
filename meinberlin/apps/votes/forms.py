9a8690f064f56407fffbc3a31de9cfcb0b7f2c3b
sabinammm <isobel@posteo.de>
2021-12-06 13:49:21 +0100

    apps/votes/forms: added three-input-boxes widget to voting form
    apps/votes/forms: define minlength on VotingTokenWidget and remove maxlength from token form field to make it use the one from widget
    
    apps/votes/forms: removed size and n variable from widget

--- meinberlin/apps/votes/forms.py
+++ meinberlin/apps/votes/forms.py
@@ -4,9 +4,31 @@ from django.utils.translation import gettext_lazy as _
 from meinberlin.apps.votes.models import VotingToken
 
 
-class TokenForm(forms.Form):
+class VotingTokenWidget(forms.MultiWidget):
 
-    token = forms.CharField(max_length=40)
+    def __init__(self, attrs=None):
+        widgets = (
+            forms.TextInput(attrs={'minlength': '4',
+                                   'maxlength': '4'}),
+            forms.TextInput(attrs={'minlength': '4',
+                                   'maxlength': '4'}),
+            forms.TextInput(attrs={'minlength': '4',
+                                   'maxlength': '4'}),
+        )
+        super(VotingTokenWidget, self).__init__(widgets, attrs)
+
+    def decompress(self, value):
+        if value:
+            return [value[i:i + 4] for i in range(0, len(value), 4)]
+        return [None, None, None]
+
+    def value_from_datadict(self, data, files, name):
+        values = super().value_from_datadict(data, files, name)
+        return "".join(values)
+
+
+class TokenForm(forms.Form):
+    token = forms.CharField(widget=VotingTokenWidget())
 
     def __init__(self, *args, **kwargs):
         self.module_id = kwargs.pop('module_id')
