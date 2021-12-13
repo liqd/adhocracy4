e4798e69dd9f0329f2f3fb2eb697114a23fe89ac
Kha <k.tran@liqd.net>
2021-12-13 13:44:26 +0000

    apps/budgeting/tests: BudgetingProposalList.jest.jsx fixing annoying act(...) warning

--- meinberlin/apps/budgeting/assets/__tests__/BudgetingProposalList.jest.jsx
+++ meinberlin/apps/budgeting/assets/__tests__/BudgetingProposalList.jest.jsx
@@ -1,42 +1,80 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, act, screen } from '@testing-library/react'
 import { BudgetingProposalList } from '../BudgetingProposalList'
 
-// const data = {results: [
-//   {
-//     name: 'myProposal',
-//     url: 'www',
-//     budget: '80639',
-//     creator: 'creator',
-//     created: '2021-11-11T15:37:19.490201+01:00'
-//   },
-//   {
-//     name: 'This is a second proposal',
-//     url: 'www',
-//     budget: '12000',
-//     creator: 'creator part 2',
-//     created: '2021-10-29T09:33:36.280033+01:00'
-//   }
-// ]}
-
-// Mocks the fetch request
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () => Promise.resolve({ data: {} })
+test('Budgeting Proposal List without list item (empty)', async () => {
+  // mimicking fetch response with empty list
+  const mockedFetchEmpty = Promise.resolve({
+    json: () => Promise.resolve({ results: [] })
   })
-)
 
-// FIXME add test for when list is populated when know more about tests
-// test('Budgeting Proposal List component renders a list when there are items', () => {
-//   const { asFragment } = render(
-//     <BudgetingProposalList data={data} />
-//   )
-//   expect(asFragment()).toMatchSnapshot()
-// })
+  // overwrite global.fetch with mock function
+  global.fetch = jest.fn().mockImplementation(() => mockedFetchEmpty)
 
-test('Budgeting Proposal List component renders a list when there are no items', () => {
-  const { asFragment } = render(
-    <BudgetingProposalList />
-  )
-  expect(asFragment()).toMatchSnapshot()
+  render(<BudgetingProposalList />)
+  expect(global.fetch).toHaveBeenCalledTimes(1)
+
+  // waiting for async fetching ends --> without this, the
+  // act(...) console.error will appear
+  await act(() => mockedFetchEmpty)
+  const emptyList = screen.queryByText('mock text')
+  expect(emptyList).toBeTruthy()
+
+  // reverse overwrite of global.fetch
+  await global.fetch.mockClear()
+})
+
+test('Budgeting Proposal List with one list item', async () => {
+  // sample data (one proposal item)
+  const mockedResults = [
+    {
+      budget: 0,
+      category: { id: 3, name: 'candalf' },
+      comment_count: 0,
+      created: '2021-11-11T15:36:57.941072+01:00',
+      creator: 'admin',
+      is_archived: false,
+      locale: 'de-DE',
+      moderator_feedback: null,
+      moderator_feedback_choices: [],
+      name: 'erster vorschlag',
+      negative_rating_count: 0,
+      pk: 7,
+      positive_rating_count: 0,
+      url: '/budgeting/2021-00007/'
+    }
+  ]
+
+  // mimicking fetch response
+  const mockedFetch = Promise.resolve({
+    json: () => Promise.resolve({ results: mockedResults })
+  })
+
+  // overwrite global.fetch with mock function
+  global.fetch = jest.fn().mockImplementation(() => mockedFetch)
+
+  render(<BudgetingProposalList />)
+  expect(global.fetch).toHaveBeenCalledTimes(1)
+
+  // waiting for async fetching ends --> without this, the
+  // act(...) console.error will appear
+  await act(() => mockedFetch)
+  const listItem = screen.queryByText('erster vorschlag')
+  expect(listItem).toBeTruthy()
+
+  // reverse overwrite of global.fetch
+  await global.fetch.mockClear()
+})
+
+test('Budgeting Proposal List with fetch error', async () => {
+  // overwrite global.fetch with mock function
+  global.fetch = jest.fn().mockRejectedValue('testing: expected network error')
+
+  render(<BudgetingProposalList />)
+  expect(global.fetch).toHaveBeenCalledTimes(1)
+  const emptyList = screen.queryByText('mock text')
+  expect(emptyList).toBeTruthy()
+
+  // reverse overwrite of global.fetch
+  await global.fetch.mockClear()
 })
