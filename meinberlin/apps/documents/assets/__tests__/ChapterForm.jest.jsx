c8d76603dc1936902fad484dca6337da9825a8bc
Kha <k.tran@liqd.net>
2021-12-13 16:28:55 +0000

    apps/documents/tests: ChapterForm.jsx test added

---
+++ meinberlin/apps/documents/assets/__tests__/ChapterForm.jest.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import { render, screen, fireEvent } from '@testing-library/react'
+import ChapterForm from '../ChapterForm'
+
+// This mocks the child component <ParagraphForm> since we do not
+// want to provide all props to this child component
+// FIXME: Because of mocking ParagraphForm, some things cannot be tested
+// E.g. not able to reach 100% coverage for this component at the moment.
+// Note: () => () => {} to unwrap it properly.
+jest.mock('../ParagraphForm', () => () => <div>MockedParagraphForm</div>)
+
+test('Chapter Form without paragraphs', () => {
+  const mockedChapter = {
+    key: 'chapter_key',
+    name: 'chapter_name',
+    paragraphs: []
+  }
+  render(<ChapterForm id="chapter-form-id" chapter={mockedChapter} />)
+  const chapterLabel = screen.queryByLabelText('mock text')
+  expect(chapterLabel).toBeTruthy()
+})
+
+test('Chapter Form with paragraph', () => {
+  const mockedChapter = {
+    key: 'chapter_key',
+    name: 'chapter_name',
+    paragraphs: [
+      {
+        id: 1,
+        name: 'paragraph_title',
+        text: '<p>paragraph body text</p>'
+      }
+    ]
+  }
+  render(<ChapterForm id="chapter-form-id" chapter={mockedChapter} />)
+  const chapterLabel = screen.queryByLabelText('mock text')
+  expect(chapterLabel).toBeTruthy()
+})
+
+test('Chapter Form changing chapter title', () => {
+  const mockedFn = jest.fn()
+  const mockedChapter = {
+    key: 'chapter_key',
+    name: 'chapter_name',
+    paragraphs: []
+  }
+  render(
+    <ChapterForm
+      id="chapter-form-id"
+      chapter={mockedChapter}
+      onChapterNameChange={mockedFn}
+    />
+  )
+  const chapterNameInput = screen.getByDisplayValue('chapter_name')
+  fireEvent.change(chapterNameInput, { target: { value: 'changed_name_to_this' } })
+  expect(mockedFn).toHaveBeenCalledTimes(1)
+})
