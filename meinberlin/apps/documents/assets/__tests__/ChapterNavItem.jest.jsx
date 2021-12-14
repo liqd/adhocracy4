cc829a08c571559fbd2566dcd7c1ea1164fc45fc
Kha <k.tran@liqd.net>
2021-12-14 11:26:09 +0000

    apps/documents/tests: ChapterNavItem.jsx test added

--- meinberlin/apps/documents/assets/__tests__/ChapterNavItem.jest.jsx
+++ meinberlin/apps/documents/assets/__tests__/ChapterNavItem.jest.jsx
@@ -1,26 +1,19 @@
 // tools needed for testing
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import ChapterNavItem from '../ChapterNavItem'
 
 test('ChapterNavItem top renders correctly', () => {
   const { asFragment } = render(
-    <ChapterNavItem
-      name="Chapter name"
-      index={0}
-      active
-      onMoveUp={null}
-    />)
+    <ChapterNavItem name="Chapter name" index={0} active onMoveUp={null} />
+  )
   expect(asFragment()).toMatchSnapshot()
 })
 
 test('ChapterNavItem middle renders correctly', () => {
   const { asFragment } = render(
-    <ChapterNavItem
-      name="Chapter name"
-      index={2}
-      active={false}
-    />)
+    <ChapterNavItem name="Chapter name" index={2} active={false} />
+  )
   expect(asFragment()).toMatchSnapshot()
 })
 
@@ -31,6 +24,21 @@ test('ChapterNavItem bottom renders correctly', () => {
       index={3}
       active={false}
       onMoveDown={null}
-    />)
+    />
+  )
   expect(asFragment()).toMatchSnapshot()
+})
+
+test('ChapterNavItem error case', () => {
+  render(
+    <ChapterNavItem
+      name="Chapter name"
+      index={3}
+      active={false}
+      onMoveDown={null}
+      errors={{ paragraphs: ['paragraph1'] }}
+    />
+  )
+  const errorCountElement = screen.queryByText(/\s*\(\s*\d\s*\)/)
+  expect(errorCountElement).toBeTruthy()
 })
