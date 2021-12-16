db607281dbc6c61d0a7bd7be85035c37b5e15394
Kha <k.tran@liqd.net>
2021-12-16 16:07:43 +0000

    apps/budgeting: fix list items - moderatorfeedback

--- meinberlin/apps/budgeting/assets/ListItemBadges.jsx
+++ meinberlin/apps/budgeting/assets/ListItemBadges.jsx
@@ -2,39 +2,38 @@ import React from 'react'
 import { intComma } from './helpers'
 import { SpacedSpan } from './SpacedSpan'
 
-export const ListItemBadges = (props) => {
-  const getModeratorChoice = () => {
-    return props.moderatorChoices.find(choice => {
-      return choice[0] === props.moderatorFeedback
-    })
-  }
-
+export const ListItemBadges = props => {
   return (
     <div className="list-item__labels">
-      {props.category &&
+      {props.category && (
         <SpacedSpan className="label label--big">
           {props.category.name}
-        </SpacedSpan>}
-      {props.pointLabel &&
+        </SpacedSpan>
+      )}
+      {props.pointLabel && (
         <SpacedSpan className="label label--big">
           <i className="fas fa-map-marker-alt" aria-hidden="true" />
           {props.pointLabel}
-        </SpacedSpan>}
-      {props.budget > 0 &&
+        </SpacedSpan>
+      )}
+      {props.budget > 0 && (
         <SpacedSpan className="label label--big">
           {intComma(props.budget)}€
-        </SpacedSpan>}
-      {props.moderatorFeedback &&
+        </SpacedSpan>
+      )}
+      {props.moderatorFeedback[0] && (
         <SpacedSpan
           className={
             'label' +
             ' label--big' +
-            ' label--' + props.moderatorFeedback.toLowerCase() +
+            ' label--' +
+            props.moderatorFeedback[0].toLowerCase() +
             ' list-item__label--moderator-feedback'
-            }
+          }
         >
-          {getModeratorChoice()[1]}
-        </SpacedSpan>}
+          {props.moderatorFeedback[1]}
+        </SpacedSpan>
+      )}
     </div>
   )
 }
