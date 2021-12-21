b66fe16c7e80cf8e59447ae022258a4640bd03e7
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-12-21 12:09:39 +0100

    apps/modules/predicates: refactor predicates
    - improves naming of one predicate
    - fixes #927
    - adds project group members to the tests

--- adhocracy4/polls/rules.py
+++ adhocracy4/polls/rules.py
@@ -1,21 +1,17 @@
 import rules
-from rules.predicates import is_superuser
 
 from adhocracy4.modules import predicates as module_predicates
-from adhocracy4.projects import predicates as project_predicates
 
 from . import models
 
 rules.add_perm(
     'a4polls.change_poll',
-    is_superuser |
-    module_predicates.is_context_initiator |
-    project_predicates.is_prj_group_member
+    module_predicates.is_allowed_crud_project
 )
 
 rules.add_perm(
     'a4polls.view_poll',
-    (module_predicates.is_project_admin |
+    (module_predicates.is_allowed_moderate_project |
      module_predicates.is_allowed_view_item)
 )
 
--- adhocracy4/ratings/rules.py
+++ adhocracy4/ratings/rules.py
@@ -18,7 +18,7 @@ def content_object_allows_rating(user, rating):
 
 rules.add_perm(
     'a4ratings.change_rating',
-    module_predicates.is_project_admin |
+    module_predicates.is_allowed_moderate_project |
     (
         module_predicates.is_context_member &
         content_object_allows_rating &
@@ -29,7 +29,7 @@ rules.add_perm(
 
 rules.add_perm(
     'a4ratings.delete_rating',
-    module_predicates.is_project_admin |
+    module_predicates.is_allowed_moderate_project |
     (
         content_object_allows_rating &
         module_predicates.is_context_member &
