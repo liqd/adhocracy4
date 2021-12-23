36ea74d3964b3aea92a49309963656513fce0027
fuzzylogic2000 <k.lindenlaub@liqd.net>
2021-12-23 11:00:32 +0100

    [#5049] rules: make sure group members can access upload in CK editor and moderators cannot

--- meinberlin/apps/users/decorators.py
+++ meinberlin/apps/users/decorators.py
@@ -1,13 +1,13 @@
 from django.contrib.auth.decorators import user_passes_test
 
-from adhocracy4.projects.models import Project
 from meinberlin.apps.organisations.models import Organisation
 
 
 def user_is_project_admin(view_func):
     """Projet admin view decorator.
 
-    Checks that the user is an admin, moderator or initiator of any project.
+    Checks that the user is an admin, initiator or project group member
+    of any project.
     """
     return user_passes_test(
         _user_is_project_admin,
@@ -15,7 +15,7 @@ def user_is_project_admin(view_func):
 
 
 def _user_is_project_admin(user):
-    is_moderator = Project.objects.filter(moderators__id=user.id).exists()
+    is_group_member = user.groups.exists()
     is_initiator = Organisation.objects.filter(initiators__id=user.id).exists()
     return user.is_active and \
-        (user.is_superuser or is_initiator or is_moderator)
+        (user.is_superuser or is_initiator or is_group_member)
--- tests/users/test_decorators.py
+++ tests/users/test_decorators.py
@@ -5,9 +5,18 @@ from meinberlin.apps.users.decorators import _user_is_project_admin
 
 
 @pytest.mark.django_db
-def test_user_is_project_admin(project, user):
+def test_user_is_project_admin(project, user_factory, group_factory,
+                               admin):
     anonymous, moderator, initiator = setup_users(project)
+    user = user_factory()
+    group = group_factory()
+    group_member = user_factory.create(groups=(group,))
+    project.group = group
+    project.save()
+
     assert not _user_is_project_admin(anonymous)
     assert not _user_is_project_admin(user)
-    assert _user_is_project_admin(moderator)
+    assert not _user_is_project_admin(moderator)
+    assert _user_is_project_admin(group_member)
     assert _user_is_project_admin(initiator)
+    assert _user_is_project_admin(admin)
