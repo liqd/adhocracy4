f692509988f6def0c785ee99c1e3d0760b39d28c
Kha <k.tran@liqd.net>
2022-02-28 14:38:56 +0000

    comments_async/comment.jsx: using created or modified dates directly and remove helper file

--- adhocracy4/comments_async/static/comments_async/helpers.jsx
+++
@@ -1,4 +0,0 @@
-export function localeDate (dateStr) {
-  const options = { day: 'numeric', month: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' }
-  return new Date(dateStr).toLocaleString(document.documentElement.lang, options)
-}
