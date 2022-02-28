b6b39de0308fa4fa228089db528da57837e0ee94
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-02-28 16:05:15 +0100

    jest: update tests

--- meinberlin/apps/contrib/assets/CheckboxBtn.jsx
+++ meinberlin/apps/contrib/assets/CheckboxBtn.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default class SwitchButton extends React.Component {
+export default class CheckboxBtn extends React.Component {
   constructor (props) {
     super(props)
 
