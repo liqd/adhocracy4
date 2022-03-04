599d7ff8625a66e901e99fe4f43cffead1a305ee
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-03-04 13:19:10 +0100

    tests/comments: test async api PaginationCommentLinkMixin

--- tests/comments/test_async_serializer.py
+++ tests/comments/test_async_serializer.py
@@ -43,6 +43,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_1['user_name'] == ''
     # django auth user model does not define get_absolute_url()
     assert comment_1['user_profile_url'] == ''
+    assert comment_1['user_image'] is None
     assert comment_1['is_deleted'] is False
     assert comment_1['author_is_moderator'] is False
     assert len(comment_1['child_comments']) == 0
@@ -61,6 +62,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_2['id'] == 2
     assert comment_2['user_name'] == ''
     assert comment_2['user_profile_url'] == ''
+    assert comment_2['user_image'] is None
     assert comment_2['is_deleted'] is False
     assert comment_2['author_is_moderator'] is False
     assert len(comment_2['child_comments']) == 0
@@ -80,6 +82,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_3['id'] == 3
     assert comment_3['user_name'] == 'unknown user'
     assert comment_3['user_profile_url'] == ''
+    assert comment_3['user_image'] is None
     assert comment_3['is_deleted'] is True
     assert comment_3['author_is_moderator'] is False
     assert len(comment_3['child_comments']) == 0
@@ -99,6 +102,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_4['id'] == 4
     assert comment_4['user_name'] == 'unknown user'
     assert comment_4['user_profile_url'] == ''
+    assert comment_4['user_image'] is None
     assert comment_4['is_deleted'] is True
     assert comment_4['author_is_moderator'] is False
     assert len(comment_4['child_comments']) == 0
@@ -118,6 +122,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_5['id'] == 5
     assert comment_5['user_name'] == 'unknown user'
     assert comment_5['user_profile_url'] == ''
+    assert comment_5['user_image'] is None
     assert comment_5['is_deleted'] is True
     assert comment_5['author_is_moderator'] is False
     assert len(comment_5['child_comments']) == 0
@@ -137,6 +142,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_6['id'] == 6
     assert comment_6['user_name'] == ''
     assert comment_6['user_profile_url'] == ''
+    assert comment_6['user_image'] is None
     assert comment_6['is_deleted'] is False
     assert comment_6['author_is_moderator'] is False
     assert len(comment_6['child_comments']) == 0
@@ -155,6 +161,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_7['id'] == 7
     assert comment_7['is_deleted'] is False
     assert comment_7['author_is_moderator'] is True
+    assert comment_7['user_image'] is None
     assert len(comment_7['child_comments']) == 1
     assert comment_7['comment'] == Comment.objects.get(pk=7).comment
     assert comment_7['object_pk'] == question.pk
@@ -171,6 +178,7 @@ def test_serializer(apiclient, question, comment_factory, user):
     assert comment_8['id'] == 8
     assert comment_8['is_deleted'] is False
     assert comment_8['author_is_moderator'] is False
+    assert comment_8['user_image'] is None
     assert comment_8['comment'] == Comment.objects.get(pk=8).comment
     assert comment_8['object_pk'] == comment.pk
     assert comment_8['is_removed'] is False
