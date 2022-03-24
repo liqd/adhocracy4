25fef5a7b06c6b9d1b7b5d62763615db648637a2
sabinammm <isobel@posteo.de>
2022-03-24 09:46:05 +0100

    meinberlin/apps/contrib: add missing texts to account messages
    Fixes #4295

--- meinberlin/apps/contrib/django_standard_messages.py
+++ meinberlin/apps/contrib/django_standard_messages.py
@@ -12,7 +12,13 @@ django_standard_messages_to_override = [
       "you do not receive it within a few minutes."),
     _("We have sent an e-mail to you for verification. "
       "Follow the link provided to finalize the signup process. "
-      "Please contact us if you do not receive it within a few minutes."),
+      "If you do not see the verification e-mail in your main inbox, "
+      "check your spam folder. "
+      "Please contact us if you do not receive the verification e-mail "
+      "within a few minutes."),
+    _("We have sent you an e-mail. If you have not received it "
+      "please check your spam folder. Otherwise contact us if you "
+      "do not receive it in a few minutes."),
     _(u'You must select a minimum of  %(limit_value)d choices.'),
     _(u'You must select a maximum of  %(limit_value)d choices.'),
     _('Enter a valid email address.')
