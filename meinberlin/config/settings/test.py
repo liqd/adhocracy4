ce8afaa7da084e40bd1e5dc10cdca938c532861a
Julian Dehm <j.dehm@liqd.net>
2022-03-31 12:09:29 +0200

    tests: add missing CAPTCHA_URL

--- meinberlin/config/settings/test.py
+++ meinberlin/config/settings/test.py
@@ -10,6 +10,7 @@ A4_USER_FACTORY = 'meinberlin.test.factories.UserFactory'
 ACCOUNT_EMAIL_VERIFICATION = 'optional'
 
 CAPTCHA_TEST_ACCEPTED_ANSWER = 'testpass'
+CAPTCHA_URL = 'https://captcheck.netsyms.com/api.php'
 
 try:
     from .polygons import *
