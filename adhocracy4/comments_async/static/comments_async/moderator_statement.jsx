862d26561ce631112f455c71f4e948c770f3d299
Kha <k.tran@liqd.net>
2022-04-14 17:34:54 +0100

    apps/comments_async/moderator_statement.jsx: adding new moderator statement component and integrate into comments
    apps/comments/moderator_statement.jsx: using data from new api object 'statement'
    
    apps/comment/moderator statement: making translations not kosmo specific and set default to show mod statement
    
    small issue fixes: wrongly Moderator title in text and showing statement, even if statement was empty

---
+++ adhocracy4/comments_async/static/comments_async/moderator_statement.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export const ModeratorStatement = ({ last_edit: lastEdit, statement }) => {
+  const translated = {
+    moderator: 'Moderator'
+  }
+  return (
+    <div className="row">
+      <div className="a4-comments__moderator-statement__container">
+        <i className="fas fa-share a4-comments__moderator-statement__icon" />
+        <div className="a4-comments__moderator-statement__content">
+          <div className="a4-comments__moderator-statement__moderator">
+            {translated.moderator}
+          </div>
+          <div className="a4-comments__moderator-statement__date">
+            {lastEdit}
+          </div>
+          <p className="a4-comments__moderator-statement__text">
+            {statement}
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
