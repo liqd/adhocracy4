28dc92dc8b30d5c305648ceb4625c3719919149c
rine <k.schmid@liqd.net>
2022-05-11 16:05:38 +0200

    comments/serializers: adapt update method of CommentModerateSerializer to fixed version

--- adhocracy4/comments/serializers.py
+++ adhocracy4/comments/serializers.py
@@ -177,6 +177,14 @@ class CommentModerateSerializer(serializers.ModelSerializer):
         return data
 
     def update(self, instance, validated_data):
+        """Update comment instance without changing comment.modified.
+
+        This is essentially copied from
+        rest_framework.serializers.ModelSerializer.update(),
+        only difference is ignore_modified=true when saving the instance.
+        See also here:
+        https://github.com/encode/django-rest-framework/blob/master/rest_framework/serializers.py#L991-L1015
+        """
         serializers.raise_errors_on_nested_writes('update', self,
                                                   validated_data)
         info = model_meta.get_field_info(instance)
@@ -185,13 +193,21 @@ class CommentModerateSerializer(serializers.ModelSerializer):
         # Note that unlike `.create()` we don't need to treat many-to-many
         # relationships as being a special case. During updates we already
         # have an instance pk for the relationships to be associated with.
+        m2m_fields = []
         for attr, value in validated_data.items():
             if attr in info.relations and info.relations[attr].to_many:
-                field = getattr(instance, attr)
-                field.set(value)
+                m2m_fields.append((attr, value))
             else:
                 setattr(instance, attr, value)
+
         instance.save(ignore_modified=True)
+
+        # Note that many-to-many fields are set after updating instance.
+        # Setting m2m fields triggers signals which could potentially change
+        # updated instance and we do not want it to collide with .update()
+        for attr, value in m2m_fields:
+            field = getattr(instance, attr)
+            field.set(value)
 
         return instance
 
