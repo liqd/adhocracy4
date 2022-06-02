2f20464f76221206720d4f6189fd9cbe43a2f123
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-06-02 15:01:27 +0200

    babel.config: add peer deps as externals

--- babel.config.js
+++ babel.config.js
@@ -10,7 +10,12 @@ const config = {
   presets: [
     '@babel/preset-react',
     '@babel/preset-env'
-  ]
+  ],
+  externals: {
+    react_markdown: 'react-markdown',
+    react_popper: 'react-popper',
+    react_slick: 'react-slick'
+  }
 }
 
 module.exports = config
