a50186fec1bedc277d7f9dc9781bdb78c55a26f6
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-06-08 15:24:41 +0200

    static/*: update render functions to new syntax

--- adhocracy4/comments/static/comments/react_comments.jsx
+++ adhocracy4/comments/static/comments/react_comments.jsx
@@ -1,8 +1,10 @@
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
 const CommentBox = require('./CommentBox')
-const React = require('react')
-const ReactDOM = require('react-dom')
 
 module.exports.renderComment = function (el) {
   const props = JSON.parse(el.getAttribute('data-attributes'))
-  ReactDOM.render(<CommentBox {...props} />, el)
+  const root = createRoot(el)
+  root.render(<CommentBox {...props} />)
 }
