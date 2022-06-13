2f15ebc6bf1e8eeef1b2eb069ade5e1fdadfc212
Julian Dehm <j.dehm@liqd.net>
2022-06-13 09:21:57 +0200

    bleach 5.0.0

--- adhocracy4/transforms.py
+++ adhocracy4/transforms.py
@@ -1,18 +1,22 @@
 import bleach
+from bleach.css_sanitizer import CSSSanitizer
 from django.conf import settings
 
 
 def clean_html_all(text):
-    return bleach.clean(text,
-                        tags=[], attributes={}, styles=[], strip=True)
+    css_sanitizer = CSSSanitizer(
+        allowed_css_properties=[])
+    return bleach.clean(text, tags=[], attributes={},
+                        css_sanitizer=css_sanitizer, strip=True)
 
 
 def clean_html_field(text, setting='default'):
+    css_sanitizer = CSSSanitizer(
+        allowed_css_properties=settings.BLEACH_LIST[setting].get('styles', []))
     allowed_tags = settings.BLEACH_LIST[setting]['tags']
     allowed_attrs = settings.BLEACH_LIST[setting]['attributes']
-    allowed_styles = settings.BLEACH_LIST[setting].get('styles', [])
     return bleach.clean(text,
                         tags=allowed_tags,
                         attributes=allowed_attrs,
-                        styles=allowed_styles,
+                        css_sanitizer=css_sanitizer,
                         strip=True)
