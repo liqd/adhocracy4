34dd4cac4a766fe0bd952e014ff16817bb0b378b
Kha <k.tran@liqd.net>
2022-06-20 15:49:18 +0100

    eslint: fix to meet react rules
    squash
    
    squash
    
    squash

--- adhocracy4/comments_async/static/comments_async/comment_list.jsx
+++ adhocracy4/comments_async/static/comments_async/comment_list.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import Comment from './comment'
 
-export default function (props) {
+const CommentList = (props) => {
   return (
     <div>
       {
@@ -69,3 +69,5 @@ export default function (props) {
     </div>
   )
 }
+
+export default CommentList
