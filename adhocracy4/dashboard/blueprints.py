9918201430a9490407303f0f129b32f77c236e12
rine <k.schmid@liqd.net>
2022-07-01 11:18:41 +0200

    dashboard/blueprints: add type to blueprint if setting is there

--- adhocracy4/dashboard/blueprints.py
+++ adhocracy4/dashboard/blueprints.py
@@ -3,11 +3,19 @@ from collections import namedtuple
 from django.conf import settings
 from django.utils.module_loading import import_string
 
-ProjectBlueprint = namedtuple(
-    'ProjectBlueprint', [
-        'title', 'description', 'content', 'image', 'settings_model'
-    ]
-)
+if hasattr(settings, 'A4_BLUEPRINT_TYPES'):
+    ProjectBlueprint = namedtuple(
+        'ProjectBlueprint', [
+            'title', 'description', 'content', 'image', 'settings_model',
+            'type'
+        ]
+    )
+else:
+    ProjectBlueprint = namedtuple(
+        'ProjectBlueprint', [
+            'title', 'description', 'content', 'image', 'settings_model'
+        ]
+    )
 
 
 def get_blueprints():
