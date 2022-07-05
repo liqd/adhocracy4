988d0fa9941cc159714d4645102dbff38f8abf83
rine <k.schmid@liqd.net>
2022-07-05 10:36:20 +0200

    comments: move date conversion to own function and use in serializer

---
+++ adhocracy4/api/dates.py
@@ -0,0 +1,19 @@
+from django.template import defaultfilters
+from django.utils import timezone
+
+
+def get_datetime_display(datetime):
+    """Convert datetime instance to localtime and return as str."""
+    local_datetime = timezone.localtime(datetime)
+    local_date = defaultfilters.date(local_datetime)
+    local_time = defaultfilters.time(local_datetime)
+
+    return local_date + ', ' + local_time
+
+
+def get_date_display(datetime):
+    """Convert datetime instance to localtime and return date as str."""
+    local_datetime = timezone.localtime(datetime)
+    local_date = defaultfilters.date(local_datetime)
+
+    return local_date
