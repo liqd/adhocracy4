836402f8e1151dc2a9d90deda66ad022f6d0a61d
rine <k.schmid@liqd.net>
2022-07-08 00:59:59 +0200

    tests/api: test display of local dates

---
+++ tests/api/test_dates_display.py
@@ -0,0 +1,41 @@
+import datetime
+
+import pytest
+import pytz
+from django.test.utils import override_settings
+from django.utils import translation
+
+from adhocracy4.api.dates import get_date_display
+from adhocracy4.api.dates import get_datetime_display
+
+created = datetime.datetime(2022, 7, 1, 17, 0, 0, tzinfo=pytz.utc)
+
+
+@pytest.mark.django_db
+def test_datetime_display_utc():
+    with translation.override('en_EN'):
+        datetime_display = get_datetime_display(created)
+    assert datetime_display == 'July 1, 2022, 5 p.m.'
+
+
+@override_settings(TIME_ZONE='Europe/Berlin')
+@pytest.mark.django_db
+def test_datetime_display_berlin():
+    with translation.override('de_DE'):
+        datetime_display = get_datetime_display(created)
+    assert datetime_display == '1. Juli 2022, 19:00'
+
+
+@pytest.mark.django_db
+def test_date_display_utc():
+    with translation.override('en_EN'):
+        date_display = get_date_display(created)
+    assert date_display == 'July 1, 2022'
+
+
+@override_settings(TIME_ZONE='Europe/London')
+@pytest.mark.django_db
+def test_date_display_berlin():
+    with translation.override('de_DE'):
+        date_display = get_date_display(created)
+    assert date_display == '1. Juli 2022'
