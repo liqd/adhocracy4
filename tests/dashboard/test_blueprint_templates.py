b0b31203c867d46b4c01cab2d4dea4feec0e6990
rine <k.schmid@liqd.net>
2022-09-07 19:29:23 +0200

    tests/dashboard: add blueprints_with_type for testing and test ProjectCreateView with that

--- tests/dashboard/test_blueprint_templates.py
+++ tests/dashboard/test_blueprint_templates.py
@@ -1,28 +1,29 @@
 import importlib
 
 import pytest
-from django.conf import settings
 from django.test.utils import override_settings
-from django.utils.module_loading import import_string
+
+from adhocracy4.dashboard.blueprints import get_blueprints
 
 
 @override_settings(A4_BLUEPRINT_TYPES=[('QS', 'questions')])
+@override_settings(A4_DASHBOARD={
+    'BLUEPRINTS': 'tests.project.blueprints_with_type.blueprints'})
 @pytest.mark.django_db
-def test_blueprint_template_with_types_raises_error():
+def test_blueprint_template_with_types():
     # reload blueprints to reinitialize ProjectBlueprint with
     # overridden settings
     bp_module = importlib.import_module('adhocracy4.dashboard.blueprints')
     importlib.reload(bp_module)
-    with pytest.raises(TypeError) as error:
-        import_string(settings.A4_DASHBOARD['BLUEPRINTS'])
-    assert str(error.value).\
-        endswith('missing 1 required positional argument: \'type\'')
+    blueprints = get_blueprints()
+    assert blueprints[0][1]._fields == ('title', 'description', 'content',
+                                        'image', 'settings_model', 'type')
 
 
 @pytest.mark.django_db
 def test_blueprint_template_without_types():
     bp_module = importlib.import_module('adhocracy4.dashboard.blueprints')
     importlib.reload(bp_module)
-    blueprints = import_string(settings.A4_DASHBOARD['BLUEPRINTS'])
+    blueprints = get_blueprints()
     assert blueprints[0][1]._fields == ('title', 'description', 'content',
                                         'image', 'settings_model')
