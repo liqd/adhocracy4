84136b5a2b6f828dba9ed8f58457a1dc4731c97a
Kha <k.tran@liqd.net>
2022-10-04 11:29:08 +0100

    jstests: test conditional rendering of filters after click

--- meinberlin/apps/budgeting/assets/__tests__/ControlBar.jest.jsx
+++ meinberlin/apps/budgeting/assets/__tests__/ControlBar.jest.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import { ControlBar } from '../ControlBar'
 
 const filters = {
@@ -16,7 +16,7 @@ const filters = {
   }
 }
 
-test('ControlBar shows filters', () => {
+test('ControlBar collapsed bar', () => {
   const onChangeFiltersFn = jest.fn()
   render(
     <ControlBar
@@ -29,4 +29,21 @@ test('ControlBar shows filters', () => {
   expect(numOfResultsElement).toBeTruthy()
   const orderingFilterElement = screen.getByText('ordering: Most recent')
   expect(orderingFilterElement).toBeTruthy()
+})
+
+test('ControlBar expanded bar', async () => {
+  const onChangeFiltersFn = jest.fn()
+  render(
+    <ControlBar
+      filters={filters}
+      onChangeFilters={() => onChangeFiltersFn()}
+      numOfResults={2}
+    />
+  )
+  const filterButton = screen.getByLabelText('Toggle filters')
+  let expandedFilter = screen.queryByText(/category1/)
+  expect(expandedFilter).toBeFalsy()
+  fireEvent.click(filterButton)
+  expandedFilter = screen.queryByText(/category1/)
+  expect(expandedFilter).toBeTruthy()
 })
