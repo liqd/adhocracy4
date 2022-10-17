d6dab4c6c77a1c9286406b8ef860fbfed493ca17
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-10-17 17:32:17 +0200

    static/alert: add correct role=alert for a11y

--- adhocracy4/static/ErrorList.jsx
+++ adhocracy4/static/ErrorList.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ErrorList = ({ errors, field }) => {
   if (errors && errors[field]) {
     return (
-      <ul className="errorlist">
+      <ul className="errorlist" role="alert">
         {errors[field].map(function (msg, index) {
           return <li key={msg}>{msg}</li>
         })}
