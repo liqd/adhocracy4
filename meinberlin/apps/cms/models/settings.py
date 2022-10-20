cf343b7a5275a1d9c2ea9e42c37713e7f49ecdc1
phillimorland <5871230+phillimorland@users.noreply.github.com>
2022-10-20 12:36:32 +0200

    base.txt: update wagtail, cms/settings: update BaseSetting to BaseSiteSetting

--- meinberlin/apps/cms/models/settings.py
+++ meinberlin/apps/cms/models/settings.py
@@ -1,11 +1,11 @@
 from django.db import models
 from wagtail.admin.panels import FieldPanel
-from wagtail.contrib.settings.models import BaseSetting
+from wagtail.contrib.settings.models import BaseSiteSetting
 from wagtail.contrib.settings.models import register_setting
 
 
 @register_setting
-class HeaderPages(BaseSetting):
+class HeaderPages(BaseSiteSetting):
     help_page = models.ForeignKey(
         'wagtailcore.Page',
         related_name="help_page",
