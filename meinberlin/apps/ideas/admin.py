8e2a21d9f6a37ddab37e8cfd9a5c0a050364a313
rine <k.schmid@liqd.net>
2022-11-17 19:31:00 +0100

    ideas/admin: limit choices for categories, labels and moderation tasks to the ones belonging to module

--- meinberlin/apps/ideas/admin.py
+++ meinberlin/apps/ideas/admin.py
@@ -1,7 +1,10 @@
 from django import forms
 from django.contrib import admin
 
+from adhocracy4.categories.models import Category
+from adhocracy4.labels.models import Label
 from adhocracy4.modules import admin as module_admin
+from meinberlin.apps.moderationtasks.models import ModerationTask
 
 from . import models
 
@@ -9,7 +12,9 @@ from . import models
 class ItemAdminForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.fields['category'].required = True
         self.fields['labels'].required = False
+        self.fields['completed_tasks'].required = False
 
 
 class IdeaAdmin(module_admin.ItemAdmin):
@@ -17,6 +22,26 @@ class IdeaAdmin(module_admin.ItemAdmin):
 
     def has_add_permission(self, request, obj=None):
         return False
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'category':
+            kwargs['queryset'] = \
+                Category.objects.filter(module=self.get_module(request))
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == 'labels':
+            kwargs['queryset'] = \
+                Label.objects.filter(module=self.get_module(request))
+        elif db_field.name == 'completed_tasks':
+            kwargs['queryset'] = \
+                ModerationTask.objects.filter(module=self.get_module(request))
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+    def get_module(self, request):
+        item = self.get_object(
+            request, request.resolver_match.kwargs['object_id'])
+        return item.module
 
 
 admin.site.register(models.Idea, IdeaAdmin)
