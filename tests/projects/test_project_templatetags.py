4ebde6c3e2c3807ceeaa30b5a1cce337a5539a76
fuzzylogic2000 <k.lindenlaub@liqd.net>
2022-12-06 17:15:00 +0100

    apps/projects: remove deprecated properties

--- tests/projects/test_project_templatetags.py
+++ tests/projects/test_project_templatetags.py
@@ -1,9 +1,5 @@
-from datetime import timedelta
-
 import pytest
-from freezegun import freeze_time
 
-from adhocracy4.projects.enums import Access
 from adhocracy4.test.helpers import render_template
 
 
@@ -13,40 +9,6 @@ def test_get_days_tag():
     assert 'a few hours left' == render_template(template, {'days': 0})
     assert '1 day left' == render_template(template, {'days': 1})
     assert '2 days left' == render_template(template, {'days': 2})
-
-
-@pytest.mark.django_db
-def test_get_class_public(phase):
-    project = phase.module.project
-    template = '{% load project_tags %}{% get_class project as x %}{{x}}'
-
-    with freeze_time(phase.end_date - timedelta(days=5, minutes=1)):
-        assert 'public' == render_template(template, {'project': project})
-
-
-@pytest.mark.django_db
-def test_get_class_running_out(phase):
-    project = phase.module.project
-    template = '{% load project_tags %}{% get_class project as x %}{{x}}'
-
-    with freeze_time(phase.end_date - timedelta(seconds=1)):
-        assert 'running-out' == render_template(template, {'project': project})
-
-
-@pytest.mark.django_db
-def test_get_class_finished(phase):
-    project = phase.module.project
-    template = '{% load project_tags %}{% get_class project as x %}{{x}}'
-
-    with freeze_time(phase.end_date):
-        assert 'finished' == render_template(template, {'project': project})
-
-
-@pytest.mark.parametrize('project__access', [Access.PRIVATE])
-@pytest.mark.django_db
-def test_get_class_privat(project):
-    template = '{% load project_tags %}{% get_class project as x %}{{x}}'
-    assert 'private' == render_template(template, {'project': project})
 
 
 @pytest.mark.django_db
