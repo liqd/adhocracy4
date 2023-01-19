0c3d47dcabd093620d3bde9c3db7b788a6ff0e50
Kha <k.tran@liqd.net>
2023-01-19 18:25:29 +0100

    [#6951]scss/modal: styling modal to match design (depending on a4)

--- meinberlin/apps/budgeting/assets/BudgetingProposalList.jsx
+++ meinberlin/apps/budgeting/assets/BudgetingProposalList.jsx
@@ -5,6 +5,7 @@ import { Pagination } from './Pagination'
 import { CountDown } from '../../contrib/assets/CountDown'
 import { ControlBar } from './ControlBar'
 import { useLocation, useSearchParams } from 'react-router-dom'
+import Modal from 'adhocracy4/adhocracy4/static/Modal'
 
 export const BudgetingProposalList = (props) => {
   const [data, setData] = useState([])
@@ -15,8 +16,22 @@ export const BudgetingProposalList = (props) => {
     const countText = django.ngettext('you have 1 vote left.', 'you have %s votes left.', votes)
     return django.interpolate(countText, [votes])
   }
-  const noResults = django.gettext('Nothing to show')
-  const endSessionString = django.gettext('Are you ready? End session')
+
+  const translations = {
+    noResults: django.gettext('Nothing to show'),
+    finished: django.gettext('Are you finished?'),
+    endSession: django.gettext('End session'),
+    modalDescription: django.gettext('To save your votes you do not need to do anything else. End the session to enter a new code. If you want to change the votes you have already cast, you can re-enter your code as long as the voting phase is running. '),
+    modalBodyQuestion: django.gettext('Do you want to end the session?'),
+    modalCancel: django.gettext('Cancel')
+  }
+
+  const modalPartials = {
+    title: translations.endSession,
+    description: translations.modalDescription,
+    abort: translations.modalCancel
+  }
+
   const scrolledRef = useRef(false)
 
   const fetchProposals = () => {
@@ -42,7 +57,7 @@ export const BudgetingProposalList = (props) => {
       .catch(error => console.log(error))
   }
 
-  const endSession = () => {
+  const handleEndSession = () => {
     const endSessionUrl = props.end_session_url
     fetch(endSessionUrl)
       .then(() => window.location.reload(true))
@@ -106,9 +121,24 @@ export const BudgetingProposalList = (props) => {
                 inactiveClass="btn btn--full btn--light u-spacer-bottom btn--huge"
                 counter={meta?.token_info.num_votes_left}
               />
-              <button className="btn" onClick={endSession}>
-                {endSessionString}
-              </button>
+              <div className="u-spacer-bottom u-align-center">
+                <span>{translations.finished} </span>
+                <a
+                  className="btn--link"
+                  href="something"
+                  data-bs-toggle="modal"
+                  data-bs-target="#end-session_modal"
+                >
+                  {translations.endSession}
+                </a>
+              </div>
+              <Modal
+                name="end-session_modal"
+                abort={translations.modalCancel}
+                action={translations.endSession}
+                partials={modalPartials}
+                handleSubmit={handleEndSession}
+              />
             </div>
           </div>
         </div>}
@@ -121,7 +151,7 @@ export const BudgetingProposalList = (props) => {
           <div className="offset-lg-2 col-lg-8">
             {Object.keys(data).length > 0
               ? renderList(data)
-              : noResults}
+              : translations.noResults}
           </div>
         </div>
       </div>
