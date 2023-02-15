8a4bf4e768d9bafb56a02f689f4a1e603f5d0336
fuzzylogic2000 <k.lindenlaub@liqd.net>
2023-02-15 09:51:58 +0100

    apps/models: add verbose names for created and modified

--- adhocracy4/models/base.py
+++ adhocracy4/models/base.py
@@ -1,12 +1,21 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 
 
 class TimeStampedModel(models.Model):
-
-    created = models.DateTimeField(editable=False, default=timezone.now)
-    modified = models.DateTimeField(blank=True, null=True, editable=False)
+    created = models.DateTimeField(
+        verbose_name=_("Created"),
+        editable=False,
+        default=timezone.now,
+    )
+    modified = models.DateTimeField(
+        verbose_name=_("Modified"),
+        blank=True,
+        null=True,
+        editable=False,
+    )
 
     class Meta:
         abstract = True
@@ -18,7 +27,6 @@ class TimeStampedModel(models.Model):
 
 
 class UserGeneratedContentModel(TimeStampedModel):
-
     creator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     class Meta:
