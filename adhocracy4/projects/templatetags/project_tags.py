ca0331aa7612304691fe637903fec4f6936a1f9f
fuzzylogic2000 <k.lindenlaub@liqd.net>
2023-02-17 15:09:48 +0100

    apps/projects: remove method from templatetag that was only used in opin

--- adhocracy4/projects/templatetags/project_tags.py
+++ adhocracy4/projects/templatetags/project_tags.py
@@ -1,22 +1,6 @@
 from django import template
-from django.utils.translation import gettext as _
-from django.utils.translation import ngettext
 
 register = template.Library()
-
-
-@register.simple_tag
-def get_days(number):
-    # FIXME: only used in opin. Do we need it? Is there a better way?
-    if number and number >= 1 and number <= 5:
-        text = ngettext("%(number)d day left", "%(number)d days left", number) % {
-            "number": number,
-        }
-        return text
-    elif number == 0:
-        return _("a few hours left")
-    else:
-        return ""
 
 
 @register.simple_tag
--- tests/projects/test_project_templatetags.py
+++ tests/projects/test_project_templatetags.py
@@ -3,14 +3,6 @@ import pytest
 from adhocracy4.test.helpers import render_template
 
 
-def test_get_days_tag():
-    template = "{% load project_tags %}{% get_days days as x %}{{x}}"
-
-    assert "a few hours left" == render_template(template, {"days": 0})
-    assert "1 day left" == render_template(template, {"days": 1})
-    assert "2 days left" == render_template(template, {"days": 2})
-
-
 @pytest.mark.django_db
 def test_project_tile_image(project):
     template = "{% load project_tags %}" "{% project_tile_image project as x %}{{x}}"
