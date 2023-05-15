44e72f0ad3e5257f8abf062e7a8b6e4e0efd385f
Phillippa <phillippa@PhillipasLaptop.fritz.box>
2023-05-15 15:35:43 +0200

    webpack: add poll entry point

--- webpack.common.js
+++ webpack.common.js
@@ -150,6 +150,18 @@ module.exports = {
       ],
       dependOn: 'adhocracy4'
     },
+    poll_management: {
+      import: [
+        'adhocracy4/adhocracy4/polls/static/react_poll_management.jsx'
+      ],
+      dependOn: 'adhocracy4'
+    },
+    polls: {
+      import: [
+        'adhocracy4/adhocracy4/polls/static/react_polls.jsx'
+      ],
+      dependOn: 'adhocracy4'
+    },
     select_dropdown_init: {
       import: [
         'adhocracy4/adhocracy4/categories/assets/select_dropdown_init.js'
