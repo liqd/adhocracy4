856ec1007b1952e8c668d7706baa8a9d0a6f1275
rine <k.schmid@liqd.net>
2023-07-07 13:10:05 +0200

    tests/labels: add LabelAliasFactory and tests static get_label_alias()

--- tests/labels/conftest.py
+++ tests/labels/conftest.py
@@ -1,8 +1,8 @@
 from pytest_factoryboy import register
 
 from tests.apps.ideas import factories as idea_factories
+from tests.labels import factories as label_factories
 
-from .factories import LabelFactory
-
-register(LabelFactory)
 register(idea_factories.IdeaFactory)
+register(label_factories.LabelFactory)
+register(label_factories.LabelAliasFactory)
--- tests/labels/test_model.py
+++ tests/labels/test_model.py
@@ -1,6 +1,22 @@
 import pytest
 
+from adhocracy4.labels.models import LabelAlias
+
 
 @pytest.mark.django_db
 def test_str_label(label):
     assert str(label) == label.name
+
+
+@pytest.mark.django_db
+def test_str_label_alias(label_alias):
+    assert str(label_alias) == label_alias.title
+
+
+@pytest.mark.django_db
+def test_get_label_alias(module, label_factory, label_alias_factory):
+    label_factory(module=module)
+    assert not LabelAlias.get_label_alias(module)
+
+    label_alias = label_alias_factory(module=module)
+    assert LabelAlias.get_label_alias(module) == label_alias
