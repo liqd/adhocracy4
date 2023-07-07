95c6227c3c35cf3ec0423f2090795ac7e2656fa7
rine <k.schmid@liqd.net>
2023-07-07 13:20:59 +0200

    tests/categories//labels: move category and label factories to test folder, so they can be imported in other projects

--- tests/categories/conftest.py
+++ tests/categories/conftest.py
@@ -1,6 +1,6 @@
 from pytest_factoryboy import register
 
-from tests.categories import factories as category_factories
+from adhocracy4.test.factories import categories as category_factories
 
 register(category_factories.CategoryFactory)
 register(category_factories.CategoryAliasFactory)
--- tests/exports/conftest.py
+++ tests/exports/conftest.py
@@ -1,11 +1,11 @@
 from pytest_factoryboy import register
 
+from adhocracy4.test.factories import categories as category_factories
+from adhocracy4.test.factories import labels as label_factories
 from tests.apps.ideas import factories as idea_factories
 from tests.apps.moderatorfeedback import factories as moderatorfeedback_factories
 from tests.apps.questions import factories as question_factories
-from tests.categories import factories as category_factories
 from tests.comments import factories as comment_factories
-from tests.labels import factories as label_factories
 from tests.ratings import factories as rating_factories
 
 register(idea_factories.IdeaFactory)
