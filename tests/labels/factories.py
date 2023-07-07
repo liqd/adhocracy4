936618f956ae6ffeb267b2b0fd14aa8aba012bdb
rine <k.schmid@liqd.net>
2023-07-07 13:10:05 +0200

    tests/labels: add LabelAliasFactory and tests static get_label_alias()

--- tests/labels/factories.py
+++ tests/labels/factories.py
@@ -1,6 +1,7 @@
 import factory
 
 from adhocracy4.labels.models import Label
+from adhocracy4.labels.models import LabelAlias
 from adhocracy4.test.factories import ModuleFactory
 
 
@@ -9,4 +10,13 @@ class LabelFactory(factory.django.DjangoModelFactory):
         model = Label
 
     name = factory.Faker("name")
+    module = factory.SubFactory(ModuleFactory)
+
+
+class LabelAliasFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = LabelAlias
+
+    title = factory.Faker("name")
+    description = factory.Faker("sentence", nb_words=4)
     module = factory.SubFactory(ModuleFactory)
