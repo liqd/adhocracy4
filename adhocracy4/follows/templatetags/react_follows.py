c8979970e37e0964e68380077884dd1e660c8ca4
Julian Dehm <j.dehm@liqd.net>
2023-10-24 13:37:05 +0200

    adhocracy4/follows: on click redirect to login page if user is not logged in

--- adhocracy4/follows/templatetags/react_follows.py
+++ adhocracy4/follows/templatetags/react_follows.py
@@ -1,12 +1,20 @@
+import json
+
 from django import template
 from django.utils.html import format_html
 
 register = template.Library()
 
 
-@register.simple_tag()
-def react_follows(project):
+@register.simple_tag(takes_context=True)
+def react_follows(context, project):
+    request = context["request"]
+    user = request.user
+    authenticated_as = None
+    if user.is_authenticated:
+        authenticated_as = user.username
+    attributes = {"project": project.name, "authenticatedAs": authenticated_as}
     return format_html(
-        '<span data-a4-widget="follows" data-project={project}></span>',
-        project=project.slug,
+        '<span data-a4-widget="follows" data-attributes="{attributes}"></span>',
+        attributes=json.dumps(attributes),
     )
