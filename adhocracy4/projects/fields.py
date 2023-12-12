ee18d9d8a8edf289ccbf800b5391ecb6378867e6
Mara Karagianni <m.karagianni@liqd.net>
2023-12-12 13:51:31 +0200

    deps: removed django-multiselectedfield

--- adhocracy4/projects/fields.py
+++ adhocracy4/projects/fields.py
@@ -1,19 +1,5 @@
-from multiselectfield import MultiSelectField
+from django.db import models
 
 
-class TopicField(MultiSelectField):
+class TopicField(models.CharField):
     """Deprecated, don't use"""
-
-    # TODO: remove once topic migrations are rolled out
-    def __init__(self, *args, **kwargs):
-        kwargs["max_length"] = 254
-        kwargs["max_choices"] = 2
-        kwargs["default"] = ""
-        kwargs["blank"] = False
-        super().__init__(*args, **kwargs)
-
-    def contribute_to_class(self, cls, name, **kwargs):
-        self.choices = ()
-
-        # Call the super method at last so that choices are already initialized
-        super().contribute_to_class(cls, name, **kwargs)
