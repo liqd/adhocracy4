5f4d352727403b576fa6d3cb3414ebc44bf4fa16
Julian Dehm <j.dehm@liqd.net>
2024-02-14 13:03:05 +0100

    tests/helpers.py: add helper to get all emails for an email address which match a subject

--- tests/helpers.py
+++ tests/helpers.py
@@ -2,6 +2,7 @@ import re
 
 from django.contrib.contenttypes.models import ContentType
 from django.contrib.sites.models import Site
+from django.core import mail
 
 
 class pytest_regex:
@@ -23,3 +24,14 @@ def clear_query_cache():
     some queries will be cached. Call directly before django_assert_num_queries."""
     ContentType.objects.clear_cache()
     Site.objects.clear_cache()
+
+
+def get_emails_for_address_and_subject(email_address, subject):
+    """Return all emails send to email_address with a subject starting with subject"""
+    emails = list(
+        filter(
+            lambda mails: mails.to[0] == email_address and subject in mails.subject,
+            mail.outbox,
+        )
+    )
+    return emails
