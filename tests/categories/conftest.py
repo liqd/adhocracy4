61ad39f88a8cfb0e2c4e7d4e1253d89e2867f122
Julian Dehm <j.dehm@liqd.net>
2024-04-22 16:43:02 +0200

    categories: make icon form field optional to allow categories without icon

--- tests/categories/conftest.py
+++ tests/categories/conftest.py
@@ -1,6 +1,8 @@
 from pytest_factoryboy import register
 
 from adhocracy4.test.factories import categories as category_factories
+from adhocracy4.test.factories.maps import AreaSettingsFactory
 
 register(category_factories.CategoryFactory)
 register(category_factories.CategoryAliasFactory)
+register(AreaSettingsFactory)
