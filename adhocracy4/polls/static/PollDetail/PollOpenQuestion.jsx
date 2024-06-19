1ef70bdbcedc90b8a7a230fe2a9bc680710f8f7d
hom3mad3 <8156337+hom3mad3@users.noreply.github.com>
2024-06-19 16:40:19 +0200

    mB redesign poll

--- adhocracy4/polls/static/PollDetail/PollOpenQuestion.jsx
+++ adhocracy4/polls/static/PollDetail/PollOpenQuestion.jsx
@@ -25,7 +25,7 @@ export const PollOpenQuestion = (props) => {
   }
 
   return (
-    <div className="poll">
+    <div className="poll poll--question">
       <h2>{props.question.label}</h2>
       {questionHelpText}
       <div className="poll__rows">
