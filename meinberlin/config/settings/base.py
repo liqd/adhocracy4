4a1b87261eb527230ce7efe76dfcc6b9a0bf18fc
Julian Dehm <j.dehm@liqd.net>
2024-07-17 16:33:14 +0200

    settings: add organisation logo max resolution

--- meinberlin/config/settings/base.py
+++ meinberlin/config/settings/base.py
@@ -219,7 +219,7 @@ IMAGE_ALIASES = {
     },
     "heroimage": {"min_resolution": (1500, 500)},
     "tileimage": {"min_resolution": (500, 300)},
-    "logo": {"min_resolution": (200, 50)},
+    "logo": {"min_resolution": (200, 50), "max_resolution": (650, 650)},
     "avatar": {"min_resolution": (200, 200)},
     "idea_image": {"min_resolution": (600, 400)},
     "plan_image": {"min_resolution": (600, 400)},
