8cd222b94bf4d23696bc741a4df0f372c03ba944
Julian Dehm <j.dehm@liqd.net>
2024-09-10 13:19:04 +0200

    delete actions

--- adhocracy4/actions/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Action
-
-
-@admin.register(Action)
-class ActionAdmin(admin.ModelAdmin):
-    readonly_fields = ("actor",)
--- adhocracy4/actions/apps.py
+++ adhocracy4/actions/apps.py
@@ -4,6 +4,3 @@ from django.apps import AppConfig
 class Config(AppConfig):
     name = "adhocracy4.actions"
     label = "a4actions"
-
-    def ready(self):
-        import adhocracy4.actions.signals  # noqa:F401
