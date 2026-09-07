ec1178924c8cad362d24742dcb65d09a0c00b333
glanzel <glanzel@gmx.de>
2026-09-07 16:42:50 +0200

    [ST-2573] assets: Better Map Zoom

--- adhocracy4/maps/static/a4maps/a4maps_display_points.js
+++ adhocracy4/maps/static/a4maps/a4maps_display_points.js
@@ -32,11 +32,16 @@ function init () {
       mapboxToken: e.getAttribute('data-mapbox-token'),
       omtToken: e.getAttribute('data-omt-token'),
       dragging: true,
-      scrollWheelZoom: false,
+      scrollWheelZoom: true,
       zoomControl: false,
       maxZoom: 18,
       tap: false
     })
+
+    L.control.scale({
+      position: 'bottomright',
+      imperial: false
+    }).addTo(map)
 
     map.on('zoomend', function () {
       const currentZoom = map.getZoom()
